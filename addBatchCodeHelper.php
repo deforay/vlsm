fbe2f2e00b502f0920946bdb80175e6df4101ffb
Saravanan A.S. <saravanan@deforay.com>
2016-07-29 13:11:31 +0530

    multi select added under add batch file.

---
+++ addBatchCodeHelper.php
@@ -0,0 +1,25 @@
+<?php
+ob_start();
+include('./includes/MysqliDb.php');
+include('header.php');
+
+$tableName1="batch_details";
+$tableName2="vl_request_form";
+try {
+        $data=array('batch_code'=>$_POST['batchCode']);
+        $db->insert($tableName1,$data);
+        $lastId = $db->getInsertId();
+        if($lastId!=0 && $lastId!=''){
+            for($j=0;$j<=count($_POST['sampleCode']);$j++){
+                $treamentId = $_POST['sampleCode'][$j];
+                $value = array('batch_id'=>$lastId);
+                $db=$db->where('treament_id',$treamentId);
+                $db->update($tableName2,$value); 
+            }
+            $_SESSION['alertMsg']="Batch code added successfully";
+            header("location:addBatch.php");
+        }
+} catch (Exception $exc) {
+    error_log($exc->getMessage());
+    error_log($exc->getTraceAsString());
+}
--- footer.php
+++ footer.php
@@ -10,6 +10,7 @@
 <!--<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>-->
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script type="text/javascript" src="assets/js/jquery-ui-timepicker-addon.js"></script>
+
 	<script src="assets/js/select2.js"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="assets/js/bootstrap.min.js"></script>
