f3028a0b9b76efd809b4c5c8aeed8e2328155edb
Ilahir Rahman Ali <ilahir@deforay.com>
2016-08-09 20:11:09 +0530

    Lable change in user and facilities page

--- facilities.php
+++ facilities.php
@@ -6,10 +6,10 @@ include('header.php');
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <h1>Manage Facilities</h1>
+      <h1>Facilities</h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Manage Facilities</li>
+        <li class="active">Facilities</li>
       </ol>
     </section>
 
