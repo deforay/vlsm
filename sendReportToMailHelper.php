dea6855d3b119c486670eb2362a7d7fc75a24893
Ilahir Rahman Ali <ilahir@deforay.com>
2016-08-11 18:57:28 +0530

    Completed report sent to mail

--- sendReportToMailHelper.php
+++ sendReportToMailHelper.php
@@ -5,63 +5,81 @@ include('./includes/MysqliDb.php');
 require './includes/mail/PHPMailerAutoload.php';
 //print_r($_POST);die;
 //insert record
-$reportData=array('to_mail'=>$_POST['toMail'],
-                  'encrypt'=>$_POST['encrypt'],
-                  'password'=>$_POST['password'],
-                    'comment'=>$_POST['comment'],
-                    'batch_id'=>$_POST['batchId'],
-                    );
-$reportId = $db->insert('report_to_mail', $reportData);
-//get email id
-$geQuery="SELECT * FROM global_config where name='email'";
-$geResult = $db->rawQuery($geQuery);
-$gpQuery="SELECT * FROM global_config where name='password'";
-$gpResult = $db->rawQuery($gpQuery);
-if($reportId){
-     //Create a new PHPMailer instance
-                $mail = new PHPMailer();
-                //Tell PHPMailer to use SMTP
-                $mail->isSMTP();
-                //Enable SMTP debugging
-                // 0 = off (for production use)
-                // 1 = client messages
-                // 2 = client and server messages
-                $mail->SMTPDebug = 0;
-                //Ask for HTML-friendly debug output
-                $mail->Debugoutput = 'html';
-                //Set the hostname of the mail server
-                $mail->Host = 'smtp.gmail.com';
-                //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-                $mail->Port = 587;
-                //Set the encryption system to use - ssl (deprecated) or tls
-                $mail->SMTPSecure = 'tls';
-                //Whether to use SMTP authentication
-                $mail->SMTPAuth = true;
-                $mail->SMTPKeepAlive = true; 
-                //Username to use for SMTP authentication - use full email address for gmail
-                $mail->Username = $geResult[0]['value'];
-                //Password to use for SMTP authentication
-                $mail->Password = $gpResult[0]['value'];
-                //Set who the message is to be sent from
-                $mail->setFrom($geResult[0]['value']);
+$batchId=base64_decode($_POST['batchId']);
+if(isset($_POST['fileName']) && trim($_POST['fileName'])!="" && $batchId>0){
+	 
+	 $reportData=array(
+		  'to_mail'=>$_POST['toMail'],
+		  'subject'=>$_POST['mailSubject'],
+		  'encrypt'=>$_POST['encrypt'],
+		  'password'=>$_POST['password'],
+		  'comment'=>$_POST['comment'],
+		  'batch_id'=>$batchId,
+	 );
+	 $reportId = $db->insert('report_to_mail',$reportData);
+	 //get email id
+	 $geQuery="SELECT * FROM global_config where name='email'";
+	 $geResult = $db->rawQuery($geQuery);
+	 $gpQuery="SELECT * FROM global_config where name='password'";
+	 $gpResult = $db->rawQuery($gpQuery);
+	 if($reportId){
+		  //Create a new PHPMailer instance
+		  $mail = new PHPMailer();
+		  //Tell PHPMailer to use SMTP
+		  $mail->isSMTP();
+		  //Enable SMTP debugging
+		  // 0 = off (for production use)
+		  // 1 = client messages
+		  // 2 = client and server messages
+		  $mail->SMTPDebug = 0;
+		  //Ask for HTML-friendly debug output
+		  $mail->Debugoutput = 'html';
+		  //Set the hostname of the mail server
+		  $mail->Host = 'smtp.gmail.com';
+		  //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+		  $mail->Port = 587;
+		  //Set the encryption system to use - ssl (deprecated) or tls
+		  $mail->SMTPSecure = 'tls';
+		  //Whether to use SMTP authentication
+		  $mail->SMTPAuth = true;
+		  $mail->SMTPKeepAlive = true; 
+		  //Username to use for SMTP authentication - use full email address for gmail
+		  $mail->Username = $geResult[0]['value'];
+		  //Password to use for SMTP authentication
+		  $mail->Password = $gpResult[0]['value'];
+		  //Set who the message is to be sent from
+		  $mail->setFrom($geResult[0]['value']);
                 
-                //Admin Mail
-                  $mail->Subject = 'Report Data';
-                  $message='Hi Sir,<br/> PFA';
-                  
-                    $mail->addAddress($_POST['toMail']);
-                    $file_to_attach = 'temporary/'.$_POST['fileName'];
+          //Admin Mail
+		  if(isset($_POST['mailSubject']) && trim($_POST['mailSubject'])!=""){
+			   $subject=$_POST['mailSubject'];
+		  }else{
+			   $subject="Request report";
+		  }
+		  
+		  if(isset($_POST['comment']) && trim($_POST['comment'])!=""){
+			   $message=$_POST['comment'];
+		  }else{
+			   $message='Hi Sir,<br/> PFA';
+		  }
+		  
+		  $mail->Subject = $subject;
+		  
+		  $mail->addAddress($_POST['toMail']);
+		  $file_to_attach = 'temporary/'.$_POST['fileName'];
 
-                    $mail->AddAttachment($file_to_attach);
-                    $mail->msgHTML($message);
-                    if (!$mail->send())
-                    {
-			$_SESSION['alertMsg']='Unable to send message. Please try later.';
-                        echo "Mailer Error: " . $mail->ErrorInfo;die;
-                    }
-                   else{
-                        $_SESSION['alertMsg']='Report sent successfully';
-                        header('location:vlResultMail.php');
-                   }
+		  $mail->AddAttachment($file_to_attach);
+		  $mail->msgHTML($message);
+		  if (!$mail->send())
+		  {
+			   $_SESSION['alertMsg']='Unable to send message. Please try later.';
+			   error_log("Mailer Error: " . $mail->ErrorInfo);
+			   header('location:vlRequestMail.php');
+		  }
+		  else{
+			   $_SESSION['alertMsg']='Report sent successfully';
+			   header('location:vlRequestMail.php');
+		  }
+	 }
 }
 ?>
--- vlRequestExportInExcel.php
+++ vlRequestExportInExcel.php
@@ -6,16 +6,22 @@ include('General.php');
 include ('./includes/PHPExcel.php');
 $general=new Deforay_Commons_General();
 
-if(isset($_SESSION['vlResultQuery']) && trim($_SESSION['vlResultQuery'])!=""){
+$batchId=base64_decode($_POST['batchId']);
+
+if($batchId>0){
  
- $rResult = $db->rawQuery($_SESSION['vlResultQuery']);
+ $query="SELECT vl.*,f.*,s.sample_name,b.*,ts.*,acd.art_code,rst.sample_name as routineSampleName,fst.sample_name as failureSampleName,sst.sample_name as suspectedSampleName  FROM vl_request_form as vl INNER JOIN facility_details as f ON vl.facility_id=f.facility_id  INNER JOIN r_sample_type as s ON s.sample_id=vl.sample_id INNER JOIN testing_status as ts ON ts.status_id=vl.status LEFT JOIN r_art_code_details as acd ON acd.art_id=vl.current_regimen LEFT JOIN r_sample_type as rst ON rst.sample_id=vl.routine_monitoring_sample_type LEFT JOIN r_sample_type as fst ON fst.sample_id=vl.vl_treatment_failure_adherence_counseling_sample_type  LEFT JOIN r_sample_type as sst ON sst.sample_id=vl.suspected_treatment_failure_sample_type LEFT JOIN batch_details as b ON b.batch_id=vl.batch_id where vl.batch_id=".$batchId;
+ $rResult = $db->rawQuery($query);
  
  $excel = new PHPExcel();
  $output = array();
  $sheet = $excel->getActiveSheet();
- 
- $headings = array("Facility Name","Facility Code","Country","State","Hub Name","Batch Code","Sample Code","Unique ART No","Patient's Name","DOB","Age in years","Age in months","Other Id","Gender","Phone Number","Sample Collected On","Sample Type","Treatment Period","Treatment Initiated On","Current Regimen","Regiment Initiated On","Treatment Details","Patient Is Pregnant","ARC No","Patient Is Breastfeeding","ARV Adherence","Routine Monitoring Last VL Date","Routine Monitoring VL Value","Routine Monitoring Sample Type","VL Test After Suspected treatment failure adherence counseling VL Date","VL Test After Suspected treatment failure adherence counseling VL Value","VL Test After Suspected treatment failure adherence counseling Sample Type","Suspect Treatment Failure VL Date","Suspect Treatment Failure VL Value","Suspect Treatment Failure Sample Type","Clinician Name","Clinician Phone No","Request Date","VL Focal Person","VL Focal Person Phone Number","Email For HF","Lab Name","Lab Contact Person","Lab Phone No.","Sample Received Date","Sample Testing Date","Dispatched Date","Reviewed By","Reviewed Date","Justification","Comments","Log Value","Absolute Value","Text Value","Result","Status");
- 
+ if(isset($_POST['encValue']) && trim($_POST['encValue'])=='yes' && trim($_POST['pass'])!=""){
+  $headings = array("Batch Code","Facility Name","Facility Code","State","Hub Name","Country","Unique ART No","Sample Code","Other Id","Patient's Name","DOB","Age in years","Age in months","Gender","Phone Number","Sample Collected On","Sample Type","Treatment Period","Treatment Initiated On","Current Regimen","Regiment Initiated On","Treatment Details","Patient Is Pregnant","ARC No","Patient Is Breastfeeding","ARV Adherence","Routine Monitoring Last VL Date","Routine Monitoring VL Value","Routine Monitoring Sample Type","VL Test After Suspected treatment failure adherence counseling VL Date","VL Test After Suspected treatment failure adherence counseling VL Value","VL Test After Suspected treatment failure adherence counseling Sample Type","Suspect Treatment Failure VL Date","Suspect Treatment Failure VL Value","Suspect Treatment Failure Sample Type","Clinician Name","Clinician Phone No","Request Date","VL Focal Person","VL Focal Person Phone Number","Email For HF","Status");
+ }else{
+  $headings = array("Batch Code","Facility Name","Facility Code","State","Hub Name","Country","Unique ART No","Sample Code","Other Id","Sample Collected On","Sample Type","Treatment Period","Treatment Initiated On","Current Regimen","Regiment Initiated On","Treatment Details","Patient Is Pregnant","ARC No","Patient Is Breastfeeding","ARV Adherence","Routine Monitoring Last VL Date","Routine Monitoring VL Value","Routine Monitoring Sample Type","VL Test After Suspected treatment failure adherence counseling VL Date","VL Test After Suspected treatment failure adherence counseling VL Value","VL Test After Suspected treatment failure adherence counseling Sample Type","Suspect Treatment Failure VL Date","Suspect Treatment Failure VL Value","Suspect Treatment Failure Sample Type","Clinician Name","Clinician Phone No","Request Date","VL Focal Person","VL Focal Person Phone Number","Email For HF","Status");
+ }
+
  $colNo = 0;
  
  $styleArray = array(
@@ -44,16 +50,16 @@ if(isset($_SESSION['vlResultQuery']) && trim($_SESSION['vlResultQuery'])!=""){
          ),
      )
  );
- 
+ //if(isset($_POST['encValue']) && trim($_POST['encValue'])=='yes' && trim($_POST['pass'])!=""){
+
+ //}
  foreach ($headings as $field => $value) {
-  
   $sheet->getCellByColumnAndRow($colNo, 1)->setValueExplicit(html_entity_decode($value), PHPExcel_Cell_DataType::TYPE_STRING);
+  $cellName = $sheet->getCellByColumnAndRow($colNo,1)->getColumn();
+  $sheet->getStyle($cellName.'1')->applyFromArray($styleArray);
   $colNo++;
-  
  }
- $sheet->getStyle('A1:BD1')->applyFromArray($styleArray);
- 
- 
+ //$sheet->getStyle('A1:BD1')->applyFromArray($styleArray);
  
  foreach ($rResult as $aRow) {
   $row = array();
@@ -105,48 +111,24 @@ if(isset($_SESSION['vlResultQuery']) && trim($_SESSION['vlResultQuery'])!=""){
    $aRow['request_date']='';
   }
   
-  if(isset($aRow['date_sample_received_at_testing_lab']) && trim($aRow['date_sample_received_at_testing_lab'])!='' && $aRow['date_sample_received_at_testing_lab']!='0000-00-00'){
-   $aRow['date_sample_received_at_testing_lab']=$general->humanDateFormat($aRow['date_sample_received_at_testing_lab']);
-  }else{
-   $aRow['date_sample_received_at_testing_lab']='';
-  }
-  
-  if(isset($aRow['lab_tested_date']) && trim($aRow['lab_tested_date'])!='' && $aRow['lab_tested_date']!='0000-00-00'){
-   $aRow['lab_tested_date']=$general->humanDateFormat($aRow['lab_tested_date']);
-  }else{
-   $aRow['lab_tested_date']='';
-  }
-  
-  if(isset($aRow['date_results_dispatched']) && trim($aRow['date_results_dispatched'])!='' && $aRow['date_results_dispatched']!='0000-00-00'){
-   $aRow['date_results_dispatched']=$general->humanDateFormat($aRow['date_results_dispatched']);
-  }else{
-   $aRow['date_results_dispatched']='';
-  }
-  
-  if(isset($aRow['result_reviewed_date']) && trim($aRow['result_reviewed_date'])!='' && $aRow['result_reviewed_date']!='0000-00-00'){
-   $aRow['result_reviewed_date']=$general->humanDateFormat($aRow['result_reviewed_date']);
-  }else{
-   $aRow['result_reviewed_date']='';
-  }
-  
+  $row[] = $aRow['batch_code'];
   $row[] = ucwords($aRow['facility_name']);
   $row[] = $aRow['facility_code'];
-  $row[] = $aRow['country'];
   $row[] = $aRow['state'];
   $row[] = $aRow['hub_name'];
-  $row[] = $aRow['batch_code'];
-  $row[] = $aRow['sample_code'];
-  
-  
+  $row[] = $aRow['country'];
   $row[] = $aRow['art_no'];
-  $row[] = ucwords($aRow['patient_name']);
-  $row[] = $aRow['patient_dob'];
-  $row[] = $aRow['age_in_yrs'];
-  $row[] = $aRow['age_in_mnts'];
+  $row[] = $aRow['sample_code'];
   $row[] = $aRow['other_id'];
-  $row[] = $aRow['gender'];
-  
-  $row[] = $aRow['patient_phone_number'];
+  //Product patient info
+  if(isset($_POST['encValue']) && trim($_POST['encValue'])=='yes' && trim($_POST['pass'])!=""){
+   $row[] = ucwords($aRow['patient_name']);
+   $row[] = $aRow['patient_dob'];
+   $row[] = $aRow['age_in_yrs'];
+   $row[] = $aRow['age_in_mnts'];
+   $row[] = $aRow['gender'];
+   $row[] = $aRow['patient_phone_number'];
+  }
   $row[] = $aRow['sample_collection_date'];
   $row[] = ucwords($aRow['sample_name']);
   $row[] = $aRow['treatment_initiation'];
@@ -158,7 +140,6 @@ if(isset($_SESSION['vlResultQuery']) && trim($_SESSION['vlResultQuery'])!=""){
   $row[] = $aRow['arc_no'];
   $row[] = $aRow['is_patient_breastfeeding'];
   $row[] = $aRow['arv_adherence'];
-  
   
   $row[] = $aRow['routine_monitoring_last_vl_date'];
   $row[] = $aRow['routine_monitoring_value'];
@@ -178,24 +159,6 @@ if(isset($_SESSION['vlResultQuery']) && trim($_SESSION['vlResultQuery'])!=""){
   $row[] = $aRow['vl_focal_person'];
   $row[] = $aRow['focal_person_phone_number'];
   $row[] = $aRow['email_for_HF'];
-  
-  $row[] = $aRow['lab_name'];
-  $row[] = $aRow['lab_contact_person'];
-  $row[] = $aRow['lab_phone_no'];
-  $row[] = $aRow['date_sample_received_at_testing_lab'];
-  $row[] = $aRow['lab_tested_date'];
-  $row[] = $aRow['date_results_dispatched'];
-  
-  $row[] = $aRow['result_reviewed_by'];
-  $row[] = $aRow['result_reviewed_date'];
-  $row[] = $aRow['justification'];
-  $row[] = $aRow['comments'];
-  $row[] = $aRow['log_value'];
-  $row[] = $aRow['absolute_value'];
-  $row[] = $aRow['text_value'];
-  
-  $row[] = ucwords($aRow['result']); 
-  
   $row[] = ucwords($aRow['status_name']);
   
   $output[] = $row;
@@ -214,9 +177,25 @@ if(isset($_SESSION['vlResultQuery']) && trim($_SESSION['vlResultQuery'])!=""){
     $colNo++;
   }
  }
+ 
+ //$excel->getSecurity()->setLockWindows(true);
+ //$excel->getSecurity()->setLockStructure(true);
+ //$excel->getSecurity()->setWorkbookPassword('secret');
+ //
+ //
+ //$excel->getActiveSheet()->getProtection()->setSort(true);
+ //$excel->getActiveSheet()->getProtection()->setInsertRows(true);
+ //$excel->getActiveSheet()->getProtection()->setFormatCells(true);
+ //
+ //$excel->getActiveSheet()->getProtection()->setPassword('password');
+ //$excel->getActiveSheet()->getProtection()->setSheet(true);
+ 
  $writer = PHPExcel_IOFactory::createWriter($excel, 'Excel5');
- $filename = 'vl-result-' . date('d-M-Y-H-i-s') . '.xls';
+ $filename = 'vl-request-' . date('d-M-Y-H-i-s') . '.xls';
  $writer->save("./temporary". DIRECTORY_SEPARATOR . $filename);
+ 
+ $workbook=$excel->load($filename);
+ $workbook->getActiveSheet()->getSecurity()->setWorkbookPassword("test");
  echo $filename;
  
 }
