4c3da2170a1aa239d53d409d8379cdc4b7bb2708
Saravanan A.S. <saravanan@deforay.com>
2016-08-26 19:03:03 +0530

    aded reviewd by in vl form.

--- editVlRequest.php
+++ editVlRequest.php
@@ -8,6 +8,16 @@ $id=base64_decode($_GET['id']);
 $fQuery="SELECT vl.*,f.facility_name,f.facility_code,f.hub_name,f.state,f.district from vl_request_form as vl INNER JOIN facility_details as f ON vl.facility_id=f.facility_id where treament_id=$id";
 $result=$db->query($fQuery);
 
+$uQuery = "Select * from user_details where user_id=".$result[0]['result_reviewed_by'];
+$uResult=$db->query($uQuery);
+if($uResult){
+ $uName = $uResult[0]['user_name'];
+ $uId = $uResult[0]['user_id'];
+}else{
+ $uName = $_SESSION['userId'];
+ $uId = $_SESSION['userName'];
+}
+
 if(isset($result[0]['patient_dob']) && trim($result[0]['patient_dob'])!='' && $result[0]['patient_dob']!='0000-00-00'){
  $result[0]['patient_dob']=$general->humanDateFormat($result[0]['patient_dob']);
 }else{
@@ -960,14 +970,15 @@ $tsResult = $db->rawQuery($tsQuery);
                 </div>
                 
                 <div class="row">
-                 <!--<div class="col-md-6">
+                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="reviewedBy" class="col-lg-4 control-label">Reviewed By</label>
                         <div class="col-lg-7">
-                        <input type="text" class="form-control" id="reviewedBy" name="reviewedBy" placeholder="Enter Reviewed By Name" title="Please enter reviewed by name" value="< ?php echo $result[0]['result_reviewed_by']; ?>"/>
+                        <input type="hidden" class="form-control" id="reviewedBy" name="reviewedBy" readonly="readonly" placeholder="Enter Reviewed By Name" title="Please enter reviewed by name" value="<?php echo $uId; ?>"/>
+                        <input type="text" class="form-control readonly" placeholder="Enter Reviewed By Name" readonly="readonly" value="<?php echo $uName; ?>"/>
                         </div>
                     </div>
-                  </div>-->
+                  </div>
                  <div class="col-md-6">
                     <div class="form-group">
                         <label for="" class="col-lg-4 control-label">Reviewed Date</label>
