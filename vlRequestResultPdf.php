2cff16fcd02b7783df8297ef49e3a03dfef2ee0f
Amit Dugar <amit@deforay.com>
2016-09-16 17:10:15 +0530

    Minor textual and UI changes

--- vlRequestResultPdf.php
+++ vlRequestResultPdf.php
@@ -145,9 +145,9 @@ $vlResult = '';
   
 $smileyContent = '';  
 if(isset($arr['show_smiley']) && trim($arr['show_smiley']) == "yes"){
-  if(isset($result[0]['absolute_decimal_value']) && trim($result[0]['absolute_decimal_value'])!= '' && trim($result[0]['absolute_decimal_value']) > 1000){
+  if(isset($result[0]['absolute_value']) && trim($result[0]['absolute_value'])!= '' && trim($result[0]['absolute_value']) > 1000){
     $smileyContent = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="assets/img/smiley_frown.png" alt="frown_face"/>';
-  }else if(isset($result[0]['absolute_decimal_value']) && trim($result[0]['absolute_decimal_value'])!= '' && trim($result[0]['absolute_decimal_value']) <= 1000){
+  }else if(isset($result[0]['absolute_value']) && trim($result[0]['absolute_value'])!= '' && trim($result[0]['absolute_value']) <= 1000){
     $smileyContent = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="assets/img/smiley_smile.png" alt="smile_face"/>';
   }
 }
