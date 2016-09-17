d2c78524c9173622830fd60bbc1ef5bf4b588cb4
Saravanan A.S. <saravanan@deforay.com>
2016-09-17 13:49:08 +0530

    removed un approval page permission.

--- header.php
+++ header.php
@@ -8,7 +8,7 @@ if(!isset($_SESSION['userId'])){
 
 $link = $_SERVER['PHP_SELF'];
 $link_array = explode('/',$link);
-if(end($link_array)!='error.php'){
+if(end($link_array)!='error.php' && end($link_array)!='vlResultUnApproval.php'){
   if(isset($_SESSION['privileges']) && !in_array(end($link_array), $_SESSION['privileges'])){
     header("location:error.php");
   }
