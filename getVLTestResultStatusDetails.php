cb4954ae1c86bedfd7c5dff7bd628223cc34160f
Palpandian V <palpandian@deforay.com>
2016-09-20 13:35:26 +0530

    Added the vlteststatus page.

--- getVLTestResultStatusDetails.php
+++ getVLTestResultStatusDetails.php
@@ -2,13 +2,6 @@
 session_start();
 include('./includes/MysqliDb.php');
 include('General.php');
-$formConfigQuery ="SELECT * from global_config where name='vl_form'";
-$configResult=$db->query($formConfigQuery);
-$arr = array();
-// now we create an associative array so that we can easily create view variables
-for ($i = 0; $i < sizeof($configResult); $i++) {
-  $arr[$configResult[$i]['name']] = $configResult[$i]['value'];
-}
 $general=new Deforay_Commons_General();
 $tableName="vl_request_form";
 $primaryKey="treament_id";
@@ -16,8 +9,8 @@ $primaryKey="treament_id";
         /* Array of database columns which should be read and sent back to DataTables. Use a space where
          * you want to insert a non-database field (for example a counter or static image)
         */
-        $aColumns = array('vl.serial_no',"DATE_FORMAT(vl.sample_collection_date,'%d-%b-%Y')",'b.batch_code','vl.art_no','vl.patient_name','f.facility_name','f.state','f.district','s.sample_name','vl.absolute_value','vl.log_value','vl.text_value','ts.status_name');
-        $orderColumns = array('vl.serial_no','vl.sample_collection_date','b.batch_code','vl.art_no','vl.patient_name','f.facility_name','f.state','f.district','s.sample_name','vl.result','ts.status_name');
+        $aColumns = array('vl.serial_no',"DATE_FORMAT(vl.sample_collection_date,'%d-%b-%Y')",'b.batch_code','vl.art_no','vl.patient_name','f.facility_name','f.state','f.district','s.sample_name','vl.absolute_value','vl.log_value','vl.text_value');
+        $orderColumns = array('vl.serial_no','vl.sample_collection_date','b.batch_code','vl.art_no','vl.patient_name','f.facility_name','f.state','f.district','s.sample_name','vl.result');
         
         /* Indexed column (used for fast and accurate table cardinality) */
         $sIndexColumn = $primaryKey;
@@ -94,11 +87,7 @@ $primaryKey="treament_id";
          * SQL queries
          * Get data to display
         */
-	$aWhere = '';
-        //$sQuery="SELECT vl.treament_id,vl.facility_id,vl.patient_name,f.facility_name,f.facility_code,art.art_code,s.sample_name FROM vl_request_form as vl INNER JOIN facility_details as f ON vl.facility_id=f.facility_id  INNER JOIN r_art_code_details as art ON vl.current_regimen=art.art_id INNER JOIN r_sample_type as s ON s.sample_id=vl.sample_id";
-	$sQuery="SELECT * FROM vl_request_form as vl LEFT JOIN facility_details as f ON vl.facility_id=f.facility_id LEFT JOIN r_sample_type as s ON s.sample_id=vl.sample_id INNER JOIN testing_status as ts ON ts.status_id=vl.status LEFT JOIN r_art_code_details as art ON vl.current_regimen=art.art_id LEFT JOIN batch_details as b ON b.batch_id=vl.batch_id";
-	
-        //echo $sQuery;die;
+	$sQuery="SELECT * FROM vl_request_form as vl INNER JOIN testing_status as ts ON ts.status_id=vl.status LEFT JOIN facility_details as f ON vl.facility_id=f.facility_id LEFT JOIN r_sample_type as s ON s.sample_id=vl.sample_id LEFT JOIN r_art_code_details as art ON vl.current_regimen=art.art_id LEFT JOIN batch_details as b ON b.batch_id=vl.batch_id";
 	$start_date = '';
 	$end_date = '';
 	if(isset($_POST['sampleCollectionDate']) && trim($_POST['sampleCollectionDate'])!= ''){
@@ -110,7 +99,7 @@ $primaryKey="treament_id";
 	     $end_date = $general->dateFormat(trim($s_c_date[1]));
 	   }
 	}
-	  
+	
 	if (isset($sWhere) && $sWhere != "") {
            $sWhere=' where '.$sWhere;
 	    //$sQuery = $sQuery.' '.$sWhere;
@@ -170,11 +159,13 @@ $primaryKey="treament_id";
 		}
 	    }
 	}
-	if($sWhere!=''){
-	    $sWhere = $sWhere.' AND vl.form_id="'.$arr['vl_form'].'"';
+	
+        if (isset($sWhere) && $sWhere != "") {
+	    $sWhere = $sWhere.' AND vl.status = "'.$_POST['status'].'"';
 	}else{
-	    $sWhere = $sWhere.' where vl.form_id="'.$arr['vl_form'].'"';
+	    $sWhere =' WHERE vl.status = "'.$_POST['status'].'"';
 	}
+	
 	$sQuery = $sQuery.' '.$sWhere;
 	$sQuery = $sQuery." ORDER BY vl.modified_on DESC";
         if (isset($sOrder) && $sOrder != "") {
@@ -185,14 +176,14 @@ $primaryKey="treament_id";
         if (isset($sLimit) && isset($sOffset)) {
             $sQuery = $sQuery.' LIMIT '.$sOffset.','. $sLimit;
         }
-        $_SESSION['vlRequestSearchResultQuery'] = $sQuery;
+        
         $rResult = $db->rawQuery($sQuery);
         /* Data set length after filtering */
-        $aResultFilterTotal =$db->rawQuery("SELECT vl.treament_id,vl.facility_id,vl.patient_name,vl.result,f.facility_name,f.facility_code,vl.art_no,s.sample_name,b.batch_code,vl.batch_id,ts.status_name FROM vl_request_form as vl LEFT JOIN facility_details as f ON vl.facility_id=f.facility_id  LEFT JOIN r_sample_type as s ON s.sample_id=vl.sample_id INNER JOIN testing_status as ts ON ts.status_id=vl.status LEFT JOIN batch_details as b ON b.batch_id=vl.batch_id $sWhere  ORDER BY vl.modified_on DESC, $sOrder");
+        $aResultFilterTotal =$db->rawQuery("SELECT vl.treament_id,vl.facility_id,vl.patient_name,vl.result,f.facility_name,f.facility_code,vl.art_no,s.sample_name,b.batch_code,vl.batch_id,ts.status_name FROM vl_request_form as vl INNER JOIN testing_status as ts ON ts.status_id=vl.status LEFT JOIN facility_details as f ON vl.facility_id=f.facility_id  LEFT JOIN r_sample_type as s ON s.sample_id=vl.sample_id LEFT JOIN batch_details as b ON b.batch_id=vl.batch_id $sWhere  ORDER BY vl.modified_on DESC, $sOrder");
         $iFilteredTotal = count($aResultFilterTotal);
 
         /* Total data set length */
-        $aResultTotal =  $db->rawQuery("select COUNT(treament_id) as total FROM vl_request_form where form_id='".$arr['vl_form']."'");
+        $aResultTotal =  $db->rawQuery("select COUNT(treament_id) as total FROM vl_request_form where status = ".$_POST['status']);
        // $aResultTotal = $countResult->fetch_row();
        //print_r($aResultTotal);
         $iTotal = $aResultTotal[0]['total'];
@@ -206,18 +197,9 @@ $primaryKey="treament_id";
             "iTotalDisplayRecords" => $iFilteredTotal,
             "aaData" => array()
         );
-	$vlRequest = false;
-	$vlView = false;
-	if(isset($_SESSION['privileges']) && (in_array("editVlRequest.php", $_SESSION['privileges']))){
-	    $vlRequest = true;
-	}
-	if(isset($_SESSION['privileges']) && (in_array("viewVlRequest.php", $_SESSION['privileges']))){
-	    $vlView = true;
-	}
         
         foreach ($rResult as $aRow) {
 	    $vlResult='';
-	    $edit='';
 	    if(isset($aRow['sample_collection_date']) && trim($aRow['sample_collection_date'])!= '' && $aRow['sample_collection_date']!= '0000-00-00 00:00:00'){
 	       $xplodDate = explode(" ",$aRow['sample_collection_date']);
 	       $aRow['sample_collection_date'] = $general->humanDateFormat($xplodDate[0]);
@@ -225,7 +207,6 @@ $primaryKey="treament_id";
 	       $aRow['sample_collection_date'] = '';
 	    }
             $row = array();
-	    //$row[]='<input type="checkbox" name="chk[]" class="checkTests" id="chk' . $aRow['treament_id'] . '"  value="' . $aRow['treament_id'] . '" onclick="toggleTest(this);"  />';
 	    $row[] = $aRow['serial_no'];
 	    $row[] = $aRow['sample_collection_date'];
 	    $row[] = $aRow['batch_code'];
@@ -236,43 +217,13 @@ $primaryKey="treament_id";
 	    $row[] = ucwords($aRow['district']);
             $row[] = ucwords($aRow['sample_name']);
 	    if(trim($aRow['absolute_value'])!=''){
-		    $vlResult=$aRow['absolute_value'];
+		$vlResult=$aRow['absolute_value'];
 	    }else if(trim($aRow['log_value'])!=''){
-		    $vlResult=$aRow['log_value'];
+		$vlResult=$aRow['log_value'];
 	    }else if(trim($aRow['text_value'])!=''){
-		    $vlResult=$aRow['text_value'];
+		$vlResult=$aRow['text_value'];
 	    }
             $row[] = ucwords($vlResult);
-			
-            $row[] = ucwords($aRow['status_name']);
-	    //$printBarcode='<a href="javascript:void(0);" class="btn btn-info btn-xs" style="margin-right: 2px;" title="View" onclick="printBarcode(\''.base64_encode($aRow['treament_id']).'\');"><i class="fa fa-barcode"> Print Barcode</i></a>';
-	    //$enterResult='<a href="javascript:void(0);" class="btn btn-success btn-xs" style="margin-right: 2px;" title="Result" onclick="showModal(\'updateVlResult.php?id=' . base64_encode($aRow['treament_id']) . '\',900,520);"> Result</a>';
-		if($aRow['form_id']==2){
-			if($vlRequest){
-				$edit='<a href="editVlRequest.php?id=' . base64_encode($aRow['treament_id']) . '" class="btn btn-primary btn-xs" style="margin-right: 2px;" title="Edit"><i class="fa fa-pencil"> Edit</i></a>';
-			}
-			$pdf = '<a href="javascript:void(0);" class="btn btn-success btn-xs" style="margin-right: 2px;" title="View" onclick="convertZmbPdf('.$aRow['treament_id'].');"><i class="fa fa-file-text"> PDF</i></a>';
-			if($vlView){
-			    $view = '<a href="viewVlRequestZm.php?id=' . base64_encode($aRow['treament_id']) . '" class="btn btn-default btn-xs" style="margin-right: 2px;" title="View"><i class="fa fa-eye"> View</i></a>';
-			}
-		}else{
-			if($vlRequest){
-			    $edit='<a href="editVlRequest.php?id=' . base64_encode($aRow['treament_id']) . '" class="btn btn-primary btn-xs" style="margin-right: 2px;" title="Edit"><i class="fa fa-pencil"> Edit</i></a>';
-			}
-			$pdf = '<a href="javascript:void(0);" class="btn btn-success btn-xs" style="margin-right: 2px;" title="View" onclick="convertPdf('.$aRow['treament_id'].');"><i class="fa fa-file-text"> PDF</i></a>';
-			if($vlView){
-			    $view = '<a href="viewVlRequest.php?id=' . base64_encode($aRow['treament_id']) . '" class="btn btn-default btn-xs" style="margin-right: 2px;" title="View"><i class="fa fa-eye"> View</i></a>';
-			}
-		}
-		
-		
-	    if($vlView){
-			$row[] = $edit;//.$pdf.$view;
-	    }else if($vlRequest || $editVlRequestZm){
-		$row[] = $edit;//.$pdf;
-	    }else if($vlView){
-		$row[] = "";//$pdf.$view;
-	    }
             $output['aaData'][] = $row;
         }
         
