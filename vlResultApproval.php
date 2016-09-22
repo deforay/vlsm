71564fda0ac6e0ad536667d43ee7f96f33afeb06
Saravanan A.S. <saravanan@deforay.com>
2016-09-22 11:53:11 +0530

    removed heading from vlresult approval apge.

--- vlResultApproval.php
+++ vlResultApproval.php
@@ -14,7 +14,7 @@ $batResult = $db->rawQuery($batQuery);
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <h1>Imported Results Approval</h1>
+      <h1>Results Approval</h1>
       <ol class="breadcrumb">
         <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active">Test Request</li>
