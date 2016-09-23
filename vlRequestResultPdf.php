92cc372cbf24af1d91fdd533c7c5b81ca1fbcf5d
Palpandian V <palpandian@deforay.com>
2016-09-23 15:43:34 +0530

    Made some changes in result pdf.

--- vlRequestResultPdf.php
+++ vlRequestResultPdf.php
@@ -96,7 +96,7 @@ for ($i = 0; $i < sizeof($configResult); $i++) {
   $arr[$configResult[$i]['name']] = $configResult[$i]['value'];
 }
 $id=$_POST['id'];
-$fQuery="SELECT vl.treament_id,vl.sample_code,vl.serial_no,vl.patient_name,vl.patient_name,vl.surname,vl.patient_dob,vl.age_in_yrs,vl.age_in_mnts,vl.art_no,vl.gender,vl.patient_receive_sms,vl.patient_phone_number,vl.sample_collection_date,vl.clinician_ph_no,vl.sample_testing_date,vl.date_sample_received_at_testing_lab,vl.lab_name,vl.lab_contact_person,vl.lab_phone_no,vl.lab_tested_date,vl.lab_no,vl.log_value,vl.absolute_value,vl.text_value,vl.result,vl.comments,vl.result_reviewed_by,vl.last_viral_load_result,vl.last_viral_load_date,vl.result_reviewed_date,vl.result_approved_by,f.facility_name,l_f.facility_name as labName,f.facility_code,f.state,f.district,s.sample_name FROM vl_request_form as vl LEFT JOIN r_sample_type as s ON s.sample_id=vl.sample_id LEFT JOIN facility_details as f ON vl.facility_id=f.facility_id LEFT JOIN facility_details as l_f ON vl.lab_id=l_f.facility_id WHERE treament_id=$id";
+$fQuery="SELECT vl.treament_id,vl.sample_code,vl.serial_no,vl.patient_name,vl.patient_name,vl.surname,vl.patient_dob,vl.age_in_yrs,vl.age_in_mnts,vl.art_no,vl.gender,vl.patient_receive_sms,vl.patient_phone_number,vl.sample_collection_date,vl.clinician_ph_no,vl.sample_testing_date,vl.date_sample_received_at_testing_lab,vl.lab_name,vl.lab_contact_person,vl.lab_phone_no,vl.lab_tested_date,vl.lab_no,vl.log_value,vl.absolute_value,vl.text_value,vl.result,vl.comments,vl.result_reviewed_by,vl.last_viral_load_result,vl.last_viral_load_date,vl.result_reviewed_date,vl.result_approved_by,vl.vl_test_platform,f.facility_name,l_f.facility_name as labName,f.facility_code,f.state,f.district,s.sample_name FROM vl_request_form as vl LEFT JOIN r_sample_type as s ON s.sample_id=vl.sample_id LEFT JOIN facility_details as f ON vl.facility_id=f.facility_id LEFT JOIN facility_details as l_f ON vl.lab_id=l_f.facility_id WHERE treament_id=$id";
 $result=$db->query($fQuery);
 if(!isset($result[0]['facility_code']) || trim($result[0]['facility_code']) == ''){
   $result[0]['facility_code'] = '';
@@ -168,17 +168,21 @@ if(isset($result[0]['result_approved_by']) && trim($result[0]['result_approved_b
 $vlResult = '';
 $smileyContent = '';
 $showMessage = '';
-$fontBig = '12px';
+$tndMessage = '';
+$resultTextSize = '12px';
+$messageTextSize = '12px';
 if($result[0]['result']!= NULL && trim($result[0]['result'])!= '') {
   if(strtolower(trim($result[0]['result'])) == "target not detected"){
     $vlResult = 'TND*';
     $smileyContent = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="assets/img/smiley_smile.png" alt="smile_face"/>';
-    $showMessage = 'TND* - Target not Detectable';
+    $showMessage = 'Viral load adequately controlled : continue current regimen';
+    $tndMessage = 'TND* - Target not Detectable';
+    $resultTextSize = '18px';
   }else if(trim($result[0]['result']) > 1000){
     $vlResult = $result[0]['result'];
     $smileyContent = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="assets/img/smiley_frown.png" alt="frown_face"/>';
-    $showMessage = 'High Viral Load - need assessment for enhanced adherence or clinical assessment';
-    $fontBig = '18px';
+    $showMessage = 'High Viral Load - need assessment for enhanced adherence or clinical assessment for possible switch to second line.';
+    $messageTextSize = '18px';
   }else if(trim($result[0]['result']) <= 1000){
     $vlResult = $result[0]['result'];
     $smileyContent = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="assets/img/smiley_smile.png" alt="smile_face"/>';
@@ -191,7 +195,7 @@ if(isset($arr['show_smiley']) && trim($arr['show_smiley']) == "no"){
 
 $html = '';
 $html .= '<div style="">';
-$html.='<table style="padding:2px;">';
+  $html.='<table style="padding:2px;">';
     if(isset($arr['logo']) && trim($arr['logo'])!= '' && file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . "logo" . DIRECTORY_SEPARATOR . $arr['logo'])){
       $html .='<tr>';
         $html .='<td colspan="4" style="text-align:center;"><img src="uploads/logo/'.$arr['logo'].'" style="width:80px;height:80px;" alt="logo"></td>';
@@ -294,13 +298,15 @@ $html.='<table style="padding:2px;">';
         $html .='</tr>';
         $html .='<tr>';
           $html .='<td style="line-height:22px;font-size:12px;font-weight:bold;text-align:left;">Specimen type</td>';
-          $html .='<td colspan="3" style="line-height:22px;font-size:12px;text-align:left;">'.ucwords($result[0]['sample_name']).'</td>';
+          $html .='<td style="line-height:22px;font-size:12px;text-align:left;">'.ucwords($result[0]['sample_name']).'</td>';
+          $html .='<td style="line-height:22px;font-size:12px;font-weight:bold;text-align:left;">Testing Platform</td>';
+          $html .='<td style="line-height:22px;font-size:12px;text-align:left;">'.ucwords($result[0]['vl_test_platform']).'</td>';
         $html .='</tr>';
         $html .='<tr>';
           $html .='<td colspan="4" style="line-height:22px;font-size:13px;font-weight:bold;text-align:left;">Result of viral load(copies/ml)</td>';
         $html .='</tr>';
         $html .='<tr>';
-          $html .='<td colspan="4" style="line-height:22px;font-size:12px;text-align:left;">'.$vlResult.'</td>';
+          $html .='<td colspan="4" style="line-height:22px;font-size:'.$resultTextSize.';text-align:left;">'.$vlResult.'</td>';
         $html .='</tr>';
         $html .='<tr>';
           $html .='<td style="line-height:22px;font-size:13px;font-weight:bold;text-align:left;">Reviewed by</td>';
@@ -310,7 +316,18 @@ $html.='<table style="padding:2px;">';
         $html .='</tr>';
         if(trim($showMessage)!= ''){
           $html .='<tr>';
-            $html .='<td colspan="4" style="line-height:22px;font-size:'.$fontBig.';text-align:left;">'.$showMessage.'</td>';
+            $html .='<td colspan="4" style="line-height:22px;font-size:'.$messageTextSize.';text-align:left;">'.$showMessage.'</td>';
+          $html .='</tr>';
+          $html .='<tr>';
+            $html .='<td colspan="4" style="line-height:4px;"></td>';
+          $html .='</tr>';
+        }
+        if(trim($tndMessage)!= ''){
+          $html .='<tr>';
+            $html .='<td colspan="4" style="line-height:22px;font-size:12px;text-align:left;">'.$tndMessage.'</td>';
+          $html .='</tr>';
+          $html .='<tr>';
+            $html .='<td colspan="4" style="line-height:6px;"></td>';
           $html .='</tr>';
         }
         $html .='<tr>';
@@ -335,25 +352,28 @@ $html.='<table style="padding:2px;">';
      $html .='<td colspan="2" style="font-size:10px;text-align:left;">'.$result[0]['last_viral_load_result'].'</td>';
     $html .='</tr>';
     $html .='<tr>';
-     $html .='<td colspan="4" style="line-height:40px;border-bottom:1px solid #333;"></td>';
+     $html .='<td colspan="4" style="line-height:68px;border-bottom:1px solid #333;"></td>';
     $html .='</tr>';
     $html .='<tr>';
       $html .='<td colspan="4">';
        $html .='<table>';
         $html .='<tr>';
-          $html .='<td style="font-size:10px;text-align:left;width:60%;"><img src="assets/img/smiley_smile.png" alt="smile_face" style="width:14px;height:14px;"/> = VL < = 1000 copies/ml: Continue on current regimen</td>';
+          $html .='<td colspan="2" style="line-height:4px;"></td>';
+        $html .='</tr>';
+        $html .='<tr>';
+          $html .='<td style="font-size:10px;text-align:left;width:60%;"><img src="assets/img/smiley_smile.png" alt="smile_face" style="width:10px;height:10px;"/> = VL < = 1000 copies/ml: Continue on current regimen</td>';
           $html .='<td style="font-size:10px;text-align:left;">Print date '.$printDate.'&nbsp;&nbsp;&nbsp;&nbsp;time '.$printDateTime.'</td>';
         $html .='</tr>';
         $html .='<tr>';
-          $html .='<td colspan="2" style="line-height:10px;"></td>';
+          $html .='<td colspan="2" style="line-height:6px;"></td>';
         $html .='</tr>';
         $html .='<tr>';
-          $html .='<td colspan="2" style="font-size:10px;text-align:left;width:60%;"><img src="assets/img/smiley_frown.png" alt="frown_face" style="width:14px;height:14px;"/> = VL > 1000 copies/ml: copies/ml: Clinical and counselling action required</td>';
+          $html .='<td colspan="2" style="font-size:10px;text-align:left;width:60%;"><img src="assets/img/smiley_frown.png" alt="frown_face" style="width:10px;height:10px;"/> = VL > 1000 copies/ml: copies/ml: Clinical and counselling action required</td>';
         $html .='</tr>';
        $html .='</table>';
       $html .='</td>';
     $html .='</tr>';
-$html.='</table>';
+  $html.='</table>';
 $html .= "</div>";
 $pdf->writeHTML($html);
 $pdf->lastPage();
