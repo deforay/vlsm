347f799191a3e5d3b84f1d0c48fc8e303f570532
Palpandian V <palpandian@deforay.com>
2016-10-03 16:49:11 +0530

    Made small UI changes in drc add/edit form.

--- header.php
+++ header.php
@@ -62,11 +62,10 @@ $formConfigResult=$db->query($formConfigQuery);
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="content-language" content="en,fr">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
   <title>VL Lab Request</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
