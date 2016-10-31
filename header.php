375f95bd95237b860a9ed1bd8a9de6e27fedae82
Ilahir Ali <ilahir@deforay.com>
2016-10-31 20:08:45 +0530

    Made small changes on header.

--- header.php
+++ header.php
@@ -209,7 +209,7 @@ $formConfigResult=$db->query($formConfigQuery);
 		<?php }  if(isset($_SESSION['privileges']) && in_array("otherConfig.php", $_SESSION['privileges'])){ ?>
 		<li class="allMenu otherConfigMenu"><a href="otherConfig.php"><i class="fa fa-circle-o"></i> Email/SMS Configuration</a></li>
 		<?php } ?>
-        <li class="allMenu vlMailMenu"><a href="vlMail.php"><i class="fa fa-circle-o"></i> Mail</a></li>
+        <li class="allMenu vlMailMenu"><a href="vlMail.php"><i class="fa fa-circle-o"></i> Request E-mail</a></li>
 	      </ul>
 	    </li>
 	<?php }
