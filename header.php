b8e1c47c6c98b81d92e7f80499796f81f68cf593
Amit Dugar <amit@deforay.com>
2016-11-28 22:32:24 +0530

    Fixed addVlRequestDrc.php encoding issue

--- header.php
+++ header.php
@@ -64,7 +64,7 @@ $formConfigResult=$db->query($formConfigQuery);
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
-  <!--<meta charset="utf-8" />   -->
+  <meta charset="utf-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
   <title>VL Lab Request</title>
   <!-- Tell the browser to be responsive to screen width -->
