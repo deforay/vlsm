cae092b00f714161a989d2e7e6b98cc23ecded80
Palpandian V <palpandian@deforay.com>
2016-12-28 19:17:35 +0530

    Changed the download vl form process.

--- login.php
+++ login.php
@@ -21,17 +21,16 @@ $configResult=$db->query($globalConfigQuery);
   
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <link href="assets/css/deforayModal.css" rel="stylesheet" />
   <!-- iCheck -->
-
     <style>
         body{
-            
             background: #F6F6F6;       
             background: #000;       
             
             background : url("assets/img/bg.jpg") center;
             background-size: cover;            
-            background-repeat:no-repeat;    
+            background-repeat:no-repeat;
         }
     </style> 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -41,7 +40,6 @@ $configResult=$db->query($globalConfigQuery);
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
   <script type="text/javascript" src="assets/js/jquery.min.2.0.2.js"></script>
-
 </head>
 <body class="">
     <div class="container-fluid">
@@ -54,41 +52,47 @@ $configResult=$db->query($globalConfigQuery);
         <?php } ?>
         <div id="loginbox" style="margin-top:15px;margin-bottom:70px;float:right;margin-right:-15px;" class="mainbox col-md-3 col-sm-8 ">
             <div class="panel panel-default" style="opacity: 0.9;">
-                    <div class="panel-heading">
-                        <div class="panel-title">Viral Load Sample Management</div>
-                        
-                    </div>     
-
-                    <div style="padding-top:10px;" class="panel-body" >
-
-                        <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-                            
-                        <form id="loginForm" name="loginForm" class="form-horizontal" role="form" method="post" action="loginProcess.php" onsubmit="validateNow();return false;">
-                                    
-                            <div style="margin-bottom: 5px" class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                <input id="login-username" type="text" class="form-control isRequired" name="username" value="" placeholder="User Name" title="Please enter the user name">
-                            </div>
-                                
-                            <div style="margin-bottom: 5px" class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                <input id="login-password" type="password" class="form-control isRequired" name="password" placeholder="Password" title="Please enter the password">
-                            </div>
-
-                            <div style="margin-top:10px" class="form-group">
-                                <!-- Button -->
-                                <div class="col-sm-12 controls">
-                                    <button class="btn btn-lg btn-success btn-block" onclick="validateNow();return false;">Login</button>
-                                </div>
-                            </div>
-                            </form>
-                        </div>                     
-                    </div>  
+                <div class="panel-heading">
+                    <div class="panel-title">Viral Load Sample Management</div>
+                    
+                </div>     
+    
+                <div style="padding-top:10px;" class="panel-body" >
+                    <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+                    <form id="loginForm" name="loginForm" class="form-horizontal" role="form" method="post" action="loginProcess.php" onsubmit="validateNow();return false;">
+                         <div style="margin-bottom: 5px" class="input-group">
+                             <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                             <input id="login-username" type="text" class="form-control isRequired" name="username" value="" placeholder="User Name" title="Please enter the user name">
+                         </div>
+                             
+                         <div style="margin-bottom: 5px" class="input-group">
+                             <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                             <input id="login-password" type="password" class="form-control isRequired" name="password" placeholder="Password" title="Please enter the password">
+                         </div>
+     
+                         <div style="margin-top:10px" class="form-group">
+                             <!-- Button -->
+                             <div class="col-sm-12 controls">
+                                 <button class="btn btn-lg btn-success btn-block" onclick="validateNow();return false;">Login</button>
+                             </div>
+                         </div>
+                     </form>
+                </div>                     
+            </div>  
         </div>
     </div>
+    <div style="opacity: 0.9;bottom:10vh;padding-left:1%;width:100%;position:fixed;">
+        <a id="download-form" href="#" style="color:#fff;text-decoration:underline;"><h3>Download VL Form</h3></a>
+        <select id="country" name="country" class="form-control" style="width:14%;display:none;">
+            <option value=""> -- Select Country -- </option>
+            <option value="3">DRC</option>
+            <option value="4">Zambia</option>
+        </select>
+        <a id="download" href="#" style="color:#fff;text-decoration:underline;display:none;"><h4>Clik here to download</h4></a>
+    </div>
     <script src="assets/js/deforayValidation.js"></script>
+    <script src="assets/js/jquery.blockUI.js"></script>
     <script type="text/javascript">
-
     function validateNow(){
       flag = deforayValidator.init({
           formId: 'loginForm'
@@ -99,16 +103,59 @@ $configResult=$db->query($globalConfigQuery);
       }
     }
     $(document).ready(function(){
-    <?php
-    if(isset($_SESSION['alertMsg']) && trim($_SESSION['alertMsg'])!=""){
-    ?>
-    alert('<?php echo $_SESSION['alertMsg']; ?>');
-    <?php
-    $_SESSION['alertMsg']='';
-    unset($_SESSION['alertMsg']);
-    }
-    ?>
+        <?php
+        if(isset($_SESSION['alertMsg']) && trim($_SESSION['alertMsg'])!=""){
+        ?>
+        alert('<?php echo $_SESSION['alertMsg']; ?>');
+        <?php
+        $_SESSION['alertMsg']='';
+        unset($_SESSION['alertMsg']);
+        }
+        ?>
     });
+    
+    $('#download-form').click(function(){
+      $('#download-form').hide(400);
+      $('#country').show(400);
+    });
+    
+    $('#country').change(function(){
+      if($('#country').val() != ''){
+         $('#download').show(400);
+         if($('#country').val() == 3){
+            $('#download').attr('onclick','downloadVLForm("drc")');
+         }else if($('#country').val() == 4){
+            $('#download').attr('onclick','downloadVLForm("zambia")');
+         }else{
+            $('#download').removeAttr('onclick');
+         }
+      }else{
+        $('#download').hide(400);
+        $('#download').removeAttr('onclick');
+      }
+    });
+    
+    function downloadVLForm(country){
+        $.blockUI();
+        var downloadURL = '#';
+        if(country == 'drc'){
+          downloadURL = '../includes/downloadDRCForm.php';
+        }else if(country == 'zambia'){
+          downloadURL = '../includes/downloadZambiaForm.php';
+        }
+        
+        $.post(downloadURL, { },
+          function(data){
+              if(data == "" || data == null || data == undefined){
+                  $.unblockUI();
+                  alert('Unable to generate download');
+              }else{
+                  $.unblockUI();
+                  window.open('../uploads/'+data,'_blank');
+              }
+              
+          });
+    }
     </script>
 </body>
 </html>
