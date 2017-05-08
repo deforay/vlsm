4e12d416240188a0dfb7dee089cd86d8cc9ccb81
Palpandian V <palpandian@deforay.com>
2017-05-08 15:03:52 +0530

    Made changes on queries rwd add/edit/update

--- alter.sql
+++ alter.sql
@@ -1050,3 +1050,11 @@ INSERT INTO `r_sample_rejection_reasons` (`rejection_reason_id`, `rejection_reas
 INSERT INTO `r_sample_rejection_reasons` (`rejection_reason_id`, `rejection_reason_name`, `rejection_type`, `rejection_reason_status`) VALUES (NULL, 'DBS cards with insufficient blood spots', 'dbs', 'active'), (NULL, 'DBS card with clotting present in spots', 'dbs', 'active');
 
 INSERT INTO `r_sample_rejection_reasons` (`rejection_reason_id`, `rejection_reason_name`, `rejection_type`, `rejection_reason_status`) VALUES (NULL, 'DBS cards that have serum rings indicating contamination around spots', 'dbs', 'active'), (NULL, 'VL Mechine Flag', 'testing', 'active');
+
+
+--Pal 08-may-2017
+ALTER TABLE `r_sample_status` ADD `status` VARCHAR(45) NOT NULL DEFAULT 'active' AFTER `status_name`;
+
+ALTER TABLE `r_art_code_details` ADD `art_status` VARCHAR(45) NOT NULL DEFAULT 'active' AFTER `nation_identifier`;
+
+DELETE FROM `r_sample_rejection_reasons` WHERE `r_sample_rejection_reasons`.`rejection_reason_id` = 16"
