27f6b0a97936a8cead824b3de3bf32e9f3ea257a
Ilahir Rahman Ali <ilahir@deforay.com>
2017-05-23 14:26:15 +0530

    added import config machines in vl_lab_request

--- import-configs/editImportConfigHelper.php
+++ import-configs/editImportConfigHelper.php
@@ -4,6 +4,8 @@ ob_start();
 include('../includes/MysqliDb.php');
 
 $tableName="import_config";
+$importMachineTable="import_config_machines";
+
 $configId=(int) base64_decode($_POST['configId']);
 try {
     if(trim($_POST['configurationName'])!=""){
@@ -21,13 +23,24 @@ try {
     //print_r($importConfigData);die;
     $db=$db->where('config_id',$configId);
     //print_r($vldata);die;
-    $db->update($tableName,$importConfigData);     
-    
-        $_SESSION['alertMsg']="Import config details updated successfully";
-    
+    $db->update($tableName,$importConfigData);
+    if(count($_POST['configMachineName'])>0){
+        for($c = 0;$c<count($_POST['configMachineName']);$c++){
+            if(trim($_POST['configMachineName'][$c])!=''){
+                if(isset($_POST['configMachineId'][$c]) && $_POST['configMachineId'][$c]!=''){
+                    $configMachineData = array('config_machine_name'=>$_POST['configMachineName'][$c]);
+                    $db=$db->where('config_machine_id',$_POST['configMachineId'][$c]);
+                    $db->update($importMachineTable,$configMachineData);
+                }else{
+                    $configMachineData = array('config_id'=>$configId,'config_machine_name'=>$_POST['configMachineName'][$c]);
+                    $db->insert($importMachineTable,$configMachineData);
+                }
+            }
+        }
+    }
+        $_SESSION['alertMsg']="Import config details updated successfully";    
         $configDir = __DIR__.DIRECTORY_SEPARATOR.'import-configs';
         $configFile = $configDir.DIRECTORY_SEPARATOR.$_POST['configurationFile'];
-        
         
         if (!file_exists($configDir)) {
             mkdir($configDir, 0777, true);
@@ -38,8 +51,6 @@ try {
             fwrite($fp, '');
             fclose($fp);
         }    
-    
-    
     }
     header("location:importConfig.php");
   
