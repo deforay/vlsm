40182a6ef338940b31c7f0699b4a967eaf7ee5ad
Palpandian V <palpandian@deforay.com>
2017-05-27 12:38:41 +0530

    Minor changes on qr import.

--- facilities/editFacilityHelper.php
+++ facilities/editFacilityHelper.php
@@ -12,12 +12,11 @@ try {
             $strSearch = $_POST['state'];
             $facilityQuery="SELECT * from province_details where province_name='".$strSearch."'";
             $facilityInfo=$db->query($facilityQuery);
-            if($facilityInfo){
-            }else{
-            $data=array(
-              'province_name'=>$_POST['state'],
-            );
-            $result=$db->insert($tableName1,$data);
+            if(!$facilityInfo || count($facilityInfo) == 0){
+		$data=array(
+		  'province_name'=>$_POST['state'],
+		);
+		$result=$db->insert($tableName1,$data);
             }
         }
 	$email = '';
@@ -26,31 +25,31 @@ try {
 	    for($i=0;$i<count($expEmail);$i++){
 		$reportEmail = filter_var($expEmail[$i], FILTER_VALIDATE_EMAIL);
 		if($reportEmail!=''){
-		if($email!=''){
-		$email.= ",".$reportEmail;
-		}else{
-		$email.= $reportEmail;    
-		}
+		    if($email!=''){
+		      $email.= ",".$reportEmail;
+		    }else{
+		      $email.= $reportEmail;    
+		    }
 		}
 	    }
 	}
         $data=array(
-			'facility_name'=>$_POST['facilityName'],
-			'facility_code'=>$_POST['facilityCode'],
-			'other_id'=>$_POST['otherId'],
-			'facility_mobile_numbers'=>$_POST['phoneNo'],
-			'address'=>$_POST['address'],
-			'country'=>$_POST['country'],
-			'facility_state'=>$_POST['state'],
-			'facility_district'=>$_POST['district'],
-			'facility_hub_name'=>$_POST['hubName'],
-			'latitude'=>$_POST['latitude'],
-			'longitude'=>$_POST['longitude'],
-			'facility_emails'=>$_POST['email'],
-			'report_email'=>$email,
-			'contact_person'=>$_POST['contactPerson'],
-			'facility_type'=>$_POST['facilityType'],
-			'status'=>$_POST['status']
+	    'facility_name'=>$_POST['facilityName'],
+	    'facility_code'=>$_POST['facilityCode'],
+	    'other_id'=>$_POST['otherId'],
+	    'facility_mobile_numbers'=>$_POST['phoneNo'],
+	    'address'=>$_POST['address'],
+	    'country'=>$_POST['country'],
+	    'facility_state'=>$_POST['state'],
+	    'facility_district'=>$_POST['district'],
+	    'facility_hub_name'=>$_POST['hubName'],
+	    'latitude'=>$_POST['latitude'],
+	    'longitude'=>$_POST['longitude'],
+	    'facility_emails'=>$_POST['email'],
+	    'report_email'=>$email,
+	    'contact_person'=>$_POST['contactPerson'],
+	    'facility_type'=>$_POST['facilityType'],
+	    'status'=>$_POST['status']
         );
         //print_r($data);die;
         $db=$db->where('facility_id',$facilityId);
