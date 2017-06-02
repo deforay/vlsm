5227e36872141bb85fe242b7a3d76f2c5aafb9d9
Palpandian V <palpandian@deforay.com>
2017-06-02 17:16:48 +0530

    Added global config check for qr section.

--- qr-code/generate.php
+++ qr-code/generate.php
@@ -8,7 +8,7 @@ include('../header.php');
       <h1><i class="fa fa-qrcode"></i> Generate QR Code</h1>
       <ol class="breadcrumb">
         <li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">QR Code</li>
+        <li class="active">Generate QR Code</li>
       </ol>
     </section>
      <!-- Main content -->
@@ -16,24 +16,6 @@ include('../header.php');
       <div class="row">
         <div class="col-xs-12">
           <div class="box">
-	    <span style="display: none;position:absolute;z-index: 9999 !important;color:#000;padding:5px;margin-left: 325px;" id="showhide" class="">
-	      <div class="row" style="background:#e0e0e0;padding: 15px;">
-		  <div class="col-md-12" >
-			  <div class="col-md-4">
-				<input type="checkbox" onclick="javascript:fnShowHide(this.value);" value="0" id="iCol0" data-showhide="batch_code" class="showhideCheckBox" /> <label for="iCol0">Batch Code</label>
-			  </div>
-			  <div class="col-md-4">
-				<input type="checkbox" onclick="javascript:fnShowHide(this.value);" value="1" id="iCol1" data-showhide="''" class="showhideCheckBox" /> <label for="iCol1">No. Of Samples</label>
-			  </div>
-			  <div class="col-md-4">
-				<input type="checkbox" onclick="javascript:fnShowHide(this.value);" value="2" id="iCol2" data-showhide="request_created_datetime" class="showhideCheckBox"  /> <label for="iCol2">Created On</label>
-			  </div>
-			  <div class="col-md-4">
-				<input type="checkbox" onclick="javascript:fnShowHide(this.value);" value="3" id="iCol3" data-showhide="batch_status" class="showhideCheckBox"  /> <label for="iCol3">Status</label> <br>
-			  </div>
-		      </div>
-		  </div>
-	      </span>
             <!-- /.box-header -->
             <div class="box-body">
               <table id="generateQRCodeDataTable" class="table table-bordered table-striped">
