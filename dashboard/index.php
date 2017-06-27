fa17b5137e7b89cc28778be199fec5c03bb314be
Ilahir Rahman Ali <ilahir@deforay.com>
2017-06-27 18:03:22 +0530

    dashboard piechart changes done

--- dashboard/index.php
+++ dashboard/index.php
@@ -115,7 +115,7 @@ include('../header.php');
     $('#sampleCollectionDate').daterangepicker({
             format: 'DD-MMM-YYYY',
 	    separator: ' to ',
-            startDate: moment().subtract('days', 30),
+            startDate: moment().subtract('days', 6),
             endDate: moment(),
             maxDate: moment(),
             ranges: {
