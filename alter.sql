0657c03d601789697eab6ffde7db62e026a59a4c
Palpandian V <palpandian@deforay.com>
2017-08-07 15:35:06 +0530

    updated the alter.sql

--- alter.sql
+++ alter.sql
@@ -1125,3 +1125,8 @@ ALTER TABLE  `vl_request_form` ADD  `sample_code_title` VARCHAR( 45 ) NOT NULL D
 --saravanan 11-July-2017
 ALTER TABLE `vl_request_form` ADD `request_mail_datetime` DATETIME NULL DEFAULT NULL AFTER `is_request_mail_sent`;
 ALTER TABLE `vl_request_form` ADD `result_mail_datetime` DATETIME NULL DEFAULT NULL AFTER `is_result_mail_sent`;
+
+--Pal 07-Aug-2017
+ALTER TABLE `vl_request_form` ADD `request_exported_datetime` DATETIME NULL DEFAULT NULL AFTER `test_result_import`, ADD `request_imported_datetime` DATETIME NULL DEFAULT NULL AFTER `request_exported_datetime`, ADD `result_exported_datetime` DATETIME NULL DEFAULT NULL AFTER `request_imported_datetime`, ADD `result_imported_datetime` DATETIME NULL DEFAULT NULL AFTER `result_exported_datetime`;
+
+ALTER TABLE `temp_sample_report` ADD `request_exported_datetime` DATETIME NULL DEFAULT NULL AFTER `import_machine_name`, ADD `request_imported_datetime` DATETIME NULL DEFAULT NULL AFTER `request_exported_datetime`, ADD `result_exported_datetime` DATETIME NULL DEFAULT NULL AFTER `request_imported_datetime`, ADD `result_imported_datetime` DATETIME NULL DEFAULT NULL AFTER `result_exported_datetime`;
