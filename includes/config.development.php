61c08aa0cbd982ec7543f7ea933d92b50f173d56
Amit Dugar <amit@deforay.com>
2017-08-23 20:38:29 +0530

    Minor changes in Patient Search

--- includes/config.development.php
+++ includes/config.development.php
@@ -5,7 +5,7 @@ require_once('config.production.php');
 $HOST = '127.0.0.1';
 $USER = 'root';
 $PASSWORD = 'zaq12345';
-$DBNAME = 'vl_lab_request_rwanda_new';
+$DBNAME = 'vl_lab_request';
 $PORT = 3306;
 
 // Uniform Server
