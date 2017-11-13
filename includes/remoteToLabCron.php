98698d93297c4715218f5795f3ea955aa09e4956
Saravanan A.S. <saravanan@deforay.com>
2017-11-13 13:40:16 +0530

    minor changes done in remote and lab cron

--- includes/remoteToLabCron.php
+++ includes/remoteToLabCron.php
@@ -1,5 +1,7 @@
 <?php
 include('MysqliDb.php');
+include('General.php');
+$general=new Deforay_Commons_General();
 //global config
 $cQuery="SELECT * FROM global_config";
 $cResult=$db->query($cQuery);
@@ -20,7 +22,7 @@ $start_date = date('Y-01-01');
     $start_date = date('Y-01-01');
   }
 //get remote data
-$vlQuery="SELECT * FROM vl_request_form WHERE `last_modified_datetime` > SUBDATE( NOW(), INTERVAL ". $arr['data_sync_interval']." HOUR)";
+$vlQuery="SELECT * FROM vl_request_form WHERE data_sync = 0 AND last_modified_datetime > SUBDATE( NOW(), INTERVAL ". $arr['data_sync_interval']." HOUR)";
 $vlRemoteResult = $syncdb->rawQuery($vlQuery);
 $allColumns = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS where TABLE_SCHEMA = '$sDBNAME' AND table_name='vl_request_form'";
 $allColResult = $syncdb->rawQuery($allColumns);
@@ -31,22 +33,26 @@ foreach($vlRemoteResult as $key=>$remoteData){
         $lab[$result] = $remoteData[$result];
     }
     //remove result value
-    $removeKeys = array('vl_sample_id','result_value_log','result_value_absolute','result_value_absolute_decimal','result_value_text','result_value_result','request_created_by');
+    $removeKeys = array('vl_sample_id','result_value_log','result_value_absolute','result_value_absolute_decimal','result_value_text','result_value_result');
     foreach($removeKeys as $keys){
         unset($lab[$keys]);
     }
     //check wheather sample code empty or not
-    if($lab['sample_code']!=''){
-        $sQuery = "Select vl_sample_id from vl_request_form where sample_code=".$lab['sample_code'];
+    if($lab['sample_code']!='' && $lab['sample_code']!=0 && $lab['sample_code']!=NULL){
+        $sQuery = "Select vl_sample_id from vl_request_form where sample_code='".$lab['sample_code']."'";
         $sResult = $db->rawQuery($sQuery);
-        unset($lab['last_modified_datetime']);
+        $lab['data_sync'] = 1;//column data sync value is 1 equal to data sync done.value 0 is not done.
+        $lab['last_modified_datetime'] = $general->getDateTime();
         $db=$db->where('vl_sample_id',$sResult[0]['vl_sample_id']);
-        $id = $db->insert('vl_request_form',$lab);
+        $id = $db->update('vl_request_form',$lab);
+        //update in lab database        
+        $db = $syncdb->where('sample_code',$lab['sample_code']);
+        $id = $syncdb->update('vl_request_form',array('data_sync'=>1));
     }else{
         $svlQuery='SELECT sample_code_key FROM vl_request_form as vl WHERE DATE(vl.request_created_datetime) >= "'.$start_date.'" AND DATE(vl.request_created_datetime) <= "'.$end_date.'" ORDER BY vl_sample_id DESC LIMIT 1';
         $svlResult=$db->query($svlQuery);
         $prefix = $arr['sample_code_prefix'];
-        if($svlResult[0]['sample_code_key']!='' && $svlResult[0]['sample_code_key']!=NULL){
+        if(isset($svlResult[0]['sample_code_key']) && $svlResult[0]['sample_code_key']!='' && $svlResult[0]['sample_code_key']!=NULL){
          $maxId = $svlResult[0]['sample_code_key']+1;
          $strparam = strlen($maxId);
          $zeros = substr("000", $strparam);
@@ -55,8 +61,8 @@ foreach($vlRemoteResult as $key=>$remoteData){
          $maxId = '001';
         }
         if($arr['sample_code']=='auto'){
-            $lab['serial_no'] = date('ymd');
-            $lab['sample_code'] = date('ymd');
+            $lab['serial_no'] = date('ymd').$maxId;
+            $lab['sample_code'] = date('ymd').$maxId;
             $lab['sample_code_key'] = $maxId;
         }else if($arr['sample_code']=='YY' || $arr['sample_code']=='MMYY'){
             $lab['serial_no'] = $prefix.$mnthYr.$maxId;
@@ -64,8 +70,16 @@ foreach($vlRemoteResult as $key=>$remoteData){
             $lab['sample_code_format'] = $prefix.$mnthYr;
             $lab['sample_code_key'] =  $maxId;
         }
-        $lab['result_status'] = 6;
+        $lab['request_created_by'] = 0;
+        $lab['last_modified_by'] = 0;
+        $lab['request_created_datetime'] = $general->getDateTime();
+        $lab['last_modified_datetime'] = $general->getDateTime();
+        //$lab['result_status'] = 6;
+        $lab['data_sync'] = 1;//column data sync value is 1 equal to data sync done.value 0 is not done.
         $id = $db->insert('vl_request_form',$lab);
+        //update in lab database
+        $syncdb = $syncdb->where('remote_sample_code',$lab['remote_sample_code']);
+        $id = $syncdb->update('vl_request_form',array('data_sync'=>1));
     }
 }
 }
