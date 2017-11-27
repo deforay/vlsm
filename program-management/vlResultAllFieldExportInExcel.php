33e18392eb23343ba7e7d3f7f177782619ce9a58
Saravanan A.S. <saravanan@deforay.com>
2017-11-27 19:20:03 +0530

    added remote login image

--- program-management/vlResultAllFieldExportInExcel.php
+++ program-management/vlResultAllFieldExportInExcel.php
@@ -15,14 +15,14 @@ include ('../includes/PHPExcel.php');
  }
  $country = $arr['vl_form'];
  
- $sQuery="SELECT vl.*,s.sample_name,s.status as sample_type_status,ts.*,f.facility_name,l_f.facility_name as labName,f.facility_code,f.facility_state,f.facility_district,f.facility_mobile_numbers,f.address,f.facility_hub_name,f.contact_person,f.report_email,f.country,f.longitude,f.latitude,f.facility_type,f.status as facility_status,ft.facility_type_name,lft.facility_type_name as labFacilityTypeName,l_f.facility_name as labName,l_f.facility_code as labCode,l_f.facility_state as labState,l_f.facility_district as labDistrict,l_f.facility_mobile_numbers as labPhone,l_f.address as labAddress,l_f.facility_hub_name as labHub,l_f.contact_person as labContactPerson,l_f.report_email as labReportMail,l_f.country as labCountry,l_f.longitude as labLongitude,l_f.latitude as labLatitude,l_f.facility_type as labFacilityType,l_f.status as labFacilityStatus,tr.test_reason_name,tr.test_reason_status FROM vl_request_form as vl LEFT JOIN facility_details as f ON vl.facility_id=f.facility_id LEFT JOIN facility_details as l_f ON vl.lab_id=l_f.facility_id LEFT JOIN r_sample_type as s ON s.sample_id=vl.sample_type INNER JOIN r_sample_status as ts ON ts.status_id=vl.result_status LEFT JOIN r_vl_test_reasons as tr ON tr.test_reason_id=vl.reason_for_vl_testing LEFT JOIN facility_type as ft ON ft.facility_type_id=f.facility_type LEFT JOIN facility_type as lft ON lft.facility_type_id=l_f.facility_type WHERE vl.vlsm_country_id = $country";
+ $sQuery="SELECT vl.*,s.sample_name,s.status as sample_type_status,ts.*,f.facility_name,l_f.facility_name as labName,f.facility_code,f.facility_state,f.facility_district,f.facility_mobile_numbers,f.address,f.facility_hub_name,f.contact_person,f.report_email,f.country,f.longitude,f.latitude,f.facility_type,f.status as facility_status,ft.facility_type_name,lft.facility_type_name as labFacilityTypeName,l_f.facility_name as labName,l_f.facility_code as labCode,l_f.facility_state as labState,l_f.facility_district as labDistrict,l_f.facility_mobile_numbers as labPhone,l_f.address as labAddress,l_f.facility_hub_name as labHub,l_f.contact_person as labContactPerson,l_f.report_email as labReportMail,l_f.country as labCountry,l_f.longitude as labLongitude,l_f.latitude as labLatitude,l_f.facility_type as labFacilityType,l_f.status as labFacilityStatus,tr.test_reason_name,tr.test_reason_status,rsrr.rejection_reason_name,rsrr.rejection_reason_status FROM vl_request_form as vl LEFT JOIN facility_details as f ON vl.facility_id=f.facility_id LEFT JOIN facility_details as l_f ON vl.lab_id=l_f.facility_id LEFT JOIN r_sample_type as s ON s.sample_id=vl.sample_type INNER JOIN r_sample_status as ts ON ts.status_id=vl.result_status LEFT JOIN r_vl_test_reasons as tr ON tr.test_reason_id=vl.reason_for_vl_testing LEFT JOIN facility_type as ft ON ft.facility_type_id=f.facility_type LEFT JOIN facility_type as lft ON lft.facility_type_id=l_f.facility_type LEFT JOIN r_sample_rejection_reasons as rsrr ON rsrr.rejection_reason_id=vl.reason_for_sample_rejection WHERE vl.vlsm_country_id = $country";
   $rResult = $db->rawQuery($sQuery);
  
  $excel = new PHPExcel();
  $output = array();
  $sheet = $excel->getActiveSheet();
  
- $headings = array("Serial No.","Instance Id","Gender","Age In Years","Clinic Name","Clinic Code","Clinic State","Clinic District","Clinic Phone Number","Clinic Address","Clinic HUB Name","Clinic Contact Person","Clinic Report Mail","Clinic Country","Clinic Longitude","Clinic Latitude","Clinic Status","Clinic Type","Sample Type","Sample Type Status","Sample Collection Date","LAB Name","Lab Code","Lab State","Lab District","Lab Phone Number","Lab Address","Lab HUB Name","Lab Contact Person","Lab Report Mail","Lab Country","Lab Longitude","Lab Latitude","Lab Status","Lab Type","Lab Tested Date","Log Value","Absolute Value","Text Value","Absolute Decimal Value","Result","Testing Reason","Test Reason Status","Testing Status");
+ $headings = array("Serial No.","Instance Id","Gender","Age In Years","Clinic Name","Clinic Code","Clinic State","Clinic District","Clinic Phone Number","Clinic Address","Clinic HUB Name","Clinic Contact Person","Clinic Report Mail","Clinic Country","Clinic Longitude","Clinic Latitude","Clinic Status","Clinic Type","Sample Type","Sample Type Status","Sample Collection Date","LAB Name","Lab Code","Lab State","Lab District","Lab Phone Number","Lab Address","Lab HUB Name","Lab Contact Person","Lab Report Mail","Lab Country","Lab Longitude","Lab Latitude","Lab Status","Lab Type","Lab Tested Date","Log Value","Absolute Value","Text Value","Absolute Decimal Value","Result","Testing Reason","Test Reason Status","Testing Status","Sample Received Datetime","Line Of Treatment","Sample Rejected","Rejection Reason Name","Rejection Reason Status","Pregnant","Breast Feeding","Art Code","Regimen Initiated Date","ARV Adherance Percentage","Is Adherance poor","Approved Datetime");
  
  $colNo = 0;
  
@@ -71,6 +71,10 @@ include ('../includes/PHPExcel.php');
   {
    $aRow['sample_collection_date'] = '';
   }
+  if($aRow['sample_received_at_vl_lab_datetime']=='0000-00-00 00:00:00')
+  {
+   $aRow['sample_received_at_vl_lab_datetime'] = '';
+  }
   $row[] = $aRow['serial_no'];
   $row[] = $aRow['vlsm_instance_id'];
   $row[] = ucwords(str_replace("_"," ",$aRow['patient_gender']));
@@ -115,6 +119,18 @@ include ('../includes/PHPExcel.php');
   $row[] = ucwords($aRow['test_reason_name']);
   $row[] = ucwords($aRow['test_reason_status']);
   $row[] = ucwords($aRow['status_name']);
+  $row[] = $aRow['sample_received_at_vl_lab_datetime'];
+  $row[] = $aRow['line_of_treatment'];
+  $row[] = $aRow['is_sample_rejected'];
+  $row[] = $aRow['rejection_reason_name'];
+  $row[] = $aRow['rejection_reason_status'];
+  $row[] = $aRow['is_patient_pregnant'];
+  $row[] = $aRow['is_patient_breastfeeding'];
+  $row[] = $aRow['current_regimen'];
+  $row[] = $aRow['date_of_initiation_of_current_regimen'];
+  $row[] = $aRow['arv_adherance_percentage'];
+  $row[] = $aRow['is_adherance_poor'];
+  $row[] = $aRow['result_approved_datetime'];
   $output[] = $row;
  }
 
