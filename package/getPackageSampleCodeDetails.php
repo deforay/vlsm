ea87527becab5d881f1023d13af586e02722f3d3
Saravanan A.S. <saravanan@deforay.com>
2018-01-10 13:11:38 +0530

    minor changes done in package sample code detals page

--- package/getPackageSampleCodeDetails.php
+++ package/getPackageSampleCodeDetails.php
@@ -25,7 +25,7 @@ $query="SELECT vl.sample_code,vl.remote_sample_code,vl.vl_sample_id FROM vl_requ
 $query = $query." ORDER BY vl.request_created_datetime ASC";
 $result = $db->rawQuery($query);
 if($sarr['user_type']=='remoteuser'){
-  $sCode = 'remote_sample_code';
+  $sCode = 'sample_code';
 }else if($sarr['user_type']=='vluser' || $sarr['user_type']=='standalone'){
   $sCode = 'sample_code';
 }
