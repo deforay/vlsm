3b2f95e444ff1a58e7cfed0e2f26496022ad1f59
Saravanan A.S. <saravanan@deforay.com>
2018-02-07 19:54:02 +0530

    minor chnages done in editvlrequesthelper page

--- vl-request/editVlRequestHelper.php
+++ vl-request/editVlRequestHelper.php
@@ -217,7 +217,6 @@ try {
           'result_dispatched_datetime'=>$_POST['resultDispatchedOn'],
           'is_sample_rejected'=>(isset($_POST['noResult']) && $_POST['noResult']!='') ? $_POST['noResult'] :  NULL,
           'reason_for_sample_rejection'=>(isset($_POST['rejectionReason']) && $_POST['rejectionReason']!='') ? $_POST['rejectionReason'] :  NULL,
-          'result_value_log'=>NULL,
           'result_value_absolute'=>(isset($_POST['vlResult']) && $_POST['vlResult']!='' && ($_POST['vlResult']!='Target Not Detected' && $_POST['vlResult']!='Below Detection Level')) ? $_POST['vlResult'] :  NULL,
           'result_value_text'=>NULL,
           'result_value_absolute_decimal'=>(isset($_POST['vlResult']) && $_POST['vlResult']!='' && ($_POST['vlResult']!='Target Not Detected' && $_POST['vlResult']!='Below Detection Level')) ? number_format((float)$_POST['vlResult'], 2, '.', '') :  NULL,
