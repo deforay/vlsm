620c2243313e834a35737ac484a111c3e3ee7c18
Saravanan A.S. <saravanan@deforay.com>
2018-02-20 19:21:00 +0530

    added api for remote folder

---
+++ remote/updateRemoteData.php
@@ -0,0 +1,77 @@
+<?php
+//this fille is get the data from lab db and update in remote db
+include(dirname(__FILE__) . "/../includes/MysqliDb.php");
+$url = 'http://vlsm-rwanda.deforay.in/remote/getFacilityData.php';
+$data = array(
+    "Key"=>"vlsm-lab-Data--",
+);
+//open connection
+$ch = curl_init($url);
+$json_data = json_encode($data);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($json_data))
+);
+// execute post
+$curl_response = curl_exec($ch);
+//close connection
+curl_close($ch);
+$result = json_decode($curl_response, true);
+//system config
+$systemConfigQuery ="SELECT * from system_config";
+$systemConfigResult=$db->query($systemConfigQuery);
+$sarr = array();
+// now we create an associative array so that we can easily create view variables
+for ($i = 0; $i < sizeof($systemConfigResult); $i++) {
+  $sarr[$systemConfigResult[$i]['name']] = $systemConfigResult[$i]['value'];
+}
+//global config
+$cQuery="SELECT * FROM global_config";
+$cResult=$db->query($cQuery);
+$arr = array();
+// now we create an associative array so that we can easily create view variables
+for ($i = 0; $i < sizeof($cResult); $i++) {
+  $arr[$cResult[$i]['name']] = $cResult[$i]['value'];
+}
+//get facility map id
+if(count($result)>0){
+  $fMapResult = implode(",",$result);
+}else{
+  $fMapResult = "''";
+}
+//get remote data
+if(trim($sarr['lab_name'])==''){
+  $sarr['lab_name'] = "''";
+}
+$vlQuery="SELECT * FROM vl_request_form WHERE data_sync=0 AND (lab_id =".$sarr['lab_name']." OR facility_id IN(".$fMapResult.")) AND `last_modified_datetime` > SUBDATE( NOW(), INTERVAL ". $arr['data_sync_interval']." HOUR)";
+$vlLabResult = $db->rawQuery($vlQuery);
+$url = 'http://vlsm-rwanda.deforay.in/remote/updateRemoteDetails.php';
+$data = array(
+    "result"=>$vlLabResult,
+    "Key"=>"vlsm-lab-Data--",
+);
+//open connection
+$ch = curl_init($url);
+$json_data = json_encode($data);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($json_data))
+);
+// execute post
+$curl_response = curl_exec($ch);
+//close connection
+curl_close($ch);
+$result = json_decode($curl_response, true);
+if(count($result)>0){
+    foreach($result as $code){
+        $db = $db->where('sample_code',$code);
+        $id = $db->update('vl_request_form',array('data_sync'=>1));
+    }
+}
+?>
---
+++ remote/updateRemoteDetails.php
@@ -0,0 +1,39 @@
+<?php
+//this file is receive lab data value and update in remote db
+$data = json_decode(file_get_contents('php://input'), true);
+include(dirname(__FILE__) . "/../includes/MysqliDb.php");
+include(dirname(__FILE__) . "/../General.php");
+$general=new Deforay_Commons_General();
+
+$allColumns = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS where TABLE_SCHEMA = '$sDBNAME' AND table_name='vl_request_form'";
+$allColResult = $db->rawQuery($allColumns);
+$oneDimensionalArray = array_map('current', $allColResult);
+$sampleCode = array();
+if(count($data['result'])>0){
+foreach($data['result'] as $key=>$remoteData){
+    foreach($oneDimensionalArray as $result){
+      $lab[$result] = $remoteData[$result];
+    }
+    //remove result value
+    $removeKeys = array('vl_sample_id');
+    foreach($removeKeys as $keys){
+        unset($lab[$keys]);
+    }
+    //check wheather sample code empty or not
+    if($lab['remote_sample_code']!=''){
+		$sQuery = "Select vl_sample_id,sample_code,remote_sample_code,remote_sample_code_key from vl_request_form where remote_sample_code='".$lab['remote_sample_code']."'";
+        $sResult = $db->rawQuery($sQuery);
+        if($sResult){
+            $lab['data_sync'] = 1;//column data sync value is 1 equal to data sync done.value 0 is not done.
+            $lab['last_modified_datetime'] = $general->getDateTime();
+            $lab['remote_sample_code'] = $sResult[0]['remote_sample_code'];
+            $lab['remote_sample_code_key'] = $sResult[0]['remote_sample_code_key'];
+            unset($lab['request_created_by']);unset($lab['last_modified_by']);unset($lab['request_created_datetime']);unset($lab['sample_package_id']);
+            $db=$db->where('vl_sample_id',$sResult[0]['vl_sample_id']);
+            $id = $db->update('vl_request_form',$lab);
+            $sampleCode[] = $lab['sample_code'];
+		}
+    }
+}
+}
+echo json_encode($sampleCode);
