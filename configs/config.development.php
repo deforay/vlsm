65195dae7e0b6d05a0171810cfaea10ba876fd7d
Palpandian V <palpandian@deforay.com>
2018-03-20 19:58:58 +0530

    minor issue fixed in png add

--- configs/config.development.php
+++ configs/config.development.php
@@ -26,4 +26,4 @@ $sPORT = 3306;
 
 // Linux : default for Ubuntu 16.04, may be different for your distribution
 $MYSQLDUMP = '/usr/bin/mysqldump'; 
-$REMOTEURL = '';
+$REMOTEURL = 'http://vl-remote';
