e19fcf2f2df6183b6532e73e64bd73adfd629604
Amit Dugar <amit@deforay.com>
2018-06-12 12:12:48 +0530

    Removed mandatory check for 'Email for HF' field on Rwanda form

--- vl-print/updateVlTestResultRwd.php
+++ vl-print/updateVlTestResultRwd.php
@@ -510,9 +510,9 @@ $disable = "disabled = 'disabled'";
                             </div>
                         </div>
                         <div class="col-md-4">
-                            <label class="col-lg-5 control-label" for="emailHf">Email for HF <span class="mandatory">*</span></label>
+                            <label class="col-lg-5 control-label" for="emailHf">Email for HF </label>
                             <div class="col-lg-7">
-                                <input type="text" class="form-control isEmail isRequired" id="emailHf" name="emailHf" placeholder="Email for HF" title="Please enter email for hf" value="<?php echo $facilityResult[0]['facility_emails'];?>" <?php echo $disable;?>/>
+                                <input type="text" class="form-control isEmail" id="emailHf" name="emailHf" placeholder="Email for HF" title="Please enter email for hf" value="<?php echo $facilityResult[0]['facility_emails'];?>" <?php echo $disable;?>/>
                             </div>
                         </div>
                      </div>
