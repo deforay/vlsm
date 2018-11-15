334e2075388fc1a50f49a883154e2438046ef07c
Amit Dugar <amit@deforay.com>
2018-11-16 03:38:09 +0530

    Version 3.9.7

--- system/system.php
+++ system/system.php
@@ -1,4 +1,4 @@
 <?php
-define('VERSION', '3.9.6');
+define('VERSION', '3.9.7');
 define('UPLOAD_PATH',realpath(__DIR__.DIRECTORY_SEPARATOR.'/../uploads'));
 define('TEMP_PATH',realpath(__DIR__.DIRECTORY_SEPARATOR.'/../temporary'));
