fcb788e9df8a6308b7f18f96cf01109c99bfed28
Amit Dugar <amit@deforay.com>
2019-02-18 13:27:32 +0200

    Version 3.10.3

--- system/system.php
+++ system/system.php
@@ -1,4 +1,4 @@
 <?php
-define('VERSION', '3.10.2');
+define('VERSION', '3.10.3');
 define('UPLOAD_PATH',realpath(__DIR__.DIRECTORY_SEPARATOR.'/../uploads'));
 define('TEMP_PATH',realpath(__DIR__.DIRECTORY_SEPARATOR.'/../temporary'));
