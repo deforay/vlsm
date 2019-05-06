682bf783ddb050bbac18e9fee97804a2c42bd1b4
Amit Dugar <amit@deforay.com>
2019-05-06 15:53:14 +0530

    Batch Code Generation filters improved

---
+++ eid/requests/eid-add-request.php
@@ -0,0 +1,34 @@
+<?php
+ob_start();
+$title = "VLSM | EID | Add EID Request";
+include('../../startup.php');
+include_once(APPLICATION_PATH.'/header.php');
+include_once(APPLICATION_PATH.'/General.php');
+
+
+
+// if($sarr['user_type']=='remoteuser'){
+//     $labFieldDisabled = 'disabled="disabled"';
+//     $vlfmQuery="SELECT GROUP_CONCAT(DISTINCT vlfm.facility_id SEPARATOR ',') as facilityId FROM vl_user_facility_map as vlfm where vlfm.user_id='".$_SESSION['userId']."'";
+//     $vlfmResult = $db->rawQuery($vlfmQuery);
+// }
+
+
+if($arr['vl_form']==1){
+    include('eid-add-southsudan.php');
+}else if($arr['vl_form']==2){
+    include('eid-add-zimbabwe.php');
+}else if($arr['vl_form']==3){
+    include('eid-add-drc.php');
+}else if($arr['vl_form']==4){
+    include('eid-add-zambia.php');
+}else if($arr['vl_form']==5){
+    include('eid-add-png.php');
+}else if($arr['vl_form']==6){
+    include('eid-add-who.php');
+}else if($arr['vl_form']==7){
+    include('eid-add-rwanda.php');
+}else if($arr['vl_form']==8){
+    include('eid-add-angola.php');
+}
+?>
