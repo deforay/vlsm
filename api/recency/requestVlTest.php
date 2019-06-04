cbc1b27f79c992d6ce6f2786813864a59afd43fc
Thanaseelan <thanaseelan@deforay.com>
2019-06-04 21:40:45 +0530

    Few changes on pending results showing

--- api/recency/requestVlTest.php
+++ api/recency/requestVlTest.php
@@ -37,18 +37,22 @@ try{
         
         /* Prepare the values to insert */
         $data['remote_sample_code']= $result[0];
+        $data['sample_code']= $result[0];
         $data['patient_art_no']= $result[1];
         $data['facility_id']= $result[2];
         $data['patient_province']= $facilityResult[0]['facility_state'];
         $data['patient_district']= $facilityResult[0]['facility_district'];
         $data['sample_collection_date']= date('Y-m-d',strtotime($result[5]));
-        $data['sample_type']= $result[6];
+        // $data['sample_type']= $result[6];
+        $data['sample_type']= '2';
         $data['request_created_by']= $result[8];
         $data['lab_id']= $result[7];
         $data['sample_tested_datetime']= $general->getDateTime();
         $data['result_status']= '6';
         $data['data_sync']= '0';
         $data['recency_vl']= 'yes';
+        $data['vlsm_country_id']= $general->getGlobalConfig('vl_form');;
+        $data['result_status']= '9';
         
         /* Check if request data already placed or not */
         // $vlFormReqQuery ="SELECT vl_sample_id from vl_request_form WHERE remote_sample_code ='".$result[0]."' AND patient_art_no ='".$result[1]."' AND facility_id ='".$result[2]."' AND patient_province ='".$facilityResult[0]['facility_state']."' AND patient_district ='".$facilityResult[0]['facility_district']."' AND sample_collection_date ='".date('Y-m-d',strtotime($result[5]))."' AND sample_type ='".$result[6]."'";
