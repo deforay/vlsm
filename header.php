e577c0fd637c4339bc0a1657420b9d40fac46f92
Thanaseelan <thanaseelan@deforay.com>
2019-11-20 16:09:33 +0530

    Recency to VLSM login was created

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <?php
 
 include_once(APPLICATION_PATH . '/includes/MysqliDb.php');
+/* Crosss Login Block Start */
 $crossLoginQuery = "SELECT login_id,password,user_name FROM user_details WHERE user_id = '".$_SESSION['userId']."'";
 $crossLoginResult = $db->rawQueryOne($crossLoginQuery);
-
+/* Crosss Login Block End */
 $gQuery = "SELECT * FROM global_config";
 $gResult = $db->query($gQuery);
 $global = array();
@@ -266,11 +267,13 @@ $formConfigResult = $db->query($formConfigQuery);
         </ul>
         <div class="navbar-custom-menu">
           <ul class="nav navbar-nav">
+            <!-- Crosss Login Block Start -->
             <?php if($allowRecencyCrossLogin){?>
               <li class="user-menu">
-                <a target="_blank" href="<?php echo $recencyUrl.'login?user='.base64_encode($crossLoginResult['login_id']).'&token='.base64_encode($crossLoginResult['password']).'&name='.base64_encode($crossLoginResult['user_name']);?>" class="btn btn-link"><i class="fa fa-fw fa-external-link"></i> Recency</a>
+                <a target="_blank" href="<?php echo $recencyUrl.'login?u='.base64_encode($crossLoginResult['login_id']).'&t='.base64_encode($crossLoginResult['password']).'&name='.base64_encode($crossLoginResult['user_name']);?>" class="btn btn-link"><i class="fa fa-fw fa-external-link"></i> Recency</a>
               </li>
             <?php }?>
+            <!-- Crosss Login Block End -->
             <li class="dropdown user user-menu">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                 <img src="/assets/img/default-user.png" class="user-image" alt="User Image">
