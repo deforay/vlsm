0e0419c6820c06e67a051aba0e6dde0104f87886
Thanaseelan <thanaseelan@deforay.com>
2020-02-17 13:31:44 +0530

    EID model included

--- eid/requests/forms/edit-rwanda.php
+++ eid/requests/forms/edit-rwanda.php
@@ -4,6 +4,7 @@
 
 ob_start();
 
+
 //Funding source list
 $fundingSourceQry = "SELECT * FROM r_funding_sources WHERE funding_source_status='active' ORDER BY funding_source_name ASC";
 $fundingSourceList = $db->query($fundingSourceQry);
