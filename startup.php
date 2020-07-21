16602021f38a09c5d430733d430aa43e2b2f0377
Amit Dugar <amit@deforay.com>
2020-07-21 15:31:02 +0530

    Minor query issue fixed in vldashboard-eid.php

--- startup.php
+++ startup.php
@@ -6,30 +6,30 @@ if (session_status() == PHP_SESSION_NONE) {
 // chdir(dirname(__DIR__));
 
 
-if(php_sapi_name() !== 'cli'){
+if (php_sapi_name() !== 'cli') {
     // base directory
     $base_dir = __DIR__;
 
     $doc_root = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
-    
+
     // server protocol
     $protocol = empty($_SERVER['HTTPS']) ? 'http' : 'https';
-    
+
     // domain name
     $domain = $_SERVER['SERVER_NAME'];
-    
+
     // base url
     $base_url = preg_replace("!^${doc_root}!", '', $base_dir);
-    
+
     // server port
     $port = $_SERVER['SERVER_PORT'];
     $disp_port = ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
-    
+
     // put em all together to get the complete base URL
     $domain = "${protocol}://${domain}${disp_port}${base_url}";
-    
+
     defined('DOMAIN')
-        || define('DOMAIN', $domain); 
+        || define('DOMAIN', $domain);
 }
 
 
@@ -38,12 +38,11 @@ defined('APPLICATION_PATH')
 
 defined('UPLOAD_PATH')
     || define('UPLOAD_PATH', realpath(dirname(__FILE__) . '/uploads'));
-    
+
 defined('TEMP_PATH')
-    || define('TEMP_PATH', realpath(dirname(__FILE__) . '/temporary'));     
+    || define('TEMP_PATH', realpath(dirname(__FILE__) . '/temporary'));
 
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? 
-                                  getenv('APPLICATION_ENV') : 
-                                  'production'));    
-
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ?
+        getenv('APPLICATION_ENV') :
+        'production'));
