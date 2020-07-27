b4a387a415ea7af9b60629eef48f9ea374ddd16f
thanaseelan <thanaseelan@deforay.com>
2020-07-27 16:14:01 +0530

    Response sub options added to drc form

--- covid-19/requests/getResponseByParentId.php
+++ covid-19/requests/getResponseByParentId.php
@@ -4,16 +4,16 @@ include_once(APPLICATION_PATH . '/includes/MysqliDb.php');
 include_once(APPLICATION_PATH.'/models/General.php');
 $general = new General($db);
 $sampleData = array();
-$sampleQuery = 'SELECT * FROM r_covid19_symptoms where parent_symptom = "'. $_POST['symptomParent'].'"';
+$sampleQuery = 'SELECT * FROM r_covid19_test_reasons where parent_reason = "'. $_POST['responseParent'].'"';
 $sampleResult = $db->query($sampleQuery);
 if(count($sampleResult) > 0){
     $index = 1;
     foreach($sampleResult as $key=>$sampleRow){ 
-        echo '<tr class="symptomRow'.$_POST['symptomParent'].'" id="'.$_POST['symptomParent'].'">
-                <th style="width:50%;">'.ucwords($sampleRow['symptom_name']).'</th>
+        echo '<tr class="responseRow'.$_POST['responseParent'].'" id="'.$_POST['responseParent'].'">
+                <th style="width:50%;">'.ucwords($sampleRow['test_reason_name']).'</th>
                 <td style="width:50%;">
-                    <input name="symptomId[]" type="hidden" value="'.$sampleRow['symptom_id'].'">
-                    <select name="symptomDetected[]" class="form-control" title="Veuillez choisir la valeur pour '. $sampleRow['symptom_name'].'" style="width:100%" onchange="checkSubSymptoms(this,'.$sampleRow['symptom_id'].','.$index.');">
+                    <input name="responseId[]" type="hidden" value="'.$sampleRow['test_reason_id'].'">
+                    <select name="responseDetected[]" class="form-control" title="Veuillez choisir la valeur pour '. $sampleRow['test_reason_name'].'" style="width:100%" onchange="checkSubResponse(this,'.$sampleRow['test_reason_id'].','.$index.');">
                         <option value="">-- Select --</option>
                         <option value="yes"> Oui </option>
                         <option value="no"> Non </option>
