9f2cc2f1f361561e9f910321ac69ce5910951375
Amit Dugar <amit@deforay.com>
2020-09-03 14:27:45 +0530

    Added DOMAIN logic again in startup.php

--- startup.php
+++ startup.php
@@ -6,6 +6,32 @@ if (session_status() == PHP_SESSION_NONE) {
     // }    
 }
 
+if (php_sapi_name() !== 'cli') {
+    // base directory
+    $base_dir = __DIR__;
+
+    $doc_root = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
+
+    // server protocol
+    $protocol = empty($_SERVER['HTTPS']) ? 'http' : 'https';
+
+    // domain name
+    $domain = $_SERVER['SERVER_NAME'];
+
+    // base url
+    $base_url = preg_replace("!^${doc_root}!", '', $base_dir);
+
+    // server port
+    $port = $_SERVER['SERVER_PORT'];
+    $disp_port = ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
+
+    // put em all together to get the complete base URL
+    $domain = "${protocol}://${domain}${disp_port}${base_url}";
+
+    defined('DOMAIN')
+        || define('DOMAIN', $domain);
+}
+
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__)));
 
