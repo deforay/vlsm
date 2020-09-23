7132110f512baddc0a6edded711d1b763695f615
thanaseelan <thanaseelan@deforay.com>
2020-09-23 18:22:47 +0530

    Created test reason chart for covid19

--- models/General.php
+++ models/General.php
@@ -438,4 +438,12 @@ class General
         $query.= " LIMIT 1";
         return $this->db->rawQueryOne($query);
     }
+
+    function random_color_part() {
+        return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+    }
+    
+    function random_color() {
+        return $this->random_color_part() . $this->random_color_part() . $this->random_color_part();
+    }
 }
