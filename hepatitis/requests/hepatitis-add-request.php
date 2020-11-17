a0a270d4f8af857d0db43ed643f888377e53c9d7
thanaseelan <thanaseelan@deforay.com>
2020-11-17 19:51:11 +0530

    Hepatitis add request completed for who country

--- hepatitis/requests/hepatitis-add-request.php
+++ hepatitis/requests/hepatitis-add-request.php
@@ -43,15 +43,23 @@ $labTechnicians = $userDb->getActiveUserInfo();
 foreach ($labTechnicians as $labTech) {
     $labTechniciansResults[$labTech['user_id']] = ucwords($labTech['user_name']);
 }
-$healthFacilities = $facilitiesDb->getHealthFacilities('covid19');
-$testingLabs = $facilitiesDb->getTestingLabs('covid19');
+$healthFacilities = $facilitiesDb->getHealthFacilities('hepatitis');
+$testingLabs = $facilitiesDb->getTestingLabs('hepatitis');
 
-$rejectionTypeQuery = "SELECT DISTINCT rejection_type FROM r_covid19_sample_rejection_reasons WHERE rejection_reason_status ='active'";
-$rejectionTypeResult = $db->rawQuery($rejectionTypeQuery);
-
-//sample rejection reason
-$rejectionQuery = "SELECT * FROM r_covid19_sample_rejection_reasons where rejection_reason_status = 'active'";
-$rejectionResult = $db->rawQuery($rejectionQuery);
+// Comorbidity
+$comorbidityData = array();
+$comorbidityQuery = "SELECT DISTINCT comorbidity_id, comorbidity_name FROM r_hepatitis_comorbidities WHERE comorbidity_status ='active'";
+$comorbidityResult = $db->rawQuery($comorbidityQuery);
+foreach($comorbidityResult as $comorbidity){
+    $comorbidityData[$comorbidity['comorbidity_id']] = ucwords($comorbidity['comorbidity_name']);
+}
+// Risk Factors
+$riskFactorsData = array();
+$riskFactorsQuery = "SELECT DISTINCT riskfactor_id, riskfactor_name FROM r_hepatitis_rick_factors WHERE riskfactor_status ='active'";
+$riskFactorsResult = $db->rawQuery($riskFactorsQuery);
+foreach($riskFactorsResult as $riskFactors){
+    $riskFactorsData[$riskFactors['riskfactor_id']] = ucwords($riskFactors['riskfactor_name']);
+}
 
 $rejectionReason = "";
 foreach ($rejectionTypeResult as $type) {
@@ -63,11 +71,7 @@ foreach ($rejectionTypeResult as $type) {
     }
     $rejectionReason .= '</optgroup>';
 }
-
-
-
-$specimenTypeResult = $general->fetchDataFromTable('r_covid19_sample_type', "status = 'active'");
-
+// $specimenTypeResult = $general->fetchDataFromTable('r_hepatitis_sample_type', "status = 'active'");
 
 $fileArray = array(
     1 => 'forms/add-southsudan.php',
@@ -179,46 +183,33 @@ if (file_exists($fileArray[$arr['vl_form']])) {
             $('.ui-datepicker-calendar').show();
         });
 
+        $('#sampleTestedDateTime').datetimepicker({
+            changeMonth: true,
+            changeYear: true,
+            dateFormat: 'dd-M-yy',
+            timeFormat: "HH:mm",
+            maxDate: "Today",
+            onChangeMonthYear: function(year, month, widget) {
+                setTimeout(function() {
+                    $('.ui-datepicker-calendar').show();
+                });
+            },
+            onSelect: function(e) {
+                
+            },
+            yearRange: <?php echo (date('Y') - 100); ?> + ":" + "<?php echo (date('Y')) ?>"
+        }).click(function() {
+            $('.ui-datepicker-calendar').show();
+        });
+
         $('.date').mask('99-aaa-9999');
         $('.dateTime').mask('99-aaa-9999 99:99');
-
-        $('#isSampleRejected').change(function(e) {
-            if (this.value == 'yes') {
-                $('.show-rejection').show();
-                $('.test-name-table-input').prop('disabled', true);
-                $('.test-name-table').addClass('disabled');
-                $('#sampleRejectionReason,#rejectionDate').addClass('isRequired');
-                $('#sampleTestedDateTime,#result,.test-name-table-input').removeClass('isRequired');
-                $('#result').prop('disabled', true);
-                $('#sampleRejectionReason').prop('disabled', false);
-                // }else if(this.value == 'no'){
-            } else {
-                $('#rejectionDate').val('');
-                $('.show-rejection').hide();
-                $('.test-name-table-input').prop('disabled', false);
-                $('.test-name-table').removeClass('disabled');
-                $('#sampleRejectionReason,#rejectionDate').removeClass('isRequired');
-                $('#sampleTestedDateTime,#result,.test-name-table-input').addClass('isRequired');
-                $('#result').prop('disabled', false);
-                $('#sampleRejectionReason').prop('disabled', true);
-                checkPostive();
-            }
-        });
-        $('#hasRecentTravelHistory').change(function(e) {
-            if (this.value == 'no' || this.value == 'unknown') {
-                $('.historyfield').hide();
-                $('#countryName,#returnDate').removeClass('isRequired');
-            } else if (this.value == 'yes') {
-                $('.historyfield').show();
-                $('#countryName,#returnDate').addClass('isRequired');
-            }
-        });
     });
 
     function checkSampleNameValidation(tableName, fieldName, id, fnct, alrt) {
         if ($.trim($("#" + id).val()) != '') {
             $.blockUI();
-            $.post("/covid-19/requests/check-sample-duplicate.php", {
+            $.post("/hepatitis/requests/check-sample-duplicate.php", {
                     tableName: tableName,
                     fieldName: fieldName,
                     value: $("#" + id).val(),
@@ -235,7 +226,7 @@ if (file_exists($fileArray[$arr['vl_form']])) {
                             <?php }
                         } else { ?>
                             data = data.split("##");
-                            document.location.href = "/covid-19/requests/covid-19-edit-request.php?id=" + data[0] + "&c=" + data[1];
+                            document.location.href = "/hepatitis/requests/hepatitis-edit-request.php?id=" + data[0] + "&c=" + data[1];
                         <?php } ?>
                     }
                 });
@@ -243,9 +234,9 @@ if (file_exists($fileArray[$arr['vl_form']])) {
         }
     }
 
-    function insertSampleCode(formId, covid19SampleId, sampleCode, sampleCodeKey, sampleCodeFormat, countryId, sampleCollectionDate, provinceCode = null, provinceId = null) {
+    function insertSampleCode(formId, hepatitisSampleId, sampleCode, sampleCodeKey, sampleCodeFormat, countryId, sampleCollectionDate, provinceCode = null, provinceId = null) {
         $.blockUI();
-        $.post("/covid-19/requests/insert-sample.php", {
+        $.post("/hepatitis/requests/insert-sample.php", {
                 sampleCode: $("#" + sampleCode).val(),
                 sampleCodeKey: $("#" + sampleCodeKey).val(),
                 sampleCodeFormat: $("#" + sampleCodeFormat).val(),
@@ -257,7 +248,7 @@ if (file_exists($fileArray[$arr['vl_form']])) {
             function(data) {
                 if (data > 0) {
                     $.unblockUI();
-                    document.getElementById("covid19SampleId").value = data;
+                    document.getElementById("hepatitisSampleId").value = data;
                     document.getElementById(formId).submit();
                 } else {
                     $.unblockUI();
