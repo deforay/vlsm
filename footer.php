db20724b147b21e45bed29004340ed0392b47428
Sudarmathi <sudarmathi@deforay.com>
2020-11-25 16:46:48 +0530

    sample type hepatitis screen done

--- footer.php
+++ footer.php
@@ -548,6 +548,11 @@
 		$(".hepatitis-reference-manage").addClass('active');
 		$(".allMenu").removeClass('active');
 		$(".hepatitis-sample-rejection-reasons").addClass('active');
+	} else if (splitsUrl == 'hepatitis-sample-type.php' || splitsUrl == 'add-hepatitis-sample-type.php' || splitsUrl == 'edit-hepatitis-sample-type.php') {
+		$(".manage").addClass('active');
+		$(".hepatitis-reference-manage").addClass('active');
+		$(".allMenu").removeClass('active');
+		$(".hepatitis-sample-type").addClass('active');
 	} else {
 		$(".allMenu").removeClass('active');
 		$(".dashboardMenu").addClass('active');
