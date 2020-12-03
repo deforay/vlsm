17bd796b706cc6caff4120d0c7c3fc2e44159838
Prasath M <prasath@deforay.com>
2020-12-03 12:38:03 +0530

    Fixed breadcrumb in all the report pages of hepatitis

--- hepatitis/management/hepatitis-clinic-report.php
+++ hepatitis/management/hepatitis-clinic-report.php
@@ -35,8 +35,8 @@ $batResult = $db->rawQuery($batQuery);
 	<section class="content-header">
 		<h1> <i class="fa fa-book"></i> Clinic Reports</h1>
 		<ol class="breadcrumb">
-			<li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li>
-			<li >Hepatitis</li>
+			<!-- <li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li> -->
+			<li > <i class="fa fa-book"></i> Hepatitis</li>
 			<li >Management</li>
 			<li class="active">Clinic Reports</li>
 		</ol>
