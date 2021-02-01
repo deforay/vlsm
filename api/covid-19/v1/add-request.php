f9cee5c9d8762870e347ae98f70dfe19173b6d6a
Prasath M <prasath@deforay.com>
2021-02-01 17:40:18 +0530

    Fixed covid19 API with insert sample

--- api/covid-19/v1/add-request.php
+++ api/covid-19/v1/add-request.php
@@ -9,7 +9,7 @@ session_unset(); // no need of session in json response
 
 ini_set('memory_limit', -1);
 header('Content-Type: application/json');
-
+$c19Model = new \Vlsm\Models\Covid19($db);
 $general = new \Vlsm\Models\General($db);
 $userDb = new \Vlsm\Models\Users($db);
 $user = null;
@@ -34,10 +34,15 @@ if (empty($user) || empty($user['user_id'])) {
     echo json_encode($response);
     exit(0);
 }
-
+$sampleFrom = '';
+// echo $c19Model->generateCovid19SampleCode($data['province'], $data['sampleCollectionDate'], $sampleFrom);
+// die;
 // $data = json_decode(file_get_contents("php://input"),true);
+// insertSampleCode('addCovid19RequestForm', 'covid19SampleId', 'sampleCode', 'sampleCodeKey', 'sampleCodeFormat', 3, 'sampleCollectionDate');
 $data['api'] = "yes";
 $_POST = $data;
+include_once(APPLICATION_PATH . '/covid-19/requests/insert-sample.php');
+print_r($_POST['covid19SampleId']);
 include_once(APPLICATION_PATH . '/covid-19/requests/covid-19-add-request-helper.php');
 
 try {
