d5d8ae3f3e0a36010090c1e93d2eb103e286f532
thanaseelan <thanaseelan@deforay.com>
2021-02-25 18:22:49 +0530

    result_approved_datetime added in covid19 and eid modules

--- covid-19/results/update-status.php
+++ covid-19/results/update-status.php
@@ -10,9 +10,10 @@ try {
     $lock = $general->getGlobalConfig('lock_approved_covid19_samples');
     for ($i = 0; $i < count($id); $i++) {
         $status = array(
-            'result_status'         => $_POST['status'],
-            'result_approved_by'    =>  $_SESSION['userId'],
-            'data_sync'             => 0
+            'result_status'             => $_POST['status'],
+            'result_approved_by'        =>  $_SESSION['userId'],
+            'result_approved_datetime'  =>  $general->getDateTime(),
+            'data_sync'                 => 0
         );
         if ($_POST['status'] == '4') {
             $status['result'] = null;
