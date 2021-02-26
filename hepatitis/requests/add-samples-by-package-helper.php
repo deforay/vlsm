bd4139c41200b3c128fc4dfbb273b93a2a7455e4
thanaseelan <thanaseelan@deforay.com>
2021-02-26 13:46:27 +0530

    Prefix issue on hepatitis sample generation added

--- hepatitis/requests/add-samples-by-package-helper.php
+++ hepatitis/requests/add-samples-by-package-helper.php
@@ -8,11 +8,11 @@ $general = new \Vlsm\Models\General($db);
 $hepatitisObj = new \Vlsm\Models\Hepatitis($db);
 
 
-$sampleQuery = "SELECT hepatitis_id, sample_collection_date, sample_package_code, province_id, sample_code FROM form_hepatitis where hepatitis_id IN (" . $_POST['sampleId'] . ") ORDER BY hepatitis_id";
+$sampleQuery = "SELECT hepatitis_id, hepatitis_test_type, sample_collection_date, sample_package_code, province_id, sample_code FROM form_hepatitis where hepatitis_id IN (" . $_POST['sampleId'] . ") ORDER BY hepatitis_id";
 $sampleResult = $db->query($sampleQuery);
 $status = 0;
 foreach ($sampleResult as $sampleRow) {
-
+    echo "<pre>";print_r($sampleRow);die;
     $provinceCode = null;
     if (isset($sampleRow['province_id']) && !empty($sampleRow['province_id'])) {
         $provinceQuery = "SELECT * FROM province_details WHERE province_id= " . $sampleRow['province_id'];
@@ -24,7 +24,7 @@ foreach ($sampleResult as $sampleRow) {
     // ONLY IF SAMPLE CODE IS NOT ALREADY GENERATED
     if ($sampleRow['sample_code'] == null || $sampleRow['sample_code'] == '' || $sampleRow['sample_code'] == 'null') {
 
-        $sampleJson = $hepatitisObj->generatehepatitisSampleCode('',$provinceCode, $general->humanDateFormat($sampleRow['sample_collection_date']));
+        $sampleJson = $hepatitisObj->generatehepatitisSampleCode($sampleRow['hepatitis_test_type'],$provinceCode, $general->humanDateFormat($sampleRow['sample_collection_date']));
         $sampleData = json_decode($sampleJson, true);
         $hepatitisData = array();
         $hepatitisData['sample_code'] = $sampleData['sampleCode'];
