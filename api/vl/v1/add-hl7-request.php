55e947326fecd6dadc6f2402639529911feebb27
thanaseelan <thanaseelan@deforay.com>
2021-03-11 17:00:43 +0530

    Started working on HL7 message for ss

--- api/vl/v1/add-hl7-request.php
+++ api/vl/v1/add-hl7-request.php
@@ -2,6 +2,10 @@
 
 session_unset(); // no need of session in json response
 
+use Aranyasen\HL7\Message;
+use Aranyasen\HL7\Segment;
+use Aranyasen\HL7\Segments\MSH;
+use Aranyasen\HL7\Segments\PID;
 // PURPOSE : Fetch Results using serial_no field which is used to
 // store the recency id from third party apps (for eg. in DRC)
 
@@ -14,14 +18,15 @@ $general = new \Vlsm\Models\General($db);
 $userDb = new \Vlsm\Models\Users($db);
 $user = null;
 
-$data = json_decode(file_get_contents("php://input"),true);
-if(isset($data['api_token']) && $data['api_token']!='')
+$data = file_get_contents("php://input");
+/* if(isset($data['api_token']) && $data['api_token']!='')
 {
     $auth = $data['api_token'];
     $authToken = str_replace("Bearer ", "", $auth);
-    /* Check if API token exists */
+    // Check if API token exists
     $user = $userDb->getAuthToken($authToken);
 }
+
 // If authentication fails then do not proceed
 if (empty($user) || empty($user['user_id'])) {
     $response = array(
@@ -33,8 +38,12 @@ if (empty($user) || empty($user['user_id'])) {
     http_response_code(401);
     echo json_encode($response);
     exit(0);
-}
-
+} */
+// echo $data;die;
+$msg = new Message("MSH|^~\\&|1|\rPID|||abcd|\r"); // Either \n or \r can be used as segment endings
+$pid = $msg->getSegmentByIndex(1);
+echo $pid->getField(3); // prints 'abcd'
+echo $msg->toString(true);
 $data['api'] = "yes";
 $_POST = $data;
 // print_r(APPLICATION_PATH . '/vl/requests/addVlRequestHelper.php');die;
