6df623f1280ae41754fe11b4e33f38361a734901
Thanaseelan <thanaseelan@deforay.com>
2021-04-01 16:03:14 +0530

    API Correction done

--- api/app/covid-19/getcountybystate.php
+++ api/app/covid-19/getcountybystate.php
@@ -2,37 +2,35 @@
 header('Content-Type: application/json');
 require_once('../../../startup.php');
 
-
 $general = new \Vlsm\Models\General($db);
+$app = new \Vlsm\Models\App($db);
+
 $input = json_decode(file_get_contents("php://input"),true);
-$queryParams = array($input['authToken'], $input['userId']);
-$admin = $db->rawQuery("SELECT user_id,user_name,phone_number,login_id,status FROM user_details as ud WHERE ud.api_token = ? AND ud.user_id = ? ", $queryParams);
-if (isset($input['authToken']) && !empty($input['authToken']) && isset($input['userId']) && !empty($input['userId']) && isset($input['province']) && !empty($input['province'])) {
-    if(count($admin) > 0)
-    {
-        $data = array();
-        $db->where("f.facility_state", $input['province']);
-        $data['facilityInfo'] = $db->setQueryOption('DISTINCT')->get('facility_details f', null, array('facility_district'));        
-        $payload = array(
-            'status' => 1,
-            'message'=>'Success',
-            'data' => $data,
-            'timestamp' => $general->getDateTime()
-        );
-    }
-    else
-    {
-        $payload = array(
-            'status' => 2,
-            'message'=>'Api token is invalid.',
-            'timestamp' => $general->getDateTime()
-        );
-    }
-}
-else {
+$check = $app->fetchAuthToken($input);
+if (isset($check['status']) && !empty($check['status']) && $check['status'] == false) {
     $payload = array(
         'status' => 0,
-        'message'=>'Please Send all the credentials',
+        'message'=> $check['message'],
+        'timestamp' => $general->getDateTime()
+    );
+    echo json_encode($payload);
+    exit(0);
+}
+
+if(isset($input['province']) && !empty($input['province'])){
+    $data = array();
+    $db->where("f.facility_state", $input['province']);
+    $data['facilityInfo'] = $db->setQueryOption('DISTINCT')->get('facility_details f', null, array('facility_district'));        
+    $payload = array(
+        'status' => 1,
+        'message'=>'Success',
+        'data' => $data,
+        'timestamp' => $general->getDateTime()
+    );
+} else{
+    $payload = array(
+        'status' => 0,
+        'message'=>'Please send province details',
         'timestamp' => $general->getDateTime()
     );
 }
