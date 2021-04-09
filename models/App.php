c6923e40c9126adebb9bf2b2cd369428d27dceee
Thanaseelan <thanaseelan@deforay.com>
2021-04-09 17:15:12 +0530

    Created init.php in covid19 APP API

--- models/App.php
+++ models/App.php
@@ -70,9 +70,10 @@ class App
     public function getHealthFacilities($testType = null, $user = null, $onlyActive = false, $facilityType = 1)
     {
         $facilityDb = new \Vlsm\Models\Facilities($this->db);
-        $query = "SELECT hf.test_type, f.facility_id, f.facility_name, f.facility_code, f.facility_state, f.facility_district, f.facility_type 
+        $query = "SELECT hf.test_type, f.facility_id, f.facility_name, f.facility_code, pd.province_id, pd.province_name, f.facility_district, f.facility_type 
                     from health_facilities AS hf 
-                    INNER JOIN facility_details as f ON hf.facility_id=f.facility_id";
+                    INNER JOIN facility_details as f ON hf.facility_id=f.facility_id
+                    INNER JOIN province_details as pd ON pd.province_name=f.facility_state";
         $where = "";
         if (!empty($user)) {
             $facilityMap = $facilityDb->getFacilityMap($user);
@@ -116,12 +117,145 @@ class App
         $query .= $where;
         $result = $this->db->rawQuery($query);
         foreach($result as $key=>$row){
-            $response[$key]['value'] = $row['facility_id'];
-            $response[$key]['show'] = $row['facility_name'].' ('.$row['facility_code'].')';
-            $response[$key]['state'] = $row['facility_state'];
-            $response[$key]['province'] = $row['facility_district'];
+            $condition1 = " province_name like '".$row['province_name']."%'";
+            $condition2 = " facility_state like '".$row['province_name']."%'";
+
+            $response[$key]['value']        = $row['facility_id'];
+            $response[$key]['show']         = $row['facility_name'].' ('.$row['facility_code'].')';
+            /* $response[$key]['provinceId']   = $row['province_id'];
+            $response[$key]['province']     = $row['province_name'];
+            $response[$key]['district']     = $row['facility_district']; */
+            $response[$key]['provinceDetails'] = $this->getSubFields('province_details', 'province_id', 'province_name', $condition1);
+            $response[$key]['districtDetails'] = $this->getSubFields('facility_details', 'facility_district', 'facility_district', $condition2);
         }
         return $response;
     }
-}
+    
+    public function getProvinceDetails($user = null, $onlyActive = false)
+    {
+        $facilityDb = new \Vlsm\Models\Facilities($this->db);
+        $query = "SELECT f.facility_id, f.facility_name, f.facility_code, pd.province_id, pd.province_name, f.facility_district, f.facility_type 
+                    from province_details AS pd 
+                    LEFT JOIN facility_details as f ON pd.province_name=f.facility_state";
+        $where = "";
+        if (!empty($user)) {
+            $facilityMap = $facilityDb->getFacilityMap($user);
+            if (!empty($facilityMap)) {
+                if(isset($where) && trim($where) != ""){
+                    $where .= " AND ";
+                } else{
+                    $where .= " WHERE ";
+                }
+                $where .=" facility_id IN (" . $facilityMap . ")";
+            }
+        }
 
+        if ($onlyActive) {
+            if(isset($where) && trim($where) != ""){
+                $where .= " AND ";
+            } else{
+                $where .= " WHERE ";
+            }
+            $where .=" f.status like 'active'";
+        }
+        
+        $where .= 'ORDER BY province_name ASC';
+        $query .= $where;
+        $result = $this->db->rawQuery($query);
+        foreach($result as $key=>$row){
+            $condition1 = " facility_state like '".$row['province_name']."%'";
+
+            $response[$key]['value']    = $row['province_id'];
+            $response[$key]['show']     = $row['province_name'];
+            // $response[$key]['district'] = $row['facility_district'];
+            $response[$key]['districtDetails'] = $this->getSubFields('facility_details', 'facility_district', 'facility_district', $condition1);
+        }
+        return $response;
+    }
+    
+    public function getDistrictDetails($user = null, $onlyActive = false)
+    {
+        $facilityDb = new \Vlsm\Models\Facilities($this->db);
+        $query = "SELECT f.facility_id, f.facility_name, f.facility_code, pd.province_id, pd.province_name, f.facility_district
+                    from province_details AS pd 
+                    LEFT JOIN facility_details as f ON pd.province_name=f.facility_state";
+        $where = "";
+        if (!empty($user)) {
+            $facilityMap = $facilityDb->getFacilityMap($user);
+            if (!empty($facilityMap)) {
+                if(isset($where) && trim($where) != ""){
+                    $where .= " AND ";
+                } else{
+                    $where .= " WHERE ";
+                }
+                $where .=" facility_id IN (" . $facilityMap . ")";
+            }
+        }
+
+        if ($onlyActive) {
+            if(isset($where) && trim($where) != ""){
+                $where .= " AND ";
+            } else{
+                $where .= " WHERE ";
+            }
+            $where .=" f.status like 'active'";
+        }
+        
+        $where .= 'ORDER BY facility_district ASC';
+        $query .= $where;
+        // die($query);
+        $result = $this->db->rawQuery($query);
+        foreach($result as $key=>$row){
+            $condition1 = " facility_district like '".$row['facility_district']."%'";
+            $condition2 = " province_name like '".$row['province_name']."%'";
+
+            $response[$key]['value']        = $row['facility_district'];
+            $response[$key]['show']         = $row['facility_district'];
+            $response[$key]['facilityDetails'] = $this->getSubFields('facility_details', 'facility_id', 'facility_name', $condition1);
+            $response[$key]['provinceDetails'] = $this->getSubFields('province_details', 'province_id', 'province_name', $condition2);
+            /* $response[$key]['facilityId']   = $row['facility_id'];
+            $response[$key]['facilityName'] = $row['facility_name'].' ('.$row['facility_code'].')';
+            $response[$key]['provinceId']   = $row['province_id'];
+            $response[$key]['province']     = $row['province_name']; */
+        }
+        return $response;
+    }
+
+    public function getSubFields($tableName, $primary, $name, $condition)
+    {
+        $query = "SELECT $primary, $name from $tableName where $condition";
+        $result = $this->db->rawQuery($query);
+        $response = array();
+        foreach($result as $key=>$row){
+            $response[$key]['value']= $row[$primary];
+            $response[$key]['show'] = $row[$name];
+        }
+        return $response;
+    }
+
+    public function fetchAllDetailsBySampleCode($sampleCode)
+    {
+        if (empty($sampleCode)) {
+            return null;
+        }
+        $sQuery = "SELECT * FROM form_covid19 WHERE sample_code like '$sampleCode%' OR remote_sample_code LIKE '$sampleCode%'";
+        $result =  $this->db->rawQueryOne($sQuery);
+        $result['tests'] = $this->getCovid19TestsByFormId($result['covid19_id']);
+        return $result;
+    }
+
+    public function getCovid19TestsByFormId($formId)
+    {
+        if (empty($formId)) {
+            return null;
+        }
+
+        // Using this in sync requests/results
+        if (is_array($formId)) {
+            $sQuery = "SELECT * FROM covid19_tests WHERE `covid19_id` IN (" . implode(",", $formId) . ") ORDER BY test_id ASC";
+        } else {
+            $sQuery = "SELECT * FROM covid19_tests WHERE `covid19_id` = $formId ORDER BY test_id ASC";
+        }
+        return $this->db->rawQuery($sQuery);
+    }
+}
