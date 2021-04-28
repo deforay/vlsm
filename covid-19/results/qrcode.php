e0b8b8e04d677c09752846e513f5501fe507affe
Prasath M <prasath@deforay.com>
2021-04-28 19:08:10 +0530

    Finished QR CODE and display page

--- covid-19/results/qrcode.php
+++ covid-19/results/qrcode.php
@@ -1,8 +1,8 @@
 <?php
-  
+//   $_GET['id'] = "6777";
 include APPLICATION_PATH . '/phpqrcode/qrlib.php';
-  
-$text = "Covid-19";
+  $id = base64_encode($_GET['id']);
+$text = $_SERVER['HTTP_HOST'] . "/covid-19/results/covid-19-pdf-results.php?id=$id";
   
 
 QRcode::png($text);
