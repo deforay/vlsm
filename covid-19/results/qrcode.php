695fe4c676eda2ed9429e0567c93e0b0a4208a24
Prasath M <prasath@deforay.com>
2021-04-29 12:42:15 +0530

    Fixed qrcode display

--- covid-19/results/qrcode.php
+++ covid-19/results/qrcode.php
@@ -1,7 +1,7 @@
 <?php
 //   $_GET['id'] = "6777";
 include APPLICATION_PATH . '/phpqrcode/qrlib.php';
-  $id = base64_encode($_GET['id']);
+  $id = ($_GET['id']);
 $text = $_SERVER['HTTP_HOST'] . "/covid-19/results/covid-19-pdf-results.php?id=$id";
   
 
