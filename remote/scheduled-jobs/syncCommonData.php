bb2ad74ce9f5c099b2eec641cdee14e0ce561e74
Amit Dugar <amit@deforay.com>
2021-08-18 12:15:13 +0530

    Added network connectivity check in remote sync scripts

--- remote/scheduled-jobs/syncCommonData.php
+++ remote/scheduled-jobs/syncCommonData.php
@@ -12,6 +12,16 @@ if (!isset($systemConfig['remoteURL']) || $systemConfig['remoteURL'] == '') {
 
 $systemConfig['remoteURL'] = rtrim($systemConfig['remoteURL'], "/");
 
+
+
+$headers = @get_headers($systemConfig['remoteURL'] . '/vlsts-icons/favicon-16x16.png');
+
+if (strpos($headers[0], '200') === false) {
+    error_log("No internet connectivity while trying remote sync.");
+    return false;
+}
+
+
 $general = new \Vlsm\Models\General($db);
 $app = new \Vlsm\Models\App($db);
 
