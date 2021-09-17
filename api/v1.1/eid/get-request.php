aa11045a72df161127a94816ca8cd5a875a0bc5d
Thanaseelan K <thanaseelan@deforay.com>
2021-09-17 12:28:58 +0530

    Remote sample code now send as covid-19 and eid forms

--- api/v1.1/eid/get-request.php
+++ api/v1.1/eid/get-request.php
@@ -61,7 +61,8 @@ try {
     $sQuery = "SELECT 
         vl.app_local_test_req_id                             as localTestReqID,
         vl.eid_id                                            as eidId,
-        CASE WHEN (vl.sample_code IS NOT NULL) THEN vl.sample_code ELSE vl.remote_sample_code END as sampleCode,
+        vl.sample_code                                       as sampleCode,
+        vl.remote_sample_code                                as remoteSampleCode,
         vl.vlsm_instance_id                                  as instanceId,
         vl.vlsm_country_id                                   as formId,
         vl.facility_id                                       as facilityId,
