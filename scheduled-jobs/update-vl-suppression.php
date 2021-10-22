cf1b78972c7dda663c366a9b8231bfcdfe3622bf
Amit Dugar <amit@deforay.com>
2021-10-22 17:38:45 +0530

    Update VL Suppression scheduled-job updated

--- scheduled-jobs/update-vl-suppression.php
+++ scheduled-jobs/update-vl-suppression.php
@@ -2,61 +2,24 @@
 
 require_once(__DIR__ . "/../startup.php");
 
-$suppressionLimit = 1000;
-
-$suppressedArray = array(
-    'target not detected',
-    'tnd',
-    'not detected',
-    'below detection limit',
-    'below detection level',
-    'bdl',
-    'suppressed',
-    'negative',
-    'negat'
-);
+$vlDb = new \Vlsm\Models\Vl($db);
 
 $sql = "SELECT vl_sample_id,result_value_absolute_decimal, result_value_text, result
 				
 		FROM vl_request_form
 
-		WHERE vl_result_category is null";
-
+		WHERE (result_status = 4 OR result_status = 7)
+        
+        AND vl_result_category is null";
 
 $result = $db->rawQuery($sql);
+
 foreach ($result as $aRow) {
 
-    $dataForUpdate = array();
-    if ($aRow['result'] == NULL || empty($aRow['result'])) {
+    $vlResultCategory = $vlDb->getVLResultCategory($aRow['result_status'], $aRow['result']);
 
-        $dataForUpdate['vl_result_category'] = 'NO-RESULT';
-    } else if (is_numeric($aRow['result']) && $aRow['result'] > 0 && $aRow['result'] == round($aRow['result'], 0)) {
-        $aRow['result'] = (float)filter_var($aRow['result'], FILTER_SANITIZE_NUMBER_FLOAT);
-
-        if ($aRow['result'] < $suppressionLimit) {
-            $dataForUpdate['vl_result_category'] = 'Suppressed';
-        } else if ($aRow['result'] >= $suppressionLimit) {
-            $dataForUpdate['vl_result_category'] = 'Not Suppressed';
-        }
-    } else {
-
-        $textResult = NULL;
-
-        if (in_array(strtolower($aRow['result']), $suppressedArray) || in_array(strtolower($aRow['result_value_text']), $suppressedArray)) {
-            $textResult = 20;
-        } else {
-            $textResult = (float)filter_var($aRow['result_value_text'], FILTER_SANITIZE_NUMBER_FLOAT);
-        }
-
-        if ($textResult == 'NULL' || empty($textResult)) {
-            $dataForUpdate['vl_result_category'] = 'NO-RESULT';
-        } else if ($textResult < $suppressionLimit) {
-            $dataForUpdate['vl_result_category'] = 'Suppressed';
-        } else if ($textResult >= $suppressionLimit) {
-            $dataForUpdate['vl_result_category'] = 'Not Suppressed';
-        }
+    if (!empty($vlResultCategory) && $vlResultCategory !== false) {
+        $db->where('vl_sample_id', $aRow['vl_sample_id']);
+        $db->update("vl_request_form", $vlResultCategory);
     }
-
-    $db->where('vl_sample_id', $aRow['vl_sample_id']);
-    $ucount = $db->update("vl_request_form", $dataForUpdate);
 }
