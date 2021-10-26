8b1f0278314987e19e58e0d0b6f189629cadf4c6
Amit Dugar <amit@deforay.com>
2021-10-26 17:46:01 +0530

    Added remote sync jobs to ScheduledTasks

--- tasks/ScheduledTasks.php
+++ tasks/ScheduledTasks.php
@@ -1,64 +1,8 @@
 <?php
 
-// require_once(__DIR__ . "/../startup.php");
-
-
-
-// $logpath = APPLICATION_PATH . DIRECTORY_SEPARATOR . 'logs/scheduled-jobs.log';
-
-// $general = new \Vlsm\Models\General($db);
-// $vldashboardUrl = $general->getGlobalConfig('vldashboard_url');
-
-
-// $jobby = new Jobby\Jobby();
-
-// if (!empty($interfaceConfig['enabled']) && $interfaceConfig['enabled'] == true) {
-//     $jobby->add('interfacing', array(
-//         'command' => PHP_BINARY . " " . __DIR__ . DIRECTORY_SEPARATOR . "interface.php",
-//         'schedule' => '* * * * *',
-//         'output' => $logpath,
-//         'enabled' => true,
-//         'debug' => false,
-//     ));
-// }
-
-// if (!empty($vldashboardUrl) && !empty($systemConfig['modules']['vl']) && $systemConfig['modules']['vl'] == true) {
-//     $jobby->add('vldashboard-vl', array(
-//         'command' => PHP_BINARY . " " . __DIR__ . DIRECTORY_SEPARATOR . "vldashboard-v2/vldashboard-vl.php",
-//         'schedule' => '0 */1 * * *',
-//         'output' => $logpath,
-//         'enabled' => true,
-//         'debug' => false,
-//     ));
-// }
-
-// if (!empty($vldashboardUrl) && !empty($systemConfig['modules']['eid']) && $systemConfig['modules']['eid'] == true) {
-//     $jobby->add('vldashboard-eid', array(
-//         'command' => PHP_BINARY . " " . __DIR__ . DIRECTORY_SEPARATOR . "vldashboard-v2/vldashboard-eid.php",
-//         'schedule' => '30 */1 * * *',
-//         'output' => $logpath,
-//         'enabled' => true,
-//         'debug' => false,
-//     ));
-// }
-// if (!empty($vldashboardUrl) && !empty($systemConfig['modules']['covid19']) && $systemConfig['modules']['covid19'] == true) {
-//     $jobby->add('vldashboard-covid19', array(
-//         'command' => PHP_BINARY . " " . __DIR__ . DIRECTORY_SEPARATOR . "vldashboard-v2/vldashboard-covid19.php",
-//         'schedule' => '45 */1 * * *',
-//         'output' => $logpath,
-//         'enabled' => true,
-//         'debug' => false,
-//     ));
-// }
-
-// $jobby->run();
-
-
 require_once(__DIR__ . "/../startup.php");
 
-use Crunz\Schedule;
-
-$logpath = APPLICATION_PATH . '/logs/scheduled-jobs.log';
+//$logpath = APPLICATION_PATH . '/logs/scheduled-jobs.log';
 
 $general = new \Vlsm\Models\General($db);
 $vldashboardUrl = $general->getGlobalConfig('vldashboard_url');
@@ -67,14 +11,66 @@ $timeZone = !empty($timeZone) ? $timeZone : 'UTC';
 
 date_default_timezone_set($timeZone);
 
-$schedule = new Schedule();
+$schedule = new \Crunz\Schedule();
 
-$task = $schedule->run(PHP_BINARY . " " . APPLICATION_PATH . "/scheduled-jobs/vldashboard-v2/vldashboard-vl.php");
-$task
-    ->everyMinute()
-    ->timezone($timeZone)
-    ->preventOverlapping()
-    ->description('Dashboard VL Sync');
 
+// REMOTE SYNC JOBS START
+if (!empty($systemConfig['remoteURL'])) {
+    $schedule->run(PHP_BINARY . " " . APPLICATION_PATH . "/remote/scheduled-jobs/syncCommonData.php")
+        ->cron("0 */30 * * *")
+        ->timezone($timeZone)
+        ->preventOverlapping()
+        ->description('Syncing common/reference data from remote system');
+
+    $schedule->run(PHP_BINARY . " " . APPLICATION_PATH . "/remote/scheduled-jobs/syncRequests.php")
+        ->cron("0 */35 * * *")
+        ->timezone($timeZone)
+        ->preventOverlapping()
+        ->description('Syncing requests from remote system');
+
+    $schedule->run(PHP_BINARY . " " . APPLICATION_PATH . "/remote/scheduled-jobs/syncResults.php")
+        ->everyFifteenMinutes()
+        ->timezone($timeZone)
+        ->preventOverlapping()
+        ->description('Syncing results to remote system');
+}
+// REMOTE SYNC JOBS END
+
+
+// MACHINE INTERFACING JOBS START
+if (!empty($interfaceConfig['enabled']) && $interfaceConfig['enabled'] == true) {
+    $schedule->run(PHP_BINARY . " " . APPLICATION_PATH . "/scheduled-jobs/interface.php")
+        ->everyMinute()
+        ->timezone($timeZone)
+        ->preventOverlapping()
+        ->description('Importing data from interface db into local db');
+}
+// MACHINE INTERFACING JOBS END
+
+
+// DASHBOARD JOBS START
+if (!empty($vldashboardUrl) && !empty($systemConfig['modules']['vl']) && $systemConfig['modules']['vl'] == true) {
+    $schedule->run(PHP_BINARY . " " . APPLICATION_PATH . "/scheduled-jobs/vldashboard-v2/vldashboard-vl.php")
+        ->cron("0 */30 * * *")
+        ->timezone($timeZone)
+        ->preventOverlapping()
+        ->description('Syncing VL data from local database to Dashboard');
+}
+
+if (!empty($vldashboardUrl) && !empty($systemConfig['modules']['eid']) && $systemConfig['modules']['eid'] == true) {
+    $schedule->run(PHP_BINARY . " " . APPLICATION_PATH . "/scheduled-jobs/vldashboard-v2/vldashboard-eid.php")
+        ->cron("0 */35 * * *")
+        ->timezone($timeZone)
+        ->preventOverlapping()
+        ->description('Syncing EID data from local database to Dashboard');
+}
+if (!empty($vldashboardUrl) && !empty($systemConfig['modules']['covid19']) && $systemConfig['modules']['covid19'] == true) {
+    $schedule->run(PHP_BINARY . " " . APPLICATION_PATH . "/scheduled-jobs/vldashboard-v2/vldashboard-covid19.php")
+        ->cron("0 */40 * * *")
+        ->timezone($timeZone)
+        ->preventOverlapping()
+        ->description('Syncing Covid-19 data from local database to Dashboard');
+}
+// DASHBOARD JOBS END
 
 return $schedule;
