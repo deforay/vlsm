6f6ce9cc6a054bb40594126a83f200cfbbce304c
Sakthivel <sakthivel.p@deforay.com>
2021-11-09 18:34:14 +0530

    Add a user-login-history datatable for edit profile page

--- loginProcess.php
+++ loginProcess.php
@@ -72,11 +72,11 @@ try {
 
         $queryParams = array($username, $password, 'active');
         $admin = $db->rawQuery("SELECT * FROM user_details as ud INNER JOIN roles as r ON ud.role_id=r.role_id WHERE ud.login_id = ? AND ud.password = ? AND ud.status = ?", $queryParams);
-        $attemptCount1 = $db->rawQueryOne("SELECT COUNT(*) as attempt FROM user_login_history as ud WHERE ud.login_id = '".$username."' AND ud.login_status='failed' AND  ud.login_attempted_datetime > DATE_SUB(NOW(), INTERVAL 15 minute)");
-        
-        if($attemptCount1['attempt'] < 3) {
-            if(count($admin) > 0){
-                $user->userHistoryLog($username,$loginStatus='successful');
+        $attemptCount1 = $db->rawQueryOne("SELECT COUNT(*) as attempt FROM user_login_history as ud WHERE ud.login_id = '" . $username . "' AND ud.login_status='failed' AND  ud.login_attempted_datetime > DATE_SUB(NOW(), INTERVAL 15 minute)");
+
+        if ($attemptCount1['attempt'] < 3) {
+            if (count($admin) > 0) {
+                $user->userHistoryLog($username, $loginStatus = 'successful');
 
                 //add random key
                 $instanceResult = $db->rawQueryOne("SELECT vlsm_instance_id, instance_facility_name FROM s_vlsm_instance");
@@ -152,18 +152,102 @@ try {
                 //     $_SESSION['vldashboard_url'] = null;
                 // }
                 header("location:" . $redirect);
-            }else{
-                $user->userHistoryLog($username,$loginStatus='failed');
+            } else {
+                $user->userHistoryLog($username, $loginStatus = 'failed');
                 $_SESSION['alertMsg'] = "Please check your login credentials";
                 header("location:/login.php");
             }
+        } else if ($attemptCount1['attempt'] >= 3) {
+            if ($_POST['captcha'] != '') {
+                if (count($admin) > 0) {
+                    $user->userHistoryLog($username, $loginStatus = 'successful');
+    
+                    //add random key
+                    $instanceResult = $db->rawQueryOne("SELECT vlsm_instance_id, instance_facility_name FROM s_vlsm_instance");
+    
+                    if ($instanceResult) {
+                        $_SESSION['instanceId'] = $instanceResult['vlsm_instance_id'];
+                        $_SESSION['instanceFacilityName'] = $instanceResult['instance_facility_name'];
+                    } else {
+                        $id = $general->generateRandomString(32);
+                        // deleting just in case there is a row already inserted
+                        $db->delete('s_vlsm_instance');
+                        $db->insert('s_vlsm_instance', array('vlsm_instance_id' => $id));
+                        $_SESSION['instanceId'] = $id;
+                        $_SESSION['instanceFacilityName'] = null;
+    
+                        //Update instance ID in facility and vl_request_form tbl
+                        $data = array('vlsm_instance_id' => $id);
+                        $db->update('facility_details', $data);
+                    }
+                    //Add event log
+                    $eventType = 'login';
+                    $action = ucwords($admin[0]['user_name']) . ' logged in';
+                    $resource = 'user-login';
+    
+                    $general->activityLog($eventType, $action, $resource);
+    
+                    $_SESSION['userId'] = $admin[0]['user_id'];
+                    $_SESSION['userName'] = ucwords($admin[0]['user_name']);
+                    $_SESSION['roleCode'] = $admin[0]['role_code'];
+                    $_SESSION['roleId'] = $admin[0]['role_id'];
+                    $_SESSION['accessType'] = $admin[0]['access_type'];
+                    $_SESSION['email'] = $admin[0]['email'];
+    
+                    $redirect = '/error/401.php';
+                    //set role and privileges
+                    $priQuery = "SELECT p.privilege_name, rp.privilege_id, r.module FROM roles_privileges_map as rp INNER JOIN privileges as p ON p.privilege_id=rp.privilege_id INNER JOIN resources as r ON r.resource_id=p.resource_id  where rp.role_id='" . $admin[0]['role_id'] . "'";
+                    $priInfo = $db->query($priQuery);
+                    $priId = array();
+                    if ($priInfo) {
+                        foreach ($priInfo as $id) {
+                            $priId[] = $id['privilege_name'];
+                            $module[$id['module']] = $id['module'];
+                        }
+    
+                        if ($admin[0]['landing_page'] != '') {
+                            $redirect = $admin[0]['landing_page'];
+                        } else {
+                            $fileNameList = array('index.php', 'addVlRequest.php', 'vlRequest.php', 'batchcode.php', 'vlRequestMail.php', 'addImportResult.php', 'vlPrintResult.php', 'vlTestResult.php', 'vl-sample-status.php', 'vlResult.php', 'highViralLoad.php', 'roles.php', 'users.php', 'facilities.php', 'globalConfig.php', 'importConfig.php');
+                            $fileName = array('dashboard/index.php', '/vl/requests/addVlRequest.php', '/vl/requests/vlRequest.php', '/vl/batch/batchcode.php', 'mail/vlRequestMail.php', 'import-result/addImportResult.php', '/vl/results/vlPrintResult.php', '/vl/results/vlTestResult.php', 'program-management/vl-sample-status.php', 'program-management/vlResult.php', 'program-management/highViralLoad.php', 'roles/roles.php', 'users/users.php', 'facilities/facilities.php', 'global-config/globalConfig.php', 'import-configs/importConfig.php');
+                            foreach ($fileNameList as $redirectFile) {
+                                if (in_array($redirectFile, $priId)) {
+                                    $arrIndex = array_search($redirectFile, $fileNameList);
+                                    $redirect = $fileName[$arrIndex];
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    //check clinic or lab user
+                    $_SESSION['userType']   = '';
+                    $_SESSION['privileges'] = $priId;
+                    $_SESSION['module']     = $module;
+    
+    
+                    if ($systemType != null && ($systemType == 'vluser' || $systemType == 'remoteuser') && $systemLabId != '' && $systemLabId != null) {
+                        $_SESSION['system'] = $systemType;
+                    } else {
+                        $_SESSION['system'] = null;
+                    }
+                    // if ($dashboardUrl != null && $dashboardUrl != '') {
+                    //     $_SESSION['vldashboard_url'] = $dashboardUrl;
+                    // } else {
+                    //     $_SESSION['vldashboard_url'] = null;
+                    // }
+                    header("location:" . $redirect);
+                }
+                else {
+                    $user->userHistoryLog($username, $loginStatus = 'failed');
+                    $_SESSION['alertMsg'] = "Please check your login credentials";
+                    header("location:/login.php");
+                }
+            } else {
+                $user->userHistoryLog($username, $loginStatus = 'failed');
+                $_SESSION['alertMsg'] = "You have exhausted maximum login attempts. Please try to login after sometime.";
+                header("location:/login.php");
+            }
         }
-        else if($attemptCount1['attempt']>=3) {
-            $user->userHistoryLog($username,$loginStatus='failed');
-            $_SESSION['alertMsg'] = "You have exhausted maximum login attempts. Please try to login after sometime.";
-            header("location:/login.php");
-        }
-        
     } else {
         header("location:/login.php");
     }
