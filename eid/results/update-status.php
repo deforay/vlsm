9c9b680b087ac77a9e6ed6a94681297b6e8ddaf2
Thanaseelan K <thanaseelan@deforay.com>
2021-11-09 12:05:09 +0530

    result status update for all test type done

--- eid/results/update-status.php
+++ eid/results/update-status.php
@@ -5,17 +5,27 @@ ob_start();
 
 $general = new \Vlsm\Models\General($db);
 $tableName = "eid_form";
+$result = "";
 try {
     $lock = $general->getGlobalConfig('lock_approved_eid_samples');
     $id = explode(",", $_POST['id']);
     for ($i = 0; $i < count($id); $i++) {
+
         $status = array(
             'result_status'             => $_POST['status'],
-            'result_reviewed_by'        =>  $_SESSION['userId'],
-            'result_approved_by'        =>  $_SESSION['userId'],
             'result_approved_datetime'  =>  $general->getDateTime(),
             'data_sync'                 => 0
         );
+
+        /* Check if already have reviewed and approved by */
+        $db = $db->where('eid_id', $id[$i]);
+        $reviewd = $db->getOne($tableName, array("result_reviewed_by", "result_approved_by"));
+        if (empty($reviewd['result_reviewed_by'])) {
+            $status['result_reviewed_by'] = $_SESSION['userId'];
+        }
+        if (empty($reviewd['result_approved_by'])) {
+            $status['result_approved_by'] = $_SESSION['userId'];
+        }
         if ($_POST['status'] == '4') {
             $status['result'] = null;
             $status['is_sample_rejected'] = 'yes';
@@ -24,7 +34,7 @@ try {
             $status['is_sample_rejected'] = 'no';
             $status['reason_for_sample_rejection'] = null;
         }
-        if($status['result_status'] == 7 && $lock == 'yes'){
+        if ($status['result_status'] == 7 && $lock == 'yes') {
             $status['locked'] = 'yes';
         }
         $db = $db->where('eid_id', $id[$i]);
