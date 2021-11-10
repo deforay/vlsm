bd1727290b5adb40a282aff698378729b3577891
Amit Dugar <amit@deforay.com>
2021-11-10 14:32:57 +0530

    Removed v1 api. Minor changes in v1.1 api

--- facilities/addFacility.php
+++ facilities/addFacility.php
@@ -3,7 +3,7 @@ ob_start();
 #require_once('../startup.php');
 include_once(APPLICATION_PATH . '/header.php');
 $general = new \Vlsm\Models\General();
-$geolocation = new \Vlsm\Models\Geolocations();
+$geolocation = new \Vlsm\Models\GeoLocations();
 
 $fQuery = "SELECT * FROM facility_type";
 $fResult = $db->rawQuery($fQuery);
