3baf37f3d0a75d4e9fe9de954d4d47801c467a28
Amit Dugar <amit@deforay.com>
2021-11-17 13:04:03 +0530

    Renamed system admin table

--- system-admin/login/adminLoginProcess.php
+++ system-admin/login/adminLoginProcess.php
@@ -10,10 +10,10 @@ try {
         $adminUsername = trim($_POST['username']);
         $adminPassword = trim($_POST['password']);
         $params = array($adminUsername,$adminPassword);
-        $admin = $db->rawQuery("SELECT * FROM user_admin_details as ud WHERE ud.user_admin_login = ? AND ud.user_admin_password = ?", $params);
+        $admin = $db->rawQuery("SELECT * FROM system_admin as ud WHERE ud.system_admin_login = ? AND ud.system_admin_password = ?", $params);
         if(count($admin)>0){
-            $_SESSION['adminUserId']=$admin[0]['user_admin_id'];
-            $_SESSION['adminUserName']=ucwords($admin[0]['user_admin_name']);
+            $_SESSION['adminUserId']=$admin[0]['system_admin_id'];
+            $_SESSION['adminUserName']=ucwords($admin[0]['system_admin_name']);
             header("location:/system-admin/edit-config/index.php");
         }else{
             header("location:/login.php");
