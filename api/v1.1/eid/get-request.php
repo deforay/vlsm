d97bae45a1631383ba5d26ea40a6f29656fe364c
Thanaseelan K <thanaseelan@deforay.com>
2021-11-17 19:06:24 +0530

    Expired token now came every API hit

--- api/v1.1/eid/get-request.php
+++ api/v1.1/eid/get-request.php
@@ -207,7 +207,11 @@ try {
         'timestamp' => time(),
         'data' => $rowData
     );
-
+    if (isset($user['token_updated']) && $user['token_updated'] == true) {
+        $payload['token'] = $user['new_token'];
+    } else {
+        $payload['token'] = null;
+    }
     http_response_code(200);
     echo json_encode($payload);
     exit(0);
