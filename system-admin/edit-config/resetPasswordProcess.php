98ec1bf6c5151fbeb063bd7aa52b1b094f3646bf
Sakthivel <sakthivel.p@deforay.com>
2021-11-19 16:25:17 +0530

    Change a password for encrpt in change password and login

--- system-admin/edit-config/resetPasswordProcess.php
+++ system-admin/edit-config/resetPasswordProcess.php
@@ -9,9 +9,7 @@ $tableName= "system_admin";
 try {
     $userId = base64_decode($_POST['userId']);
     if(isset($_POST['password']) && trim($_POST['password'])!=""){
-        $data = array(
-            'system_admin_password' => $_POST['password'],
-    );
+    $data['system_admin_password'] = sha1($_POST['password'] . $systemConfig['passwordSalt']);
     $db = $db->where('system_admin_id', $userId);
     $db->update($tableName, $data);
     $_SESSION['alertMsg'] = "Password updated successfully";
--- system-admin/login/adminLoginProcess.php
+++ system-admin/login/adminLoginProcess.php
@@ -9,7 +9,8 @@ try {
     if(isset($_POST['username']) && trim($_POST['username'])!="" && isset($_POST['password']) && trim($_POST['password'])!=""){
         $adminUsername = trim($_POST['username']);
         $adminPassword = trim($_POST['password']);
-        $params = array($adminUsername,$adminPassword);
+        $password = sha1($adminPassword . $systemConfig['passwordSalt']);
+        $params = array($adminUsername,$password);
         $admin = $db->rawQuery("SELECT * FROM system_admin as ud WHERE ud.system_admin_login = ? AND ud.system_admin_password = ?", $params);
         if(count($admin)>0){
             $_SESSION['adminUserId']=$admin[0]['system_admin_id'];
