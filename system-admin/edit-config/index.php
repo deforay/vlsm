febe5365c0b2d5e60eb15d0929832082f67bff18
Sakthivel <sakthivel.p@deforay.com>
2021-11-19 11:12:09 +0530

    Add a footer in active and inactive menus

--- system-admin/edit-config/index.php
+++ system-admin/edit-config/index.php
@@ -18,7 +18,7 @@ $fResult=$db->query($fDetails);
     <section class="content-header">
       <h1 class="fa fa-gears"> Edit System Configuration</h1>
       <ol class="breadcrumb">
-        <li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active">Manage System Config</li>
       </ol>
     </section>
