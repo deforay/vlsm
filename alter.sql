30df2c4dcab118bf450064dbe4ee11a1bd65be56
Sakthivel <sakthivel.p@deforay.com>
2021-12-02 15:14:02 +0530

    Add a priveldge for tb management on export data

--- alter.sql
+++ alter.sql
@@ -2693,11 +2693,10 @@ UPDATE `system_config` SET `value` = '4.4.5' WHERE `system_config`.`name` = 'sc_
 -- Sakthivel 01-Dec-2021
 ALTER TABLE `r_covid19_results`
   MODIFY `result_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-  
--- Thana 01-Dec-2021
-INSERT INTO `resources` (`resource_id`, `module`, `display_name`) VALUES ('tb-batches', 'tb', 'TB Batch Management');
-INSERT INTO `privileges` (`privilege_id`, `resource_id`, `privilege_name`, `display_name`) VALUES (NULL, 'tb-batches', 'tb-batches.php', 'View Batches'), (NULL, 'tb-batches', 'tb-add-batch.php', 'Add Batch'), (NULL, 'tb-batches', 'tb-edit-batch.php', 'Edit Batch'), (NULL, 'tb-batches', 'tb-add-batch-position.php', 'Add Batch Position'), (NULL, 'tb-batches', 'tb-edit-batch-position.php', 'Edit Batch Position');
 
- -- Sakthivel 01-Dec-2021
-ALTER TABLE `r_covid19_results`
+  -- Sakthivel 01-Dec-2021
+ALTER TABLE `r_eid_results`
   MODIFY `result_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+  --Sakthivel 01-Dec-2021
+INSERT INTO `privileges` (`privilege_id`, `resource_id`, `privilege_name`, `display_name`) VALUES (NULL, 'tb-results', 'tb-export-data.php', 'Export Data');
