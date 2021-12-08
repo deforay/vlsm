25489092316301f2ed785756fe8247970e5b5954
Sakthivel <sakthivel.p@deforay.com>
2021-12-08 20:20:12 +0530

    Add a patient search fuction and files for add/edit and to minimize the code for other choose value from dropdown

---
+++ tb/requests/checkPatientExist.php
@@ -0,0 +1,18 @@
+<?php
+#require_once('../../startup.php');  
+
+$general=new \Vlsm\Models\General();
+$artNo=$_POST['artPatientNo'];
+//global config
+$cQuery="SELECT * FROM global_config";
+$cResult=$db->query($cQuery);
+$arr = array();
+// now we create an associative array so that we can easily create view variables
+for ($i = 0; $i < sizeof($cResult); $i++) {
+  $arr[$cResult[$i]['name']] = $cResult[$i]['value'];
+}
+$count = 0;
+$pQuery="SELECT * FROM form_tb where vlsm_country_id='".$arr['vl_form']."' AND (patient_id like '%".$artNo."%' OR patient_name like '%".$artNo."%' OR patient_surname like '%".$artNo."%')";
+$pResult = $db->rawQuery($pQuery);
+$count = count($pResult);
+echo $count;
