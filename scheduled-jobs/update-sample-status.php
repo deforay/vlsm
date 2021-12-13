58513636eed68d43452087035a19e155ce5c950e
Amit Dugar <amit@deforay.com>
2021-12-13 15:31:47 +0530

    Fixed update-sample-status.php cron and added it to ScheduledTasks.php

--- scheduled-jobs/update-sample-status.php
+++ scheduled-jobs/update-sample-status.php
@@ -1,52 +1,45 @@
 <?php
 
 require_once(__DIR__ . "/../startup.php");
-$general = new \Vlsm\Models\General();
-/* Define the module required fields and tables */
-$primaryKey = array("vl" => "vl_sample_id", "eid" => "eid_id", "covid19" => "covid19_id", "hepatitis" => "hepatitis_id", "tb" => "tb_id");
-$tableName = array("vl" => "vl_request_form", "eid" => "eid_form", "covid19" => "form_covid19", "hepatitis" => "form_hepatitis", "tb" => "form_tb");
 
-$updateStatus = array();
+$general = new \Vlsm\Models\General();
+
+$primaryKey = array(
+    "vl" => "vl_sample_id",
+    "eid" => "eid_id",
+    "covid19" => "covid19_id",
+    "hepatitis" => "hepatitis_id",
+    "tb" => "tb_id"
+);
+$tableName = array(
+    "vl" => "vl_request_form",
+    "eid" => "eid_form",
+    "covid19" => "form_covid19",
+    "hepatitis" => "form_hepatitis",
+    "tb" => "form_tb"
+);
+
 foreach ($systemConfig['modules'] as $module => $status) {
     if ($status) {
+        //EXPIRING SAMPLES
         $expiryDays = $general->getGlobalConfig($module . '_sample_expiry_after_days');
-        $updateStatus['sample-expiry'][$module] = array();
-        $updateStatus['lock-expiry'][$module] = array();
-        if ($expiryDays > 0) {
-            /* Update sample expiry days status */
-            $db->where("sample_tested_datetime IS NULL");
-            $db->where("result_status NOT LIKE 10");
-            $db->where("(DATEDIFF(CURRENT_DATE, `sample_collection_date`)) > " . $expiryDays);
-            $result = $db->get($tableName[$module]);
-            if (sizeof($result) > 0) {
-                $id = 0;
-                foreach ($result as $testRow) {
-                    $db->where($primaryKey[$module], $testRow[$primaryKey[$module]]);
-                    $id = $db->update($tableName[$module], array("result_status" => 10));
-                    if ($id > 0) {
-                        $updateStatus['sample-expiry'][$module][] = $testRow[$primaryKey[$module]];
-                    }
-                }
-            }
+        if (empty($expiryDays)) {
+            $expiryDays = 365; // by default we consider samples more than 365 days as expired
         }
+        $db->where("result_status != 4"); // not rejected 
+        $db->where("result_status != 7"); // not approved
+        $db->where("result_status != 10"); // not expired
+        $db->where("(DATEDIFF(CURRENT_DATE, `sample_collection_date`)) > " . $expiryDays);
+        $db->update($tableName[$module], array("result_status" => 10));
+
+
+        //LOCKING SAMPLES
         $lockExpiryDays = $general->getGlobalConfig($module . '_sample_lock_after_days');
-        if ($lockExpiryDays != null) {
-            /* Updated sample lock expiry days */
-            $db->where("(result_status = 7 OR result_status = 4)");
+        if ($lockExpiryDays != null && $lockExpiryDays >= 0) {
+            $db->where("(result_status = 7 OR result_status = 4)"); // Samples that are Accepted or Rejected
             $db->where("locked NOT LIKE 'yes'");
             $db->where("(DATEDIFF(CURRENT_DATE, `last_modified_datetime`)) > " . $lockExpiryDays);
-            $lockResult = $db->get($tableName[$module]);
-            if (sizeof($lockResult) > 0) {
-                $id = 0;
-                foreach ($lockResult as $lockRow) {
-                    $db->where($primaryKey[$module], $lockRow[$primaryKey[$module]]);
-                    $id = $db->update($tableName[$module], array("locked" => "yes"));
-                    if ($id > 0) {
-                        $updateStatus['lock-expiry'][$module][] = $lockRow[$primaryKey[$module]];
-                    }
-                }
-            }
+            $db->update($tableName[$module], array("locked" => "yes"));
         }
     }
 }
-echo json_encode(array("status" => "success", "updated-records" => json_encode($updateStatus), "message" => "Updated"));
