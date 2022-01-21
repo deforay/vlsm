ecb910c3b4c450d06888ae8402c0bb9bfdce041d
Amit Dugar <amit@deforay.com>
2022-01-21 14:17:33 +0530

    Minor change to version api header

--- api/v1.1/version.php
+++ api/v1.1/version.php
@@ -1,4 +1,7 @@
 <?php
-// To send API version to APP
+
+header('Content-Type: application/json; charset=utf-8');
+
+// return VLSM Version
 $payload = array('version' => VERSION);
 echo json_encode($payload);
