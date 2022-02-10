5075d79d123cd7ac70cc0d940fcb3ede0fd9b824
Sakthivel <sakthivel.p@deforay.com>
2022-02-10 14:06:33 +0530

    change translate for VL config module pages

--- vl/reference/vl-sample-rejection-reasons.php
+++ vl/reference/vl-sample-rejection-reasons.php
@@ -1,5 +1,5 @@
 <?php
-$title = "Covid-19 Sample Rejection Reasons";
+$title = _("Viral Load Sample Rejection Reasons");
 #require_once('../startup.php'); 
 include_once(APPLICATION_PATH . '/header.php');
 
@@ -8,10 +8,10 @@ include_once(APPLICATION_PATH . '/header.php');
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content-header">
-    <h1><i class="fa fa-flask"></i> Viral Load Sample Rejection Reasons</h1>
+    <h1><i class="fa fa-flask"></i> <?php echo _("Viral Load Sample Rejection Reasons");?></h1>
     <ol class="breadcrumb">
-      <li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li>
-      <li class="active">Viral Load Sample Rejection Reasons</li>
+      <li><a href="/"><i class="fa fa-dashboard"></i> <?php echo _("Home");?></a></li>
+      <li class="active"><?php echo _("Viral Load Sample Rejection Reasons");?></li>
     </ol>
   </section>
 
@@ -22,7 +22,7 @@ include_once(APPLICATION_PATH . '/header.php');
         <div class="box">
           <div class="box-header with-border">
             <?php if (isset($_SESSION['privileges']) && in_array("vl-art-code-details.php", $_SESSION['privileges']) && $sarr['sc_user_type'] != 'vluser') { ?>
-              <a href="add-vl-sample-rejection-reasons.php" class="btn btn-primary pull-right"> <i class="fa fa-plus"></i> Add VL Sample Rejection Reasons</a>
+              <a href="add-vl-sample-rejection-reasons.php" class="btn btn-primary pull-right"> <i class="fa fa-plus"></i> <?php echo _("Add VL Sample Rejection Reasons");?></a>
             <?php } ?>
             <!--<button class="btn btn-primary pull-right" style="margin-right: 1%;" onclick="$('#showhide').fadeToggle();return false;"><span>Manage Columns</span></button>-->
           </div>
@@ -31,10 +31,10 @@ include_once(APPLICATION_PATH . '/header.php');
             <table id="samRejReasonDataTable" class="table table-bordered table-striped">
               <thead>
                 <tr>
-                  <th>Rejection Reason</th>
-                  <th>Rejection Reason Type</th>
-                  <th>Rejection Reason Code</th>
-                  <th>Rejection Reason Status</th>
+                  <th><?php echo _("Rejection Reason");?></th>
+                  <th><?php echo _("Rejection Reason Type");?></th>
+                  <th><?php echo _("Rejection Reason Code");?></th>
+                  <th><?php echo _("Rejection Reason Status");?></th>
                   <?php if (isset($_SESSION['privileges']) && in_array("vl-art-code-details.php", $_SESSION['privileges']) && $sarr['sc_user_type'] != 'vluser') { ?>
                     <!-- <th>Action</th> -->
                   <?php } ?>
@@ -42,7 +42,7 @@ include_once(APPLICATION_PATH . '/header.php');
               </thead>
               <tbody>
                 <tr>
-                  <td colspan="6" class="dataTables_empty">Loading data from server</td>
+                  <td colspan="6" class="dataTables_empty"><?php echo _("Loading data from server");?></td>
                 </tr>
               </tbody>
 
@@ -109,7 +109,7 @@ include_once(APPLICATION_PATH . '/header.php');
 
   function updateStatus(obj, optVal) {
     if (obj.value != '') {
-      conf = confirm("Are you sure you want to change the status?");
+      conf = confirm("<?php echo _("Are you sure you want to change the status?");?>");
       if (conf) {
         $.post("update-vl-rejection-status.php", {
             status: obj.value,
@@ -118,7 +118,7 @@ include_once(APPLICATION_PATH . '/header.php');
           function(data) {
             if (data != "") {
               oTable.fnDraw();
-              alert('Updated successfully.');
+              alert("<?php echo _("Updated successfully");?>.");
             }
           });
       }
