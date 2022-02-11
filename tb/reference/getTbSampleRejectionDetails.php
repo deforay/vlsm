24d57fb7cfa47da54120b8e4ebf63ddd0035810c
Sakthivel <sakthivel.p@deforay.com>
2022-02-11 12:30:21 +0530

    change translate for TB config module pages

--- tb/reference/getTbSampleRejectionDetails.php
+++ tb/reference/getTbSampleRejectionDetails.php
@@ -137,9 +137,9 @@ $output = array(
 );
 
 foreach ($rResult as $aRow) {
-    $status = '<select class="form-control" name="status[]" id="' . $aRow['rejection_reason_id'] . '" title="Please select status" onchange="updateStatus(this,\'' . $aRow['rejection_reason_status'] . '\')">
-               <option value="active" ' . ($aRow['rejection_reason_status'] == "active" ? "selected=selected" : "") . '>Active</option>
-               <option value="inactive" ' . ($aRow['rejection_reason_status'] == "inactive"  ? "selected=selected" : "") . '>Inactive</option>
+    $status = '<select class="form-control" name="status[]" id="' . $aRow['rejection_reason_id'] . '" title="'. _("Please select status").'" onchange="updateStatus(this,\'' . $aRow['rejection_reason_status'] . '\')">
+               <option value="active" ' . ($aRow['rejection_reason_status'] == "active" ? "selected=selected" : "") . '>'. _("Active").'</option>
+               <option value="inactive" ' . ($aRow['rejection_reason_status'] == "inactive"  ? "selected=selected" : "") . '>'. _("Inactive").'</option>
                </select><br><br>';
     $row = array();
     $row[] = ucwords($aRow['rejection_reason_name']);
