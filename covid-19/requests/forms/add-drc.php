38012d688ba1c8a6e56c3c8197f5138e5d737d9a
Thanaseelan K <thanaseelan@deforay.com>
2022-03-03 16:20:58 +0530

    Added autocomplete from covid19 drc form province field

--- covid-19/requests/forms/add-drc.php
+++ covid-19/requests/forms/add-drc.php
@@ -1413,6 +1413,39 @@ if (!empty($generateAutomatedPatientCode) && $generateAutomatedPatientCode == 'y
             $.unblockUI();
         });
 
+        $("#patientProvince").select2({
+            placeholder: "Entrez le province du patient",
+            minimumInputLength: 0,
+            width: '100%',
+            allowClear: true,
+            ajax: {
+                placeholder: "Tapez le nom du province à rechercher",
+                url: "/covid-19/requests/get-province-district-list.php",
+                dataType: 'json',
+                delay: 250,
+                data: function(params) {
+                    return {
+                        type: 'province',
+                        q: params.term, // search term
+                        page: params.page
+                    };
+                },
+                processResults: function(data, params) {
+                    params.page = params.page || 1;
+                    return {
+                        results: data.result,
+                        pagination: {
+                            more: (params.page * 30) < data.total_count
+                        }
+                    };
+                },
+                //cache: true
+            },
+            escapeMarkup: function(markup) {
+                return markup;
+            }
+        });
+
         $("#patientProvince").change(function() {
             $.blockUI();
             var pName = $(this).val();
@@ -1436,11 +1469,12 @@ if (!empty($generateAutomatedPatientCode) && $generateAutomatedPatientCode == 'y
             allowClear: true,
             ajax: {
                 placeholder: "Tapez le nom du district à rechercher",
-                url: "/covid-19/requests/get-district-list.php",
+                url: "/covid-19/requests/get-province-district-list.php",
                 dataType: 'json',
                 delay: 250,
                 data: function(params) {
                     return {
+                        type: 'district',
                         q: params.term, // search term
                         page: params.page
                     };
