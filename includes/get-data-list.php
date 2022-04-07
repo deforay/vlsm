a871d9bb1cbf1dfc30c91b7da8027ae1dddbed58
Thanaseelan K <thanaseelan@deforay.com>
2022-04-07 14:04:46 +0530

    Created ajax search phone no while select focal and clinician person

---
+++ includes/get-data-list.php
@@ -0,0 +1,42 @@
+<?php
+$geoDb = new \Vlsm\Models\GeoLocations($db);
+$generalDb = new \Vlsm\Models\General($db);
+$text = '';
+$field = $_GET['fieldName'];
+$table = $_GET['tableName'];
+$returnField = (isset($_GET['returnField']) && !empty($_GET['returnField'])) ? $_GET['returnField'] : null;
+$limit = (isset($_GET['limit']) && !empty($_GET['limit'])) ? $_GET['limit'] : null;
+$text = (isset($_GET['q']) && !empty($_GET['q'])) ? $_GET['q'] : null;
+
+if (isset($text) && !empty($text) && $text != "") {
+    if (isset($returnField) && $returnField != "") {
+        $cQuery = "SELECT DISTINCT $returnField FROM $table WHERE $field like '%" . $text . "%' AND $field is not null";
+    } else {
+        $cQuery = "SELECT DISTINCT $field FROM $table WHERE $field like '%" . $text . "%' AND $field is not null";
+    }
+} else {
+    if (isset($returnField) && $returnField != "") {
+        $cQuery = "SELECT DISTINCT $returnField FROM $table WHERE $field is not null";
+    } else {
+        $cQuery = "SELECT DISTINCT $field FROM $table WHERE $field is not null";
+    }
+}
+if (isset($limit) && !empty($limit) && $limit > 0) {
+    $cQuery .= " limit " . $limit;
+}
+$cResult = $db->rawQuery($cQuery);
+if (isset($returnField) && $returnField != "") {
+    echo $cResult[0][$returnField];
+} else {
+    $echoResult = array();
+    if (count($cResult) > 0) {
+        foreach ($cResult as $row) {
+            $echoResult[] = array("id" => $row[$field], "text" => ucwords($row[$field]));
+        }
+    } else {
+        $echoResult[] = array("id" => $text, 'text' => $text);
+    }
+
+    $result = array("result" => $echoResult);
+    echo json_encode($result);
+}
