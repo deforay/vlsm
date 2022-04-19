6b04f2a8cb8d9ff241897ff2731002c31785c137
Amit Dugar <amit@deforay.com>
2022-04-19 15:31:50 +0530

    Minor changes in Manifest generation

--- specimen-referral-manifest/generateHepatitisManifest.php
+++ specimen-referral-manifest/generateHepatitisManifest.php
@@ -196,7 +196,7 @@ if (trim($id) != '') {
         $tbl .= '</table>';
         //$tbl.='<br/><br/><b style="text-align:left;">Printed On:  </b>'.date('d/m/Y H:i:s');
         $pdf->writeHTMLCell('', '', 11, $pdf->getY(), $tbl, 0, 1, 0, true, 'C', true);
-        $filename = trim($bResult[0]['package_code']) . '-' . date('Y-m-d') . '-Manifest.pdf';
+        $filename = trim($bResult[0]['package_code']) . '-' . date('Ymd') . '-' . $general->generateRandomString(6) . '-Manifest.pdf';
         $pdf->Output(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'package_barcode' . DIRECTORY_SEPARATOR . $filename, "F");
         echo $filename;
     }
