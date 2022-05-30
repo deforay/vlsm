6b01fba785b26a84eb255d75c563ab53bad58892
Thanaseelan K <thanaseelan@deforay.com>
2022-05-30 16:30:37 +0530

    South sudan vl form sample recevied from hub and lab added as required fields

--- vl/results/forms/update-southsudan-result.php
+++ vl/results/forms/update-southsudan-result.php
@@ -608,15 +608,15 @@ $disable = "disabled = 'disabled'";
 												</div>
 												<div class="row">
 													<div class="col-md-4">
-														<label class="col-lg-5 control-label" for="sampleReceivedAtHubOn">Date Sample Received at Hub (PHL) </label>
+														<label class="col-lg-5 control-label" for="sampleReceivedAtHubOn">Date Sample Received at Hub (PHL) <span class="mandatory">*</span></label>
 														<div class="col-lg-7">
-															<input type="text" class="form-control dateTime" id="sampleReceivedAtHubOn" name="sampleReceivedAtHubOn" placeholder="Sample Received at HUB Date" title="Please select sample received at HUB date" value="<?php echo $vlQueryInfo['sample_received_at_hub_datetime']; ?>" />
+															<input type="text" class="form-control dateTime isRequired" id="sampleReceivedAtHubOn" name="sampleReceivedAtHubOn" placeholder="Sample Received at HUB Date" title="Please select sample received at HUB date" value="<?php echo $vlQueryInfo['sample_received_at_hub_datetime']; ?>" />
 														</div>
 													</div>
 													<div class="col-md-4">
-														<label class="col-lg-5 control-label" for="sampleReceivedOn">Date Sample Received at Testing Lab </label>
+														<label class="col-lg-5 control-label" for="sampleReceivedOn">Date Sample Received at Testing Lab <span class="mandatory">*</span></label>
 														<div class="col-lg-7">
-															<input type="text" class="form-control labSection" id="sampleReceivedOn" name="sampleReceivedOn" placeholder="Sample Received Date" title="Please select sample received date" value="<?php echo $vlQueryInfo['sample_received_at_vl_lab_datetime']; ?>" />
+															<input type="text" class="form-control labSection isRequired" id="sampleReceivedOn" name="sampleReceivedOn" placeholder="Sample Received Date" title="Please select sample received date" value="<?php echo $vlQueryInfo['sample_received_at_vl_lab_datetime']; ?>" />
 														</div>
 													</div>
 													<div class="col-md-4">
