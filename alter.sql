9a31e46b39f3eb9f225b9eb5eba3d7eae5825237
Jeyabanu <jeyabanu@deforay.com>
2022-06-30 17:16:44 +0530

    added audit form page to display rows of audit form tables according to Test type,sample code and highlighted the differences for each record

--- alter.sql
+++ alter.sql
@@ -3123,3 +3123,8 @@ VALUES
 
 -- Amit 21-Jun-2022 version 4.5.2
 UPDATE `system_config` SET `value` = '4.5.2' WHERE `system_config`.`name` = 'sc_version';
+
+
+-- Jeyabanu 27-Jun-2022
+INSERT INTO `privileges` (`privilege_id`, `resource_id`, `privilege_name`, `display_name`) VALUES 
+(NULL, 'common-reference', 'audit-trail-form.php', 'Audit Trail Form');
