4ec33763cdb281329c62f0fb38189fbb37c8e4a6
Jeyabanu <jeyabanu@deforay.com>
2022-07-01 18:15:23 +0530

    removed unwanted codes

--- admin/monitoring/audit-trail.php
+++ admin/monitoring/audit-trail.php
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css" />
 <?php
 $title = _("Audit Trail");
 require_once(APPLICATION_PATH . '/header.php');
@@ -12,23 +11,14 @@ else {
 	$table_name="audit_form_vl";
 	$sample_code="VL062288148";
 }
-$columns_sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'vlsm' AND table_name='$table_name'";
-$col_result = $db->rawQuery($columns_sql);
+
 function getDifference($arr1,$arr2)
 {
     $diff = array_merge(array_diff_assoc($arr1,$arr2),array_diff_assoc($arr2,$arr1));
     return $diff;
 }
 ?>
-<style>
-	.select2-selection__choice {
-		color: black !important;
-	}
 
-	th {
-		display: revert !important;
-	}
-</style>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
 	<!-- Content Header (Page header) -->
@@ -39,7 +29,6 @@ function getDifference($arr1,$arr2)
 			<li class="active"><?php echo _("Audit Trail"); ?></li>
 		</ol>
 	</section>
-
 	<!-- Main content -->
 	<section class="content">
 		<div class="row">
@@ -78,49 +67,33 @@ function getDifference($arr1,$arr2)
 					<!-- /.box-header -->
 					<div class="box-body">
 					<table>
-  <thead>
-    <tr>
-      <?php
-      $columns_sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'vlsm' AND table_name='$table_name'";
-      
-      $result_column = $db->rawQuery($columns_sql);
-      $col_arr = array();
-     foreach($result_column as $col)
-      {
-        $col_arr[] = $col['COLUMN_NAME'];
-        ?>
-        <th>
-          <?php //echo ucwords(str_replace('_',' ',$col['COLUMN_NAME'])); 
-          echo $col['COLUMN_NAME'];
-          ?>
-          <?php } ?>
-        </th>
-    </tr>
-  </thead>
-  <tbody>
+						<thead>
+							<tr>
+							<?php
+							$columns_sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '" . SYSTEM_CONFIG['dbName'] . "' AND table_name='$table_name'";
+							$result_column = $db->rawQuery($columns_sql);
+							$col_arr = array();
+							foreach($result_column as $col)
+							{
+								$col_arr[] = $col['COLUMN_NAME'];
+								?>
+								<th>
+								<?php //echo ucwords(str_replace('_',' ',$col['COLUMN_NAME'])); 
+								echo $col['COLUMN_NAME'];
+								?>
+								<?php } ?>
+								</th>
+							</tr>
+						</thead>
+						<tbody>
 
               <?php
-              if(isset($_POST['sample_code']))
-              		$code=$_POST['sample_code'];
-              	else
-              		$code="";
+
 
               $sql = "SELECT a.*, modifier.user_name as last_modified_by, creator.user_name as req_created_by from $table_name as a 
 			  LEFT JOIN user_details as creator ON a.request_created_by = creator.user_id LEFT JOIN user_details as modifier ON a.last_modified_by = modifier.user_id 
 			  WHERE sample_code = '$sample_code' OR remote_sample_code = '$sample_code'";
-              $result = $db->rawQuery($sql);
-
-              $posts=array();
-              
-              foreach($result as $row) {
-                
-                $posts[] = $row;
-                
-              }
-            
-
-         //  echo 'valu-'.count($posts);
-            //echo '<pre>'; print_r($posts); die();
+              $posts = $db->rawQuery($sql);
             for($i=0;$i<count($posts);$i++)
             {
               $k = ($i-1);
@@ -135,10 +108,8 @@ function getDifference($arr1,$arr2)
             ?>
                   <td class="compare_col-<?php echo $i.'-'.$j; ?>">
                     <?php 
-                     //  echo $i.'--'.($i-1);
                     if($i>0)
                     {
-                     
                           if(!empty($arr_diff[$col_arr[$j]]) && $arr_diff[$col_arr[$j]]!=$posts[$i][$col_arr[$j]] && !empty($posts[$i][$col_arr[$j]]))
                           {
                           echo '<style type="text/css">
