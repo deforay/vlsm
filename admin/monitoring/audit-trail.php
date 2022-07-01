2e49fd12246960536cdad7165bc75ac92fb535bd
Jeyabanu <jeyabanu@deforay.com>
2022-07-01 19:35:44 +0530

    modified alignment and some changes done

--- admin/monitoring/audit-trail.php
+++ admin/monitoring/audit-trail.php
@@ -8,8 +8,8 @@ if(isset($_POST['testType']))
 	$sample_code =$_POST['sampleCode'];
 }
 else {
-	$table_name="audit_form_vl";
-	$sample_code="VL062288148";
+	$table_name="";
+	$sample_code="";
 }
 
 function getDifference($arr1,$arr2)
@@ -32,12 +32,13 @@ function getDifference($arr1,$arr2)
 	<!-- Main content -->
 	<section class="content">
 		<div class="row">
-			<div class="col-xs-12">
+
+		<div class="col-xs-12">
 				<div class="box">
 				<form name="form1" action="audit-trail.php" method="post" id="searchForm">
 					<table class="table" cellpadding="1" cellspacing="3" style="margin-left:1%;margin-top:20px;width:98%;">
 						<tr>
-							<td><b><?php echo _("Test Type"); ?>*&nbsp;:</b></td>
+							<td><b><?php echo _("Test Type"); ?>&nbsp;:</b></td>
 							<td>
 							<select style="width:220px;" class="form-control" id="testType" name="testType" title="<?php echo _('Type of Test'); ?>">
 							<option value="">--Choose Test Type--</option>
@@ -48,22 +49,27 @@ function getDifference($arr1,$arr2)
 							<option <?php if(isset($_POST['testType']) && $_POST['testType']=="audit_form_tb") echo "selected='selected'"; ?> value="audit_form_tb">TB</option>
 							</select>
 							</td>
-							<td><b><?php echo _("Sample Code"); ?>&nbsp;:</b></td>
+							<td>&nbsp;<b><?php echo _("Sample Code"); ?>&nbsp;:</b></td>
 							<td>
-								<input type="text" value="<?php if(isset($_POST['sampleCode'])) echo $_POST['sampleCode']; else echo ""; ?>" name="sampleCode" id="sampleCode" class="form-control" />
-								<!--<select style="width:220px;" class="form-control" id="sampleCode" name="sampleCode" title="<?php echo _('Please select the Sample code'); ?>">
-								</select>-->
+							<input type="text" value="<?php if(isset($_POST['sampleCode'])) echo $_POST['sampleCode']; else echo ""; ?>" name="sampleCode" id="sampleCode" class="form-control" />
 							</td>
-						</tr>
+						
 						<tr>
-							<td></td>
-							<td style=" display: contents; ">
-								<button type="submit" value="Submit" class="btn btn-primary btn-sm"><span><?php echo _("Submit"); ?></span></button>
-								<a href="/admin/monitoring/audit-trail.php" class="btn btn-danger btn-sm" style=" margin-left: 15px; "><span><?php echo _("Clear"); ?></span></button>
+							<td colspan="4">&nbsp;<input type="submit"  value="<?php echo _("Submit"); ?>" class="btn btn-success btn-sm">
+								&nbsp;<button class="btn btn-danger btn-sm" onclick="document.location.href = document.location"><span><?php echo _("Reset"); ?></span></button>
 							</td>
 						</tr>
+
 					</table>
-</form>
+				</form>	
+				</div>
+			</div>
+<?php
+if(!empty($sample_code))
+{
+?>
+			<div class="col-xs-12">
+				<div class="box">
 					<!-- /.box-header -->
 					<div class="box-body">
 					<table>
@@ -87,58 +93,65 @@ function getDifference($arr1,$arr2)
 						</thead>
 						<tbody>
 
-              <?php
-
-
-              $sql = "SELECT a.*, modifier.user_name as last_modified_by, creator.user_name as req_created_by from $table_name as a 
-			  LEFT JOIN user_details as creator ON a.request_created_by = creator.user_id LEFT JOIN user_details as modifier ON a.last_modified_by = modifier.user_id 
-			  WHERE sample_code = '$sample_code' OR remote_sample_code = '$sample_code'";
-              $posts = $db->rawQuery($sql);
-            for($i=0;$i<count($posts);$i++)
-            {
-              $k = ($i-1);
-              $arr_diff = getDifference($posts[$i],$posts[$k]);
-               
-            ?>
-            <tr>
-            <?php
-          
-            for($j=0;$j<count($col_arr);$j++)
-            {
-            ?>
-                  <td class="compare_col-<?php echo $i.'-'.$j; ?>">
-                    <?php 
-                    if($i>0)
-                    {
-                          if(!empty($arr_diff[$col_arr[$j]]) && $arr_diff[$col_arr[$j]]!=$posts[$i][$col_arr[$j]] && !empty($posts[$i][$col_arr[$j]]))
-                          {
-                          echo '<style type="text/css">
-                            .compare_col-'.$i.'-'.$j.' {
-                              background: orange;
-                              color:black;
-                            }
-                            </style>';
-                          }
-                          else
-                        {
-                          echo '<style type="text/css">
-                            .compare_col-'.$i.'-'.$j.' {
-                              background: white;
-                              color:black;
-                            }
-                            </style>';
-                        }
-                        
-                  }                   
-                    
-                   echo $posts[$i][$col_arr[$j]];
-                    ?>
-                  </td>
-            <?php }
-            ?>
-  </tr>
+              	<?php
+				
+				$sql = "SELECT a.*, modifier.user_name as last_modified_by, creator.user_name as req_created_by from $table_name as a 
+				LEFT JOIN user_details as creator ON a.request_created_by = creator.user_id LEFT JOIN user_details as modifier ON a.last_modified_by = modifier.user_id 
+				WHERE sample_code = '$sample_code' OR remote_sample_code = '$sample_code'";
+				$posts = $db->rawQuery($sql);
+				if(count($posts)>0) 
+				{
+					for($i=0;$i<count($posts);$i++)
+					{
+					$k = ($i-1);
+					$arr_diff = getDifference($posts[$i],$posts[$k]);
+					
+					?>
+					<tr>
+					<?php
+				
+					for($j=0;$j<count($col_arr);$j++)
+					{
+					?>
+					<td class="compare_col-<?php echo $i.'-'.$j; ?>">
+						<?php 
+						if($i>0)
+						{
+							if(!empty($arr_diff[$col_arr[$j]]) && $arr_diff[$col_arr[$j]]!=$posts[$i][$col_arr[$j]] && !empty($posts[$i][$col_arr[$j]]))
+							{
+							echo '<style type="text/css">
+								.compare_col-'.$i.'-'.$j.' {
+								background: orange;
+								color:black;
+								}
+								</style>';
+							}
+							else
+							{
+							echo '<style type="text/css">
+								.compare_col-'.$i.'-'.$j.' {
+								background: white;
+								color:black;
+								}
+								</style>';
+							}
+							
+					}                   
+						
+					echo $posts[$i][$col_arr[$j]];
+						?>
+					</td>
+				<?php }
+				?>
+			</tr>
   <?php
 }
+				}
+			
+				else
+				{
+					echo "<tr align='center'><td colspan='10'>No records available</td></tr>";
+				}
   ?>
    
   </tbody>
@@ -149,54 +162,15 @@ function getDifference($arr1,$arr2)
 				<!-- /.box -->
 			</div>
 			<!-- /.col -->
+			<?php
+}
+?>
 		</div>
 		<!-- /.row -->
 	</section>
 	<!-- /.content -->
 </div>
-<script type="text/javascript" src="/assets/plugins/daterangepicker/moment.min.js"></script>
-<script type="text/javascript" src="/assets/plugins/daterangepicker/daterangepicker.js"></script>
-<script type="text/javascript">
-/*	function getSampleCodeList(samplecode)
-	{
-		//loadVlRequestData();
-		testType=$("#testType").val();
-		$.post({
-			type: "POST",
-			url: '/admin/monitoring/get-sample-code-list.php',
-			data: {table: testType, type:samplecode},
-			success: function(data){
-				obj = $.parseJSON(data);
-				$("#sampleCode").html('');
-				$.each( obj, function( key, value ) {
-					$("#sampleCode").append("<option value="+value.sample_code+">"+value.sample_code+"</option>") ;
-				});
-			}
-		});
-	}*/
 
-
-	/*	$("#sampleCode").autocomplete({
-  source: function(request, response) {
-    $.getJSON("get-sample-code-list.php", { testType: $('#testType').val(), code : $('#sampleCode').val() }, 
-              response);
-  },
-  minLength: 2,
-  select: function(event, ui){
-    alert('hi');
-  }
-});
-*/
-
-	//	getSampleCodeList($("#testType").val());
-		/*$("#sampleCode").select2({
-			placeholder: "<?php echo _("Select Sample Code"); ?>"
-		});
-*/
-	
-
-	
-</script>
 <?php
 require_once(APPLICATION_PATH . '/footer.php');
 ?>
