cd31884f2c61c5b72fefa20cd8aff2c0dbc7e70b
Amit Dugar <amit@deforay.com>
2022-07-08 12:33:46 +0530

    Fixed edit user profile issue

--- users/editProfileHelper.php
+++ users/editProfileHelper.php
@@ -3,7 +3,9 @@ ob_start();
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-$userDb = new \Vlsm\Models\Users();
+
+
+$userModel = new \Vlsm\Models\Users();
 $tableName = "user_details";
 $upId = 0;
 /* To check the password update from the API */
@@ -18,26 +20,7 @@ if ($fromApiTrue) {
 }
 
 try {
-    /* Check hash login id exist */
-    $sha1protect = false;
-    $hashCheckQuery = "SELECT `user_id`, `login_id`, `hash_algorithm`, `password` FROM user_details WHERE `login_id` = ?";
-    $hashCheck = $db->rawQueryOne($hashCheckQuery, array($db->escape($_POST['userName'])));
-    if (isset($hashCheck) && !empty($hashCheck['user_id']) && !empty($hashCheck['hash_algorithm'])) {
-        if ($hashCheck['hash_algorithm'] == 'sha1') {
-            $password = sha1($_POST['password'] . SYSTEM_CONFIG['passwordSalt']);
-            $sha1protect = true;
-        }
-        if ($hashCheck['hash_algorithm'] == 'phb') {
-            if (!password_verify($db->escape($_POST['password']), $hashCheck['password'])) {
-                $_SESSION['alertMsg'] = _("Invalid password!");
-                header("location:editProfile.php");
-            }
-            $password = $userDb->passwordHash($db->escape($_POST['password']), $userId);
-        }
-    } else {
-        $password = sha1($_POST['password'] . SYSTEM_CONFIG['passwordSalt']);
-    }
-    // die($password);
+    $password = sha1($_POST['password'] . SYSTEM_CONFIG['passwordSalt']);
     $queryParams = array($password);
     $admin = $db->rawQuery("SELECT * FROM user_details as ud WHERE ud.password = ?", $queryParams);
     if (count($admin) > 0) {
@@ -56,20 +39,26 @@ try {
             $db = $db->where('user_name', $data['user_name']);
         } else {
             if (isset($_POST['password']) && trim($_POST['password']) != "") {
+
+                $newPassword = $userModel->passwordHash($db->escape($_POST['password']), $userId);
+
                 if (SYSTEM_CONFIG['recency']['crosslogin']) {
                     $client = new \GuzzleHttp\Client();
                     $url = rtrim(SYSTEM_CONFIG['recency']['url'], "/");
                     $result = $client->post($url . '/api/update-password', [
                         'form_params' => [
                             'u' => $_POST['email'],
-                            't' => $password
+                            't' => $newPassword
                         ]
                     ]);
                     $response = json_decode($result->getBody()->getContents());
 
                     if ($response->status == 'fail') {
+                        error_log('Recency profile not updated! for the user->' . $_POST['userName']);
                     }
                 }
+                $data['password'] = $newPassword;
+                $data['hash_algorithm'] = 'phb';
                 $data['force_password_reset'] = $_SESSION['forcePasswordReset'] = 0;
             }
             $db = $db->where('user_id', $userId);
@@ -87,12 +76,7 @@ try {
                 print_r(json_encode($response));
             }
         }
-        /* Update Phb hash password */
-        if ($sha1protect) {
-            $password = $userDb->passwordHash($db->escape($_POST['password']), $userId);
-            $db = $db->where('user_id', $userId);
-            $db->update('user_details', array('password' => $password, 'hash_algorithm' => 'phb'));
-        }
+
         if ($fromApiFalse) {
             $_SESSION['alertMsg'] = _("Your profile changes have been saved. You can continue using VLSM. Please click on any menu on the left to navigate");
         }
