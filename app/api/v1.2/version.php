73da1b3eb371cd8c26b7ac65b91471ba1259ce7a
Thanaseelan K <thanaseelan@deforay.com>
2022-08-16 17:39:41 +0530

    Removed v1.2 directory

--- app/api/v1.2/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_unset(); // no need of session in json response
-header('Content-Type: application/json; charset=utf-8');
-
-// return VLSM Version
-$payload = array('version' => VERSION);
-echo json_encode($payload);
