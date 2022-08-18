b6ec2a1fa97170176fda33ec0b04b4316e798ed0
Amit Dugar <amit@deforay.com>
2022-08-18 12:08:41 +0530

    Minor changes in startup.php

--- app/vl/reference/save-vl-sample-rejection-reasons-helper.php
+++ app/vl/reference/save-vl-sample-rejection-reasons-helper.php
@@ -4,7 +4,7 @@ if (session_status() == PHP_SESSION_NONE) {
 	session_start();
 }
 // echo "<pre>";print_r($_POST);die;
-  
+
 $general = new \Vlsm\Models\General();
 $tableName = "r_vl_sample_rejection_reasons";
 $primaryKey = "rejection_reason_id";
@@ -14,21 +14,21 @@ try {
 
 		$data = array(
 			'rejection_reason_name' 	=> $_POST['rejectionReasonName'],
-            'rejection_type' 			=> $_POST['rejectionType'],
-            'rejection_reason_code'	=> $_POST['rejectionReasonCode'],
-            'rejection_reason_status' 	=> $_POST['rejectionReasonStatus'],
+			'rejection_type' 			=> $_POST['rejectionType'],
+			'rejection_reason_code'	=> $_POST['rejectionReasonCode'],
+			'rejection_reason_status' 	=> $_POST['rejectionReasonStatus'],
 			'updated_datetime' 			=> $general->getCurrentDateTime()
 		);
 
-		if(isset($_POST['rejectionReasonId']) && $_POST['rejectionReasonId'] != ""){
+		if (isset($_POST['rejectionReasonId']) && $_POST['rejectionReasonId'] != "") {
 			$db = $db->where($primaryKey, base64_decode($_POST['rejectionReasonId']));
-        	$lastId = $db->update($tableName, $data);
-		} else{
+			$lastId = $db->update($tableName, $data);
+		} else {
 			$data['data_sync'] = 0;
 			$db->insert($tableName, $data);
 			$lastId = $db->getInsertId();
 		}
-        if($lastId > 0){
+		if ($lastId > 0) {
 			$_SESSION['alertMsg'] = _("VL Sample Rejection Reasons details added successfully");
 			$general->activityLog('VL Sample Rejection Reasons For VL', $_SESSION['userName'] . ' added new reference Sample Rejection Reasons for VL  ' . $_POST['rejectionReasonName'], 'vl-reference');
 		}
