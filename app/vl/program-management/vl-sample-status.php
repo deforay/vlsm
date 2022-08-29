632fb120abdd4858a2e3405483fb4adf62fb9bcb
Jeyabanu <jeyabanu@deforay.com>
2022-08-29 13:50:07 +0530

    Minor changes has been done

--- app/vl/program-management/vl-sample-status.php
+++ app/vl/program-management/vl-sample-status.php
@@ -17,7 +17,7 @@ $sResult = $db->rawQuery($sQuery);
 $facilitiesDb = new \Vlsm\Models\Facilities();
 $sarr = $general->getSystemConfig();
 
-if (isset($sarr['sc_user_type']) && $sarr['sc_user_type'] == 'vluser') {
+if (isset($sarr['sc_user_type']) && $sarr['sc_user_type'] == 'vluser' && !empty($sarr['sc_testing_lab_id'])) {
 	$testingLabs = $facilitiesDb->getTestingLabs('vl', true, false, "facility_id = " . $sarr['sc_testing_lab_id']);
 } else {
 	$testingLabs = $facilitiesDb->getTestingLabs('vl');
