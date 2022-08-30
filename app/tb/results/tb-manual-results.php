5ca5c32e5c0e63434dd708e402bf6affc0b27e62
Amit Dugar <amit@deforay.com>
2022-08-30 19:14:06 +0530

    Cosmetic fixes - Icons and text

--- app/tb/results/tb-manual-results.php
+++ app/tb/results/tb-manual-results.php
@@ -52,7 +52,7 @@ $testingLabsDropdown = $general->generateSelectOptions($testingLabs, null, "-- S
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-        <h1><i class="fa-solid fa-pen-to-square"></i> <?php echo _("Enter TB Result Manually"); ?></h1>
+        <h1><i class="fa-solid fa-list-check"></i> <?php echo _("Enter TB Result Manually"); ?></h1>
         <ol class="breadcrumb">
             <li><a href="/dashboard/index.php"><i class="fa-solid fa-chart-pie"></i> <?php echo _("Home"); ?> </a></li>
             <li class="active"><?php echo _("Enter TB Result Manually"); ?></li>
