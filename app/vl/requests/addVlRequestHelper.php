0dc7c7a2f843e3e93a82e38aa1f7f59cfc467556
Thanaseelan K <thanaseelan@deforay.com>
2022-09-30 13:25:57 +0530

    Updated VL result datalst field for ss vl form

--- app/vl/requests/addVlRequestHelper.php
+++ app/vl/requests/addVlRequestHelper.php
@@ -183,14 +183,11 @@ try {
     }
 
 
-    if (isset($_POST['bdl']) && $_POST['bdl'] == 'yes' && $isRejected === false) {
+    if (isset($_POST['vlResult']) && $_POST['vlResult'] == 'Below Detection Level' && $isRejected === false) {
         $_POST['vlResult'] = 'Below Detection Level';
         $_POST['vlLog'] = null;
     }
-    if (
-        (isset($_POST['failed']) && $_POST['failed'] == 'yes')
-        || in_array(strtolower($_POST['vlResult']), ['fail', 'failed', 'failure', 'error', 'err'])
-    ) {
+    if ((isset($_POST['vlResult']) && $_POST['vlResult'] == 'Failed') || in_array(strtolower($_POST['vlResult']), ['fail', 'failed', 'failure', 'error', 'err'])) {
         $finalResult = $_POST['vlResult'] = $_POST['vlResult']  ?: 'Failed';
         $_POST['vlLog'] = null;
         $resultStatus = 5; // Invalid/Failed
@@ -238,7 +235,7 @@ try {
         $_POST['reviewedOn'] = null;
     }
 
-    $finalResult = (isset($_POST['hivDetection']) && $_POST['hivDetection'] != '') ? $_POST['hivDetection']. ' ' . $finalResult :  $finalResult;
+    $finalResult = (isset($_POST['hivDetection']) && $_POST['hivDetection'] != '') ? $_POST['hivDetection'] . ' ' . $finalResult :  $finalResult;
 
     $vldata = array(
         'vlsm_instance_id'                      => $instanceId,
