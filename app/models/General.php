1929fc682ad4bc284b9530d22e29f4958b92f285
Amit Dugar <amit@deforay.com>
2022-10-07 14:34:03 +0530

    Minor fixes on EID Print results page

--- app/models/General.php
+++ app/models/General.php
@@ -806,7 +806,9 @@ class General
 
     public function isJSON($string)
     {
-        return is_string($string) && is_array(json_decode($string, true)) && (json_last_error() == JSON_ERROR_NONE) ? true : false;
+        return (is_string($string) &&
+            is_array(json_decode($string, true)) &&
+            (json_last_error() == JSON_ERROR_NONE)) ? true : false;
     }
 
     public function prettyJson($json)
