b1eea2040dee82c338d039635e0674c88c9352f4
Jeyabanu <jeyabanu@deforay.com>
2022-10-10 17:47:25 +0530

    modified radio buttons using css

--- app/roles/editRole.php
+++ app/roles/editRole.php
@@ -44,6 +44,67 @@ if ($priInfo) {
 	.labelName {
 		font-size: 13px;
 	}
+	.switch-field {
+	display: flex;
+	margin: 30px 0 36px 90px;
+	overflow: hidden;
+}
+
+.switch-field input {
+	position: absolute !important;
+	clip: rect(0, 0, 0, 0);
+	height: 1px;
+	width: 1px;
+	border: 0;
+	overflow: hidden;
+}
+
+.switch-field label {
+	background-color: #e4e4e4;
+	color: rgba(0, 0, 0, 0.6);
+	font-size: 14px;
+	line-height: 1;
+	text-align: center;
+	padding: 8px 16px;
+	margin-right: -1px;
+	border: 1px solid rgba(0, 0, 0, 0.2);
+	box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.3), 0 1px rgba(255, 255, 255, 0.1);
+	transition: all 0.1s ease-in-out;
+}
+
+.switch-field label:hover {
+	cursor: pointer;
+}
+
+.switch-field input:checked + label {
+	background-color: #3c8dbc;
+	box-shadow: none;
+}
+
+.switch-field label:first-of-type {
+	border-radius: 4px 0 0 4px;
+}
+
+.switch-field label:last-of-type {
+	border-radius: 0 4px 4px 0;
+}
+
+/* This is just for CodePen. */
+
+.form {
+	max-width: 600px;
+	font-family: "Lucida Grande", Tahoma, Verdana, sans-serif;
+	font-weight: normal;
+	line-height: 1.625;
+	margin: 8px auto;
+	padding: 16px;
+}
+
+h2 {
+	font-size: 18px;
+	margin-bottom: 8px;
+}
+
 </style>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
@@ -176,10 +237,11 @@ if ($priInfo) {
 											echo "<div class='col-lg-3' style='margin-top:5px;border:1px solid #eee;padding:10px;'>
                               <strong>" . $privilege['display_name'] . "</strong>
                               <br>
-                              <label><input type='radio' class='cekAll layCek'  name='resource[" . $privilege['privilege_id'] . "]" . "' value='allow' $allowChecked> <em class='fa-solid fa-check'></em></label>
-							  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                              <label><input type='radio' class='unCekAll layCek'  name='resource[" . $privilege['privilege_id'] . "]" . "' value='deny' $denyChecked>  <em class='fa-solid fa-x'></em></label>
-                          </div>";
+							  <div class='switch-field'> 
+                              <input type='radio' class='cekAll layCek'  name='resource[" . $privilege['privilege_id'] . "]" . "' value='allow' id='radio-one".$privilege['privilege_id']."' $allowChecked><label for='radio-one".$privilege['privilege_id']."'>Yes</label>
+								  <input type='radio' class='unCekAll layCek'  name='resource[" . $privilege['privilege_id'] . "]" . "' value='deny' id='radio-two".$privilege['privilege_id']."' $denyChecked> <label for='radio-two".$privilege['privilege_id']."'> No</label>
+                          </div>
+							  </div>";
 										}
 										echo "</td></tr>";
 									}
