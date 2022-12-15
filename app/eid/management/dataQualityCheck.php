603c08d001467b42d6c4054e21cb6d46137b3ec1
Jeyabanu <jeyabanu@deforay.com>
2022-12-15 17:42:16 +0530

    fixed issues in data quality check in eid

--- app/eid/management/dataQualityCheck.php
+++ app/eid/management/dataQualityCheck.php
@@ -136,18 +136,18 @@ if (isset($_POST['formField']) && trim($_POST['formField']) != '') {
                $sWhereSub .= $sWhereSubC;
                $sWhereSub .= "(";
           } else {
-               $sWhereSub .= " OR (";
+               $sWhereSub .= " AND (";
           }
           if($search=='sample_collection_date')
-               $sWhereSub .=  $search . " IS NULL";
+               $sWhereSub .=  'vl.'.$search . " IS NULL";
           else
-               $sWhereSub .= $search . " ='' OR " . $search . " IS NULL";
+               $sWhereSub .= 'vl.'.$search . " ='' OR " . 'vl.'.$search . " IS NULL";
           $sWhereSub .= ")";
      }
      $sWhereSub .= ")";
      $sWhere[] = $sWhereSub;
 }
-
+//echo $sWhereSub; die;
 $dWhere = '';
 if ($_SESSION['instanceType'] == 'remoteuser') {
 
@@ -161,7 +161,7 @@ if ($_SESSION['instanceType'] == 'remoteuser') {
 
 if(isset($sWhere) && count($sWhere)>0)
 {
-    $sWhere = ' AND '.implode(' AND ',$sWhere);
+    $sWhere = ' WHERE '.implode(' AND ',$sWhere);
 }
 else
 {
@@ -179,7 +179,6 @@ if (isset($sOrder) && $sOrder != "") {
 if (isset($sLimit) && isset($sOffset)) {
      $sQuery = $sQuery . ' LIMIT ' . $sOffset . ',' . $sLimit;
 }
-// echo $sQuery;die;
 $rResult = $db->rawQuery($sQuery);
 /* Data set length after filtering */
 $aResultFilterTotal = $db->rawQuery("SELECT vl.eid_id,vl.facility_id,vl.child_name,vl.result,f.facility_name,f.facility_code,s.sample_name,b.batch_code,vl.sample_batch_id,ts.status_name FROM form_eid as vl LEFT JOIN facility_details as f ON vl.facility_id=f.facility_id LEFT JOIN r_vl_sample_type as s ON s.sample_id=vl.specimen_type INNER JOIN r_sample_status as ts ON ts.status_id=vl.result_status LEFT JOIN batch_details as b ON b.batch_id=vl.sample_batch_id $sWhere ORDER BY vl.last_modified_datetime DESC, $sOrder");
