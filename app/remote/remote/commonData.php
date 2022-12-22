507c6ed05b4990c7cb4cbc62c4a5c11e6d51472e
Amit Dugar <amit@deforay.com>
2022-12-23 04:25:15 +0530

    Minor fix in updating remote sync datetime in JSON attribute field

--- app/remote/remote/commonData.php
+++ app/remote/remote/commonData.php
@@ -258,8 +258,8 @@ if ($data['Key'] == 'vlsm-get-remote') {
 
     $general->addApiTracking($transactionId, 'vlsm-system', $counter, 'common-data-sync', 'common', null, $origData, $payload, 'json', $labId);
     
-    $sql = 'UPDATE facility_details SET facility_attributes = JSON_SET(facility_attributes, "$.version", ?, "$.lastHeartBeat", ?) WHERE facility_id = ?';
-    $db->rawQuery($sql, array($version, $general->getCurrentDateTime(), $labId));
+    $sql = 'UPDATE facility_details SET facility_attributes = JSON_SET(COALESCE(facility_attributes, "{}"), "$.lastHeartBeat", ?) WHERE facility_id = ?';
+    $db->rawQuery($sql, array($general->getCurrentDateTime(), $labId));
 
     echo $payload;
 } else {
