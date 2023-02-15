ec2a781530a60acfdc282df98ea1e501682759a4
Jeyabanu <jeyabanu@deforay.com>
2023-02-15 17:46:01 +0530

    made some changes in audit trail

--- app/footer.php
+++ app/footer.php
@@ -52,11 +52,9 @@
 <script src="/assets/js/jquery.maskedinput.js"></script>
 <script src="/assets/js/jquery.blockUI.js"></script>
 <script src="/assets/js/moment.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+<script src="/assets/js/dataTables.buttons.min.js"></script>
+<script src="/assets/js/jszip.min.js"></script>
+<script src="/assets/js/buttons.html5.min.js"></script>
 
 <!-- HTML2CANVAS -->
 
