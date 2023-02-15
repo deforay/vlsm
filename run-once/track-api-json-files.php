bd66a0dbda52de6447cfb36ca6e43402d6c27ed1
Amit Dugar <amit@deforay.com>
2023-02-15 10:40:52 +0530

    Minor fixes in request/response api tracking json storage

--- run-once/track-api-json-files.php
+++ run-once/track-api-json-files.php
@@ -1,34 +1,49 @@
-<?php 
+<?php
 require_once(__DIR__ . '/../startup.php');
 
+ini_set('memory_limit', -1);
+
+if (!file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'track-api')) {
+    mkdir(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'track-api', 0777, true);
+}
+if (!file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'track-api' . DIRECTORY_SEPARATOR . 'requests')) {
+    mkdir(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'track-api' . DIRECTORY_SEPARATOR . 'requests', 0777, true);
+}
+if (!file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'track-api' . DIRECTORY_SEPARATOR . 'responses')) {
+    mkdir(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'track-api' . DIRECTORY_SEPARATOR . 'responses', 0777, true);
+}
+
+
 //To select the list of json data
-$query = "SELECT * FROM track_api_requests WHERE (request_data IS NOT NULL OR response_data IS NOT NULL) AND transaction_id IS NOT NULL LIMIT 1000";
+$query = "SELECT * FROM track_api_requests WHERE (request_data NOT like 'x' AND response_data  NOT like 'x') AND transaction_id IS NOT NULL ORDER BY requested_on";
 $jsonResult = $db->rawQuery($query);
-foreach($jsonResult as $row){
-    if(isset($row['request_data']) && !empty($row['request_data'])){
-        $output = $row['request_data'];
+foreach ($jsonResult as $row) {
+    if (isset($row['request_data']) && !empty($row['request_data']) && $row['request_data'] != 'x' && $row['request_data'] != '[]') {
+        $output = stripslashes($row['request_data']);
         $pathname = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'track-api' . DIRECTORY_SEPARATOR . 'requests' . DIRECTORY_SEPARATOR;
         // Save in json file
-        if(isset($output) && !empty($output)){
+        if (isset($output) && !empty($output)) {
             $filename = $row['transaction_id'] . '.json';
             $fp = fopen($pathname . $filename, 'w');
-            fwrite($fp, json_encode($output));
+            fwrite($fp, $output);
             fclose($fp);
         }
-        $db->where("api_track_id", $row['api_track_id']);
-        $db->update("track_api_requests", array("request_data" => null));
+        //error_log('REQUEST - ' . $row['transaction_id']);
     }
-    if(isset($row['response_data']) && !empty($row['response_data'])){
-        $output = $row['response_data'];
-        $pathname = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'track-api' . DIRECTORY_SEPARATOR . 'response' . DIRECTORY_SEPARATOR;
+    if (isset($row['response_data']) && !empty($row['response_data']) && $row['response_data'] != 'x' && $row['response_data'] != '[]') {
+        $output = stripslashes($row['response_data']);
+        $pathname = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'track-api' . DIRECTORY_SEPARATOR . 'responses' . DIRECTORY_SEPARATOR;
         // Save in json file
-        if(isset($output) && !empty($output)){
+        if (isset($output) && !empty($output)) {
             $filename = $row['transaction_id'] . '.json';
             $fp = fopen($pathname . $filename, 'w');
-            fwrite($fp, json_encode($output));
+            fwrite($fp, $output);
             fclose($fp);
         }
-        $db->where("api_track_id", $row['api_track_id']);
-        $db->update("track_api_requests", array("response_data" => null));
+        //error_log('RESPONSE - ' . $row['transaction_id']);
     }
+
+
+    $db->where("api_track_id", $row['api_track_id']);
+    $db->update("track_api_requests", array("request_data" => 'x', "response_data" => 'x'));
 }
