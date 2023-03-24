11fbe903c72901f53a38d9ba726906d806553baf
Jeyabanu <jeyabanu@deforay.com>
2023-03-24 13:53:01 +0530

    Minor changes done in pdf rwanda

--- dev/alter.sql
+++ dev/alter.sql
@@ -3418,6 +3418,7 @@ INSERT INTO `global_config` (`display_name`, `name`, `value`, `category`, `remot
 ALTER TABLE `form_vl` CHANGE `data_sync` `data_sync` INT NOT NULL DEFAULT 0;
 
 INSERT INTO `global_config` (`display_name`, `name`, `value`, `category`, `remote_sync_needed`, `updated_on`, `updated_by`, `status`) VALUES ('Key', 'key', null, 'general', 'yes', NULL, NULL, 'active');
+
 -- Amit 23-Mar-2023
 ALTER TABLE `r_eid_results` CHANGE `result_id` `result_id` varchar(256) NOT NULL;
 ALTER TABLE `r_tb_results` CHANGE `result_id` `result_id` varchar(256) NOT NULL;
