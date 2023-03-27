1c0454495b28d04e9fca4c945b124b7816abffc6
Amit Dugar <amit@deforay.com>
2023-03-27 18:24:31 +0530

    phpspreadsheet changes

--- app/covid-19/requests/generate-pending-covid19-request-excel.php
+++ app/covid-19/requests/generate-pending-covid19-request-excel.php
@@ -280,7 +280,7 @@ foreach ($output as $rowNo => $rowData) {
         $cellName = $sheet->getCellByColumnAndRow($colNo, $rRowCount)->getColumn();
         $sheet->getStyle($cellName . $rRowCount)->applyFromArray($borderStyle);
         $sheet->getStyle($cellName . $start)->applyFromArray($borderStyle);
-        // $sheet->getDefaultRowDimension($colNo)->setRowHeight(18);
+        // // $sheet->getDefaultRowDimension($colNo)->setRowHeight(18);
         // // $sheet->getColumnDimensionByColumn($colNo)->setWidth(20);
         $sheet->getCellByColumnAndRow($colNo, $rowNo + 4)->setValueExplicit(html_entity_decode($value), \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
         $colNo++;
--- app/eid/management/generate-export-data.php
+++ app/eid/management/generate-export-data.php
@@ -202,7 +202,7 @@ if (isset($_SESSION['eidExportResultQuery']) && trim($_SESSION['eidExportResultQ
 			$cellName = $sheet->getCellByColumnAndRow($colNo, $rRowCount)->getColumn();
 			$sheet->getStyle($cellName . $rRowCount)->applyFromArray($borderStyle);
 			$sheet->getStyle($cellName . $start)->applyFromArray($borderStyle);
-			$sheet->getDefaultRowDimension($colNo)->setRowHeight(18);
+			// $sheet->getDefaultRowDimension($colNo)->setRowHeight(18);
 			// $sheet->getColumnDimensionByColumn($colNo)->setWidth(20);
 			$sheet->getCellByColumnAndRow($colNo, $rowNo + 4)->setValueExplicit(html_entity_decode($value), \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
 			$colNo++;
