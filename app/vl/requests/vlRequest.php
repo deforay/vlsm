25d957097a3d2285dc920973af5f5835e54fe00b
Amit Dugar <amit@deforay.com>
2023-04-15 16:44:05 +0530

    Added condition to check if Recency mode is on on View Test Requests for VL

--- app/vl/requests/vlRequest.php
+++ app/vl/requests/vlRequest.php
@@ -307,17 +307,17 @@ foreach ($srcResults as $list) {
 								<input type="text" id="patientName" name="patientName" class="form-control" placeholder="<?php echo _('Enter Patient Name'); ?>" style="background:#fff;" />
 							</td>
 						</tr>
-						
+
 						<tr>
-						<?php //if (!empty(SYSTEM_CONFIG['recency']['crosslogin']) && SYSTEM_CONFIG['recency']['crosslogin'] === true) { ?>
-						<td><strong><?php echo _("Include Recency Samples"); ?>&nbsp;:</strong></td>
-							<td>
-								<select name="recencySamples" id="recencySamples" class="form-control" title="<?php echo _('Please choose community sample'); ?>" style="width:100%;">
-									<option value="yes"><?php echo _("Yes"); ?></option>
-									<option value="no" selected="selected"><?php echo _("No"); ?></option>
-								</select>
-							</td>
-							<?php// } ?>
+							<?php if (!empty(SYSTEM_CONFIG['recency']['crosslogin']) && SYSTEM_CONFIG['recency']['crosslogin'] === true) { ?>
+								<td><strong><?php echo _("Include Recency Samples"); ?>&nbsp;:</strong></td>
+								<td>
+									<select name="recencySamples" id="recencySamples" class="form-control" title="<?php echo _('Please choose community sample'); ?>" style="width:100%;">
+										<option value="yes"><?php echo _("Yes"); ?></option>
+										<option value="no" selected="selected"><?php echo _("No"); ?></option>
+									</select>
+								</td>
+							<?php } ?>
 							<td><strong><?php echo _("Show Rejected Samples"); ?>&nbsp;:</strong></td>
 							<td>
 								<select name="rejectedSamples" id="rejectedSamples" class="form-control" title="<?php echo _('Please choose show rejected samples'); ?>" style="width:100%;">
@@ -326,7 +326,7 @@ foreach ($srcResults as $list) {
 								</select>
 							</td>
 						</tr>
-						
+
 						<tr>
 							<td colspan="2"><input type="button" onclick="searchVlRequestData();" value="<?php echo _('Search'); ?>" class="btn btn-default btn-sm">
 								&nbsp;<button class="btn btn-danger btn-sm" onclick="document.location.href = document.location"><span><?php echo _("Reset"); ?></span></button>
