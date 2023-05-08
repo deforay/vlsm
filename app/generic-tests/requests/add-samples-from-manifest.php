f3842685488d1a97c30750c1afdfbecc453266f9
Thanaseelan <thanaseelan@deforay.com>
2023-05-08 19:00:31 +0530

    Added samples manifest under development

---
+++ app/generic-tests/requests/add-samples-from-manifest.php
@@ -0,0 +1,31 @@
+<?php
+
+$title = " Add New Test";
+
+require_once(APPLICATION_PATH . '/header.php');
+
+?>
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <!-- <section class="content-header">
+        <h1><em class="fa-solid fa-pen-to-square"></em> Add Samples From Manifest </h1>
+        <ol class="breadcrumb">
+            <li><a href="/dashboard/index.php"><em class="fa-solid fa-chart-pie"></em> Home</a></li>
+            <li class="active">Add Samples From Manifest</li>
+        </ol>
+    </section> -->
+    <section class="content">
+
+        <div class="box box-default">
+            <div class="box-header with-border">
+                <!-- <div class="pull-right" style="font-size:15px;"><span class="mandatory">*</span> indicates required field &nbsp;</div> -->
+            </div>
+            <div class="box-body">
+                <h1>Under Development. Coming Soon.</h1>
+            </div>
+        </div>
+    </section>
+</div>
+<?php
+require_once(APPLICATION_PATH . '/footer.php');
