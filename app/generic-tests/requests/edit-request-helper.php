18d870901332d5c86057bc3166d3ca8f9a884d6f
Thanaseelan <thanaseelan@deforay.com>
2023-05-16 17:42:00 +0530

    Updated lab information alignment

--- app/generic-tests/requests/edit-request-helper.php
+++ app/generic-tests/requests/edit-request-helper.php
@@ -246,6 +246,7 @@ try {
           'sample_received_at_hub_datetime'       => $_POST['sampleReceivedAtHubOn'],
           'sample_received_at_testing_lab_datetime' => $_POST['sampleReceivedDate'],
           'sample_tested_datetime'                => $_POST['sampleTestingDateAtLab'],
+          'reason_for_testing'                    => (isset($_POST['reasonForTesting']) && $_POST['reasonForTesting'] != '') ? $_POST['reasonForTesting'] :  null,
           'result_dispatched_datetime'            => $_POST['resultDispatchedOn'],
           'is_sample_rejected'                    => (isset($_POST['noResult']) && $_POST['noResult'] != '') ? $_POST['noResult'] :  null,
           'reason_for_sample_rejection'           => (isset($_POST['rejectionReason']) && $_POST['rejectionReason'] != '') ? $_POST['rejectionReason'] :  null,
