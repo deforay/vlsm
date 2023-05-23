b3a28a0fd062513f82d0dbed0546bd979c2cbb2d
Thanaseelan <thanaseelan@deforay.com>
2023-05-23 15:00:14 +0530

    Updated update test result page for result interpretations

--- app/generic-tests/results/update-generic-test-result-helper.php
+++ app/generic-tests/results/update-generic-test-result-helper.php
@@ -115,6 +115,11 @@ try {
         $_POST['reviewedOn'] = null;
     }
 
+    $interpretationResult = null;
+    if(isset($_POST['resultType']) && isset($_POST['testType']) && !empty($_POST['resultType']) && !empty($_POST['testType'])){
+        $interpretationResult = $genericTestsService->getInterpretationResults($_POST['testType'], $_POST['result']);
+    }
+
     $vldata = array(
         'vlsm_instance_id'                          => $instanceId,
         'lab_id'                                    => (isset($_POST['labId']) && $_POST['labId'] != '') ? $_POST['labId'] :  null,
@@ -128,6 +133,7 @@ try {
         'reason_for_sample_rejection'               => (isset($_POST['rejectionReason']) && $_POST['rejectionReason'] != '') ? $_POST['rejectionReason'] :  null,
         'rejection_on'                              => (!empty($_POST['rejectionDate'])) ? DateUtility::isoDateFormat($_POST['rejectionDate']) : null,
         'result'                                    => $_POST['result'] ?: null,
+        'final_result_interpretation'               => $interpretationResult,
         'result_reviewed_by'                        => (isset($_POST['reviewedBy']) && $_POST['reviewedBy'] != "") ? $_POST['reviewedBy'] : "",
         'result_reviewed_datetime'                  => (isset($_POST['reviewedOn']) && $_POST['reviewedOn'] != "") ? $_POST['reviewedOn'] : null,
         'testing_lab_focal_person'                  => (isset($_POST['vlFocalPerson']) && $_POST['vlFocalPerson'] != '') ? $_POST['vlFocalPerson'] :  null,
