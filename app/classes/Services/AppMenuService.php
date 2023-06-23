407c591577a607e7b33210b5c481714f24407379
Amit Dugar <amit@deforay.com>
2023-06-23 14:46:15 +0530

    Improved App Menu logic

--- app/classes/Services/AppMenuService.php
+++ app/classes/Services/AppMenuService.php
@@ -2,21 +2,13 @@
 
 namespace App\Services;
 
-
 use MysqliDb;
 use App\Services\UsersService;
 use App\Services\CommonService;
 use App\Registries\ContainerRegistry;
 
-/**
- * Ui functions
- *
- * @author Amit
- */
-
 class AppMenuService
 {
-
     protected ?MysqliDb $db = null;
     protected string $table = 's_app_menu';
     protected CommonService $commonService;
@@ -31,35 +23,20 @@ class AppMenuService
 
     public function getAllActiveMenus($parentId = 0, $menuId = 0)
     {
-
-        /** @var UsersService $usersService */
-        $usersService = ContainerRegistry::get(UsersService::class);
         $this->db->where('status', 'active');
-        if (!empty($menuId) && $menuId > 0) {
+        $this->db->where('parent_id', $parentId);
+        if ($menuId) {
             $this->db->where('id', $menuId);
         }
-        $response = [];
-        $this->db->where('parent_id', $parentId);
         $this->db->orderBy("display_order", "asc");
         $menuData = $this->db->get($this->table);
+        $response = [];
         foreach ($menuData as $key => $menu) {
-            $menu['access'] = true;
-            if ($menu['link']  != "" && !empty($menu['link'])) {
-                $menu['access'] = $usersService->isAllowed($menu['link']);
-            }
-
-            if ($menu['has_children'] == 'yes') {
-                $menu['children'] = $this->getAllActiveMenus($menu['id']);
-                if (empty($menu['children'])) {
-                    $menu['access'] = false;
-                }
-            }
-
-            if ($menu['access']) {
-                $response[$key] = $menu;
-            }
+            $menu['access'] = $menu['link'] ? $this->usersService->isAllowed($menu['link']) : true;
+            $menu['children'] = $menu['has_children'] == 'yes' ? $this->getAllActiveMenus($menu['id']) : [];
+            $menu['access'] = empty($menu['children']) ? false : $menu['access'];
+            $response[$key] = $menu['access'] ? $menu : null;
         }
-
-        return $response;
+        return array_filter($response);
     }
 }
