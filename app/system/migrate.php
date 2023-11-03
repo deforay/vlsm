fe34606019ecb1882f2340b8adf8d44ad04ce803
Amit Dugar <amit@deforay.com>
2023-11-03 22:18:31 +0100

    Added new migration script

---
+++ app/system/migrate.php
@@ -0,0 +1,53 @@
+<?php
+
+// only run from command line
+if (php_sapi_name() !== 'cli') {
+    exit(0);
+}
+
+require_once(__DIR__ . "/../../bootstrap.php");
+
+use PhpMyAdmin\SqlParser\Parser;
+use App\Registries\ContainerRegistry;
+
+/** @var MysqliDb $db */
+$db = ContainerRegistry::get('db');
+
+$db->where('name', 'sc_version');
+$currentVersion = $db->getValue('system_config', 'value');
+$migrationFiles = glob(APPLICATION_PATH . '/../dev/migrations/*.sql');
+usort($migrationFiles, 'version_compare');
+
+$options = getopt("y");  // Parse command line options
+$autoContinueOnError = isset($options['y']);  // Set a flag if -y option is provided
+
+foreach ($migrationFiles as $file) {
+    $version = basename($file, '.sql');
+    if (version_compare($version, $currentVersion, '>=')) {
+        $sql_contents = file_get_contents($file);
+        $parser = new Parser($sql_contents);
+
+        $db->startTransaction();  // Start a new transaction
+
+        foreach ($parser->statements as $statement) {
+            $query = $statement->build();
+            $db->rawQuery($query);
+            if ($db->getLastErrno()) {
+                echo "Error executing query: " . $db->getLastError() . "\n";
+                if (!$autoContinueOnError) {  // Only prompt user if -y option is not provided
+                    echo "Do you want to continue? (y/n): ";
+                    $handle = fopen("php://stdin", "r");
+                    $response = trim(fgets($handle));
+                    fclose($handle);
+                    if (strtolower($response) !== 'y') {
+                        $db->rollback();  // Rollback the transaction on error
+                        exit("Migration aborted by user.\n");
+                    }
+                }
+            }
+        }
+
+        $db->where('name', 'sc_version')->update('system_config', ['value' => $version]);
+        $db->commit();  // Commit the transaction if no error occurred
+    }
+}
