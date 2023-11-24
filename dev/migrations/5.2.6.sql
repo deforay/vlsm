9874894b21ffd8f94184e594772d8a98458a8267
Amit Dugar <amit@deforay.com>
2023-11-24 19:56:18 +0530

    Updated patients table query in migrations

--- dev/migrations/5.2.6.sql
+++ dev/migrations/5.2.6.sql
@@ -2,4 +2,5 @@
 UPDATE `system_config` SET `value` = '5.2.6' WHERE `system_config`.`name` = 'sc_version';
 
 -- Jeyabanu 24-Nov-2023
-ALTER TABLE `patients` CHANGE `patient_code_key` `patient_code_key` INT NOT NULL;
+ALTER TABLE `patients` CHANGE `patient_code_key` `patient_code_key` INT NULL DEFAULT NULL;
+
