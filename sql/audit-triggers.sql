c3d36f2bbb65e0444f56876babf765a6d6f840e3
Amit Dugar <amit@deforay.com>
2024-01-11 14:18:28 +0530

    Untracked composer.lock from repo

--- sql/audit-triggers.sql
+++ sql/audit-triggers.sql
@@ -22,15 +22,15 @@ DROP TRIGGER IF EXISTS form_vl_data__au;
 DROP TRIGGER IF EXISTS form_vl_data__bd;
 
 CREATE TRIGGER form_vl_data__ai AFTER INSERT ON `form_vl` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_vl` SELECT 'insert', NULL, NOW(), d.*
+    INSERT INTO `audit_form_vl` SELECT 'insert', NULL, NOW(), d.*
     FROM `form_vl` AS d WHERE d.vl_sample_id = NEW.vl_sample_id;
 
 CREATE TRIGGER form_vl_data__au AFTER UPDATE ON `form_vl` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_vl` SELECT 'update', NULL, NOW(), d.*
+    INSERT INTO `audit_form_vl` SELECT 'update', NULL, NOW(), d.*
     FROM `form_vl` AS d WHERE d.vl_sample_id = NEW.vl_sample_id;
 
 CREATE TRIGGER form_vl_data__bd BEFORE DELETE ON `form_vl` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_vl` SELECT 'delete', NULL, NOW(), d.*
+    INSERT INTO `audit_form_vl` SELECT 'delete', NULL, NOW(), d.*
     FROM `form_vl` AS d WHERE d.vl_sample_id = OLD.vl_sample_id;
 
 
@@ -53,15 +53,15 @@ DROP TRIGGER IF EXISTS form_eid_data__au;
 DROP TRIGGER IF EXISTS form_eid_data__bd;
 
 CREATE TRIGGER form_eid_data__ai AFTER INSERT ON `form_eid` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_eid` SELECT 'insert', NULL, NOW(), d.*
+    INSERT INTO `audit_form_eid` SELECT 'insert', NULL, NOW(), d.*
     FROM `form_eid` AS d WHERE d.eid_id = NEW.eid_id;
 
 CREATE TRIGGER form_eid_data__au AFTER UPDATE ON `form_eid` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_eid` SELECT 'update', NULL, NOW(), d.*
+    INSERT INTO `audit_form_eid` SELECT 'update', NULL, NOW(), d.*
     FROM `form_eid` AS d WHERE d.eid_id = NEW.eid_id;
 
 CREATE TRIGGER form_eid_data__bd BEFORE DELETE ON `form_eid` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_eid` SELECT 'delete', NULL, NOW(), d.*
+    INSERT INTO `audit_form_eid` SELECT 'delete', NULL, NOW(), d.*
     FROM `form_eid` AS d WHERE d.eid_id = OLD.eid_id;
 
 
@@ -83,15 +83,15 @@ DROP TRIGGER IF EXISTS form_covid19_data__au;
 DROP TRIGGER IF EXISTS form_covid19_data__bd;
 
 CREATE TRIGGER form_covid19_data__ai AFTER INSERT ON `form_covid19` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_covid19` SELECT 'insert', NULL, NOW(), d.*
+    INSERT INTO `audit_form_covid19` SELECT 'insert', NULL, NOW(), d.*
     FROM `form_covid19` AS d WHERE d.covid19_id = NEW.covid19_id;
 
 CREATE TRIGGER form_covid19_data__au AFTER UPDATE ON `form_covid19` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_covid19` SELECT 'update', NULL, NOW(), d.*
+    INSERT INTO `audit_form_covid19` SELECT 'update', NULL, NOW(), d.*
     FROM `form_covid19` AS d WHERE d.covid19_id = NEW.covid19_id;
 
 CREATE TRIGGER form_covid19_data__bd BEFORE DELETE ON `form_covid19` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_covid19` SELECT 'delete', NULL, NOW(), d.*
+    INSERT INTO `audit_form_covid19` SELECT 'delete', NULL, NOW(), d.*
     FROM `form_covid19` AS d WHERE d.covid19_id = OLD.covid19_id;
 
 -- Hepatitis
@@ -112,15 +112,15 @@ DROP TRIGGER IF EXISTS form_hepatitis_data__au;
 DROP TRIGGER IF EXISTS form_hepatitis_data__bd;
 
 CREATE TRIGGER form_hepatitis_data__ai AFTER INSERT ON `form_hepatitis` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_hepatitis` SELECT 'insert', NULL, NOW(), d.*
+    INSERT INTO `audit_form_hepatitis` SELECT 'insert', NULL, NOW(), d.*
     FROM `form_hepatitis` AS d WHERE d.hepatitis_id = NEW.hepatitis_id;
 
 CREATE TRIGGER form_hepatitis_data__au AFTER UPDATE ON `form_hepatitis` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_hepatitis` SELECT 'update', NULL, NOW(), d.*
+    INSERT INTO `audit_form_hepatitis` SELECT 'update', NULL, NOW(), d.*
     FROM `form_hepatitis` AS d WHERE d.hepatitis_id = NEW.hepatitis_id;
 
 CREATE TRIGGER form_hepatitis_data__bd BEFORE DELETE ON `form_hepatitis` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_hepatitis` SELECT 'delete', NULL, NOW(), d.*
+    INSERT INTO `audit_form_hepatitis` SELECT 'delete', NULL, NOW(), d.*
     FROM `form_hepatitis` AS d WHERE d.hepatitis_id = OLD.hepatitis_id;
 
 
@@ -143,15 +143,15 @@ DROP TRIGGER IF EXISTS form_tb_data__au;
 DROP TRIGGER IF EXISTS form_tb_data__bd;
 
 CREATE TRIGGER form_tb_data__ai AFTER INSERT ON `form_tb` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_tb` SELECT 'insert', NULL, NOW(), d.*
+    INSERT INTO `audit_form_tb` SELECT 'insert', NULL, NOW(), d.*
     FROM `form_tb` AS d WHERE d.tb_id = NEW.tb_id;
 
 CREATE TRIGGER form_tb_data__au AFTER UPDATE ON `form_tb` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_tb` SELECT 'update', NULL, NOW(), d.*
+    INSERT INTO `audit_form_tb` SELECT 'update', NULL, NOW(), d.*
     FROM `form_tb` AS d WHERE d.tb_id = NEW.tb_id;
 
 CREATE TRIGGER form_tb_data__bd BEFORE DELETE ON `form_tb` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_tb` SELECT 'delete', NULL, NOW(), d.*
+    INSERT INTO `audit_form_tb` SELECT 'delete', NULL, NOW(), d.*
     FROM `form_tb` AS d WHERE d.tb_id = OLD.tb_id;
 
 
@@ -172,14 +172,14 @@ DROP TRIGGER IF EXISTS form_generic_data__au;
 DROP TRIGGER IF EXISTS form_generic_data__bd;
 
 CREATE TRIGGER form_generic_data__ai AFTER INSERT ON `form_generic` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_generic` SELECT 'insert', NULL, NOW(), d.*
+    INSERT INTO `audit_form_generic` SELECT 'insert', NULL, NOW(), d.*
     FROM `form_generic` AS d WHERE d.sample_id = NEW.sample_id;
 
 CREATE TRIGGER form_generic_data__au AFTER UPDATE ON `form_generic` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_generic` SELECT 'update', NULL, NOW(), d.*
+    INSERT INTO `audit_form_generic` SELECT 'update', NULL, NOW(), d.*
     FROM `form_generic` AS d WHERE d.sample_id = NEW.sample_id;
 
 CREATE TRIGGER form_generic_data__bd BEFORE DELETE ON `form_generic` FOR EACH ROW
-    INSERT IGNORE INTO `audit_form_generic` SELECT 'delete', NULL, NOW(), d.*
+    INSERT INTO `audit_form_generic` SELECT 'delete', NULL, NOW(), d.*
     FROM `form_generic` AS d WHERE d.sample_id = OLD.sample_id;
 
