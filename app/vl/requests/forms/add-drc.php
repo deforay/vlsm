c037b55bcd0d9dc08175d07dd1ec1875d16de1c1
Jeyabanu <jeyabanu@deforay.com>
2024-01-30 17:11:01 +0530

    made small Changes in drc kp field

--- app/vl/requests/forms/add-drc.php
+++ app/vl/requests/forms/add-drc.php
@@ -233,10 +233,7 @@ $sFormat = '';
 										<td style="width: 15% !important;"><label>KP </label></td>
 										<td style="width: 35% !important;">
 											<select class="form-control" name="keyPopulation" id="keyPopulation" title="<? _translate('Please choose KP'); ?>">
-												<option value=""><?= _translate("-- Select --"); ?> </option>
-												<option value="ps"><?= _translate("PS"); ?> </option>
-												<option value="cps"><?= _translate("CPS"); ?> </option>
-												<option value="msm"><?= _translate("MSM"); ?> </option>
+												
 											</select>
 											<input type="text" class="form-control newArtRegimen" name="newArtRegimen" id="newArtRegimen" placeholder="Enter Régime ARV" title="Please enter régime ARV" style="margin-top:1vh;display:none;">
 										</td>
@@ -702,8 +699,10 @@ $sFormat = '';
 	});
 	$("#gender").change(function() {
 		if ($(this).val() == 'female') {
+			$('#keyPopulation').html('<option value=""><?= _translate("-- Select --"); ?> </option><option value="ps"><?= _translate("PS"); ?> </option>');
 			$(".femaleSection").show();
 		} else if ($(this).val() == 'male') {
+			$('#keyPopulation').html('<option value=""><?= _translate("-- Select --"); ?> </option><option value="cps"><?= _translate("CPS"); ?> </option><option value="msm"><?= _translate("MSM"); ?> </option>');
 			$(".femaleSection").hide();
 		}
 	});
