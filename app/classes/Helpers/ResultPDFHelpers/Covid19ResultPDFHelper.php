f8c5674fc5d178e587544feeae0d366ba57788c2
Amit Dugar <amit@deforay.com>
2024-03-29 20:34:18 +0530

    Template PDF functionality added in all PDF Service. Added template PDF functionality in EID and Covid-19 tests for Cameroon

--- app/classes/Helpers/ResultPDFHelpers/Covid19ResultPDFHelper.php
+++ app/classes/Helpers/ResultPDFHelpers/Covid19ResultPDFHelper.php
@@ -17,7 +17,17 @@ class Covid19ResultPDFHelper extends Fpdi
     public ?string $resultPrintedDate;
     public ?array $systemConfig = [];
     public $dataSync;
+    public ?string $trainingTxt = null;
+    private ?string $pdfTemplatePath = null;
+    private bool $templateImported = false;
+    private bool $enableFooter = true; // Default is true to render footer
 
+    public function __construct($orientation = 'P', $unit = 'mm', $format = 'A4', $unicode = true, $encoding = 'UTF-8', $diskCache = false, $pdfTemplatePath = null, $enableFooter = true)
+    {
+        parent::__construct($orientation, $unit, $format, $unicode, $encoding, $diskCache);
+        $this->pdfTemplatePath = $pdfTemplatePath ?? null;
+        $this->enableFooter = $enableFooter;
+    }
 
     //Page header
     public function setHeading($logo, $text, $lab, $title = null, $labFacilityId = null, $formId = null, $facilityInfo = [], $resultPrintedDate = null, $dataSync = null, $systemConfig = null)
@@ -40,93 +50,100 @@ class Covid19ResultPDFHelper extends Fpdi
     //Page header
     public function Header()
     {
-        // Logo
-
-        if (!empty($this->htitle) && $this->htitle != '') {
-            if (!empty($this->logo) && trim($this->logo) != '') {
-                if (file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo)) {
-                    $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo;
-                    if ($this->formId == 3) {
-                        $this->Image($imageFilePath, 10, 5, 25, '', '', '', 'T');
-                    } else {
-                        $this->Image($imageFilePath, 95, 5, 15, '', '', '', 'T');
+        if (!empty($this->pdfTemplatePath) && MiscUtility::fileExists($this->pdfTemplatePath)) {
+            if (!$this->templateImported) {
+                $this->setSourceFile($this->pdfTemplatePath);
+                $this->templateImported = true;
+            }
+            $tplIdx = $this->importPage(1);
+            $this->useTemplate($tplIdx, 0, 0);
+        } else {
+            // Logo
+            if (!empty($this->htitle) && $this->htitle != '') {
+                if (!empty($this->logo) && trim($this->logo) != '') {
+                    if (file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo)) {
+                        $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo;
+                        if ($this->formId == 3) {
+                            $this->Image($imageFilePath, 10, 5, 25, '', '', '', 'T');
+                        } else {
+                            $this->Image($imageFilePath, 95, 5, 15, '', '', '', 'T');
+                        }
                     }
                 }
-            }
-            if ($this->formId == 3) {
-                $this->SetFont('helvetica', 'B', 16);
-                $this->writeHTMLCell(0, 0, 10, 03, $this->text, 0, 0, 0, true, 'C');
-                if (!empty($this->lab) && trim($this->lab) != '') {
-                    $this->SetFont('helvetica', '', 10);
-                    $this->writeHTMLCell(0, 0, 10, 10, strtoupper($this->lab), 0, 0, 0, true, 'C');
+                if ($this->formId == 3) {
+                    $this->SetFont('helvetica', 'B', 16);
+                    $this->writeHTMLCell(0, 0, 10, 03, $this->text, 0, 0, 0, true, 'C');
+                    if (!empty($this->lab) && trim($this->lab) != '') {
+                        $this->SetFont('helvetica', '', 10);
+                        $this->writeHTMLCell(0, 0, 10, 10, strtoupper($this->lab), 0, 0, 0, true, 'C');
+                    }
+                    $this->SetFont('helvetica', 'b', 10);
+                    $this->writeHTMLCell(0, 0, 10, 18, 'Département de Virologie', 0, 0, 0, true, 'C');
+                    $this->SetFont('helvetica', 'u', 10);
+                    $this->writeHTMLCell(0, 0, 10, 25, 'Laboratoire National de Reference pour la Grippe et les Virus Respiratoires', 0, 0, 0, true, 'C');
+                    $this->SetFont('helvetica', 'b', 12);
+                    $this->writeHTMLCell(0, 0, 10, 33, 'RESULTATS DE LABORATOIRE DES ECHANTIONS RESPIRATOIRES', 0, 0, 0, true, 'C');
+                    $this->SetFont('helvetica', 'u', 10);
+                    $this->writeHTMLCell(0, 0, 10, 40, 'TESTES AU COVID-19 PAR RT-PCR en temps réel N°', 0, 0, 0, true, 'C');
+                    $this->writeHTMLCell(0, 0, 15, 48, '<hr>', 0, 0, 0, true, 'C');
+                } else {
+                    $this->SetFont('helvetica', 'B', 16);
+                    $this->writeHTMLCell(0, 0, 10, 18, $this->text ?? '', 0, 0, 0, true, 'C');
+                    if (!empty($this->lab) && trim($this->lab) != '') {
+                        $this->SetFont('helvetica', '', 10);
+                        $this->writeHTMLCell(0, 0, 10, 25, strtoupper($this->lab), 0, 0, 0, true, 'C');
+                    }
+                    $this->SetFont('helvetica', '', 12);
+                    $this->writeHTMLCell(0, 0, 10, 30, 'COVID-19 TEST - PATIENT REPORT', 0, 0, 0, true, 'C');
+                    $this->writeHTMLCell(0, 0, 15, 38, '<hr>', 0, 0, 0, true, 'C');
                 }
-                $this->SetFont('helvetica', 'b', 10);
-                $this->writeHTMLCell(0, 0, 10, 18, 'Département de Virologie', 0, 0, 0, true, 'C');
-                $this->SetFont('helvetica', 'u', 10);
-                $this->writeHTMLCell(0, 0, 10, 25, 'Laboratoire National de Reference pour la Grippe et les Virus Respiratoires', 0, 0, 0, true, 'C');
-                $this->SetFont('helvetica', 'b', 12);
-                $this->writeHTMLCell(0, 0, 10, 33, 'RESULTATS DE LABORATOIRE DES ECHANTIONS RESPIRATOIRES', 0, 0, 0, true, 'C');
-                $this->SetFont('helvetica', 'u', 10);
-                $this->writeHTMLCell(0, 0, 10, 40, 'TESTES AU COVID-19 PAR RT-PCR en temps réel N°', 0, 0, 0, true, 'C');
-                $this->writeHTMLCell(0, 0, 15, 48, '<hr>', 0, 0, 0, true, 'C');
             } else {
-                $this->SetFont('helvetica', 'B', 16);
-                $this->writeHTMLCell(0, 0, 10, 18, $this->text ?? '', 0, 0, 0, true, 'C');
-                if (!empty($this->lab) && trim($this->lab) != '') {
-                    $this->SetFont('helvetica', '', 10);
-                    $this->writeHTMLCell(0, 0, 10, 25, strtoupper($this->lab), 0, 0, 0, true, 'C');
+                if (!empty($this->logo) && trim($this->logo) != '') {
+                    if (file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . "facility-logo" . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo)) {
+                        $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'facility-logo' . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo;
+                        $this->Image($imageFilePath, 16, 13, 15, '', '', '', 'T');
+                    } else if (file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo)) {
+                        $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo;
+                        $this->Image($imageFilePath, 20, 13, 15, '', '', '', 'T');
+                    }
                 }
-                $this->SetFont('helvetica', '', 12);
-                $this->writeHTMLCell(0, 0, 10, 30, 'COVID-19 TEST - PATIENT REPORT', 0, 0, 0, true, 'C');
-                $this->writeHTMLCell(0, 0, 15, 38, '<hr>', 0, 0, 0, true, 'C');
-            }
-        } else {
-            if (!empty($this->logo) && trim($this->logo) != '') {
-                if (file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . "facility-logo" . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo)) {
-                    $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'facility-logo' . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo;
-                    $this->Image($imageFilePath, 16, 13, 15, '', '', '', 'T');
-                } else if (file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo)) {
-                    $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo;
-                    $this->Image($imageFilePath, 20, 13, 15, '', '', '', 'T');
+                if (file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . 'drc-logo.png')) {
+                    $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . 'drc-logo.png';
+                    $this->Image($imageFilePath, 180, 13, 15, '', '', '', 'T');
                 }
-            }
-            if (file_exists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . 'drc-logo.png')) {
-                $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . 'drc-logo.png';
-                $this->Image($imageFilePath, 180, 13, 15, '', '', '', 'T');
-            }
 
-            // $this->SetFont('helvetica', 'B', 7);
-            // $this->writeHTMLCell(30,0,16,28,$this->text, 0, 0, 0, true, 'A', true);(this two lines comment out for drc)
-            $this->SetFont('helvetica', '', 14);
-            $this->writeHTMLCell(0, 0, 10, 9, 'MINISTERE DE LA SANTE PUBLIQUE', 0, 0, 0, true, 'C');
-            if (!empty($this->text) && trim($this->text) != '') {
+                // $this->SetFont('helvetica', 'B', 7);
+                // $this->writeHTMLCell(30,0,16,28,$this->text, 0, 0, 0, true, 'A', true);(this two lines comment out for drc)
+                $this->SetFont('helvetica', '', 14);
+                $this->writeHTMLCell(0, 0, 10, 9, 'MINISTERE DE LA SANTE PUBLIQUE', 0, 0, 0, true, 'C');
+                if (!empty($this->text) && trim($this->text) != '') {
+                    $this->SetFont('helvetica', '', 12);
+                    //        $this->writeHTMLCell(0,0,10,16,'PROGRAMME NATIONAL DE LUTTE CONTRE LE SIDA ET IST', 0, 0, 0, true, 'C', true);
+                    $this->writeHTMLCell(0, 0, 10, 16, strtoupper($this->text), 0, 0, 0, true, 'C');
+                    $thirdHeading = '23';
+                    $fourthHeading = '28';
+                    $hrLine = '36';
+                    $marginTop = '14';
+                } else {
+                    $thirdHeading = '17';
+                    $fourthHeading = '23';
+                    $hrLine = '30';
+                    $marginTop = '9';
+                }
+                if (!empty($this->lab) && trim($this->lab) != '') {
+                    $this->SetFont('helvetica', '', 9);
+                    $this->writeHTMLCell(0, 0, 10, $thirdHeading, strtoupper($this->lab), 0, 0, 0, true, 'C');
+                }
                 $this->SetFont('helvetica', '', 12);
-                //        $this->writeHTMLCell(0,0,10,16,'PROGRAMME NATIONAL DE LUTTE CONTRE LE SIDA ET IST', 0, 0, 0, true, 'C', true);
-                $this->writeHTMLCell(0, 0, 10, 16, strtoupper($this->text), 0, 0, 0, true, 'C');
-                $thirdHeading = '23';
-                $fourthHeading = '28';
-                $hrLine = '36';
-                $marginTop = '14';
-            } else {
-                $thirdHeading = '17';
-                $fourthHeading = '23';
-                $hrLine = '30';
-                $marginTop = '9';
+                $this->writeHTMLCell(0, 0, 10, $fourthHeading, 'RESULTATS CHARGE VIRALE', 0, 0, 0, true, 'C');
+                $this->writeHTMLCell(0, 0, 15, $hrLine, '<hr>', 0, 0, 0, true, 'C');
             }
-            if (!empty($this->lab) && trim($this->lab) != '') {
-                $this->SetFont('helvetica', '', 9);
-                $this->writeHTMLCell(0, 0, 10, $thirdHeading, strtoupper($this->lab), 0, 0, 0, true, 'C');
-            }
-            $this->SetFont('helvetica', '', 12);
-            $this->writeHTMLCell(0, 0, 10, $fourthHeading, 'RESULTATS CHARGE VIRALE', 0, 0, 0, true, 'C');
-            $this->writeHTMLCell(0, 0, 15, $hrLine, '<hr>', 0, 0, 0, true, 'C');
         }
     }
 
-    // Page footer
     public function Footer()
     {
-        // Position at 15 mm from bottom
+
         $this->SetY(-15);
         // Set font
         $this->SetFont('helvetica', '', 8);
@@ -135,7 +152,13 @@ class Covid19ResultPDFHelper extends Fpdi
         } else {
             $generatedAtTestingLab = "";
         }
-        // Page number
-        $this->Cell(0, 10, 'Page ' . $this->getAliasNumPage() . ' of ' . $this->getAliasNbPages(), 0, false, 'C', 0);
+        if ($this->enableFooter) {
+            // Position at 15 mm from bottom
+            // Page number
+            $this->Cell(0, 10, _translate('Page') . ' ' . $this->getAliasNumPage() . ' ' . _translate('of') . ' ' . $this->getAliasNbPages(), 0, false, 'C', 0);
+        }
+        if (!empty($this->trainingTxt)) {
+            $this->writeHTML('<span style="color:red">' . strtoupper((string) $this->trainingTxt) . '</span>', true, false, true, false, 'M');
+        }
     }
 }
--- app/classes/Helpers/ResultPDFHelpers/GenericTestsResultPDFHelper.php
+++ app/classes/Helpers/ResultPDFHelpers/GenericTestsResultPDFHelper.php
@@ -14,6 +14,18 @@ class GenericTestsResultPDFHelper extends Fpdi
     public ?string $labFacilityId = null;
     public ?string $labName = null;
     public ?string $testType = null;
+    public ?string $trainingTxt = null;
+    private ?string $pdfTemplatePath = null;
+    private bool $templateImported = false;
+    private bool $enableFooter = true; // Default is true to render footer
+
+
+    public function __construct($orientation = 'P', $unit = 'mm', $format = 'A4', $unicode = true, $encoding = 'UTF-8', $diskCache = false, $pdfTemplatePath = null, $enableFooter = true)
+    {
+        parent::__construct($orientation, $unit, $format, $unicode, $encoding, $diskCache);
+        $this->pdfTemplatePath = $pdfTemplatePath ?? null;
+        $this->enableFooter = $enableFooter;
+    }
 
     //Page header
     public function setHeading($logo, $text, $lab, $title = null, $labFacilityId = null, $testType = null)
@@ -33,74 +45,88 @@ class GenericTestsResultPDFHelper extends Fpdi
     //Page header
     public function Header()
     {
-        if (!empty($this->htitle) && trim($this->htitle) != '') {
-            if (!empty($this->logo) && trim($this->logo) != '') {
-                error_log($this->logo);
-                if ($this->imageExists($this->logo)) {
-                    $this->Image($this->logo, 95, 5, 15, '', '', '', 'T');
-                } else if ($this->imageExists(UPLOAD_PATH . DIRECTORY_SEPARATOR . "facility-logo" . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo)) {
-                    $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . "facility-logo" . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo;
-                    $this->Image($imageFilePath, 95, 5, 15, '', '', '', 'T');
-                } else if ($this->imageExists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo)) {
-                    $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo;
-                    $this->Image($imageFilePath, 95, 5, 15, '', '', '', 'T');
-                }
+        if (!empty($this->pdfTemplatePath) && MiscUtility::fileExists($this->pdfTemplatePath)) {
+            if (!$this->templateImported) {
+                $this->setSourceFile($this->pdfTemplatePath);
+                $this->templateImported = true;
             }
-            $this->SetFont('helvetica', 'B', 8);
-            $this->writeHTMLCell(0, 0, 10, 22, $this->text, 0, 0, 0, true, 'C');
-            if (!empty($this->lab) && trim($this->lab) != '') {
-                $this->SetFont('helvetica', '', 9);
-                $this->writeHTMLCell(0, 0, 10, 26, strtoupper($this->lab), 0, 0, 0, true, 'C');
-            }
-            $this->SetFont('helvetica', '', 14);
-            $this->writeHTMLCell(0, 0, 10, 30, strtoupper($this->testType) . ' PATIENT REPORT', 0, 0, 0, true, 'C');
-
-            $this->writeHTMLCell(0, 0, 15, 38, '<hr>', 0, 0, 0, true, 'C');
+            $tplIdx = $this->importPage(1);
+            $this->useTemplate($tplIdx, 0, 0);
         } else {
-            if (!empty($this->logo) && trim($this->logo) != '') {
-                if ($this->imageExists($this->logo)) {
-                    $this->Image($this->logo, 20, 13, 15, '', '', '', 'T');
-                } else if ($this->imageExists(UPLOAD_PATH . DIRECTORY_SEPARATOR . "facility-logo" . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo)) {
-                    $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'facility-logo' . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo;
-                    $this->Image($imageFilePath, 20, 13, 15, '', '', '', 'T');
-                } else if ($this->imageExists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo)) {
-                    $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo;
-                    $this->Image($imageFilePath, 20, 13, 15, '', '', '', 'T');
+            if (!empty($this->htitle) && trim($this->htitle) != '') {
+                if (!empty($this->logo) && trim($this->logo) != '') {
+                    error_log($this->logo);
+                    if ($this->imageExists($this->logo)) {
+                        $this->Image($this->logo, 95, 5, 15, '', '', '', 'T');
+                    } else if ($this->imageExists(UPLOAD_PATH . DIRECTORY_SEPARATOR . "facility-logo" . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo)) {
+                        $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . "facility-logo" . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo;
+                        $this->Image($imageFilePath, 95, 5, 15, '', '', '', 'T');
+                    } else if ($this->imageExists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo)) {
+                        $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo;
+                        $this->Image($imageFilePath, 95, 5, 15, '', '', '', 'T');
+                    }
                 }
-            }
+                $this->SetFont('helvetica', 'B', 8);
+                $this->writeHTMLCell(0, 0, 10, 22, $this->text, 0, 0, 0, true, 'C');
+                if (!empty($this->lab) && trim($this->lab) != '') {
+                    $this->SetFont('helvetica', '', 9);
+                    $this->writeHTMLCell(0, 0, 10, 26, strtoupper($this->lab), 0, 0, 0, true, 'C');
+                }
+                $this->SetFont('helvetica', '', 14);
+                $this->writeHTMLCell(0, 0, 10, 30, strtoupper($this->testType) . ' PATIENT REPORT', 0, 0, 0, true, 'C');
 
-            if (!empty($this->text) && trim($this->text) != '') {
-                $this->SetFont('helvetica', '', 16);
-                $this->writeHTMLCell(0, 0, 10, 12, strtoupper($this->text), 0, 0, 0, true, 'C');
-                $thirdHeading = '21';
-                $fourthHeading = '28';
-                $hrLine = '36';
-                $marginTop = '14';
+                $this->writeHTMLCell(0, 0, 15, 38, '<hr>', 0, 0, 0, true, 'C');
             } else {
-                $thirdHeading = '14';
-                $fourthHeading = '23';
-                $hrLine = '30';
-                $marginTop = '9';
+                if (!empty($this->logo) && trim($this->logo) != '') {
+                    if ($this->imageExists($this->logo)) {
+                        $this->Image($this->logo, 20, 13, 15, '', '', '', 'T');
+                    } else if ($this->imageExists(UPLOAD_PATH . DIRECTORY_SEPARATOR . "facility-logo" . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo)) {
+                        $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'facility-logo' . DIRECTORY_SEPARATOR . $this->labFacilityId . DIRECTORY_SEPARATOR . $this->logo;
+                        $this->Image($imageFilePath, 20, 13, 15, '', '', '', 'T');
+                    } else if ($this->imageExists(UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo)) {
+                        $imageFilePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $this->logo;
+                        $this->Image($imageFilePath, 20, 13, 15, '', '', '', 'T');
+                    }
+                }
+
+                if (!empty($this->text) && trim($this->text) != '') {
+                    $this->SetFont('helvetica', '', 16);
+                    $this->writeHTMLCell(0, 0, 10, 12, strtoupper($this->text), 0, 0, 0, true, 'C');
+                    $thirdHeading = '21';
+                    $fourthHeading = '28';
+                    $hrLine = '36';
+                    $marginTop = '14';
+                } else {
+                    $thirdHeading = '14';
+                    $fourthHeading = '23';
+                    $hrLine = '30';
+                    $marginTop = '9';
+                }
+                if (!empty($this->lab) && trim($this->lab) != '') {
+                    $this->SetFont('helvetica', '', 10);
+                    $this->writeHTMLCell(0, 0, 8, $thirdHeading, strtoupper($this->lab), 0, 0, 0, true, 'C');
+                }
+                $this->SetFont('helvetica', '', 12);
+                $this->writeHTMLCell(0, 0, 10, $fourthHeading, strtoupper($this->testType) . ' - PATIENT REPORT', 0, 0, 0, true, 'C');
+                $this->writeHTMLCell(0, 0, 15, $hrLine, '<hr>', 0, 0, 0, true, 'C');
             }
-            if (!empty($this->lab) && trim($this->lab) != '') {
-                $this->SetFont('helvetica', '', 10);
-                $this->writeHTMLCell(0, 0, 8, $thirdHeading, strtoupper($this->lab), 0, 0, 0, true, 'C');
-            }
-            $this->SetFont('helvetica', '', 12);
-            $this->writeHTMLCell(0, 0, 10, $fourthHeading, strtoupper($this->testType) . ' - PATIENT REPORT', 0, 0, 0, true, 'C');
-            $this->writeHTMLCell(0, 0, 15, $hrLine, '<hr>', 0, 0, 0, true, 'C');
         }
     }
 
     // Page footer
     public function Footer()
     {
-        // Position at 15 mm from bottom
+
         $this->SetY(-15);
         // Set font
         $this->SetFont('helvetica', '', 8);
-        // Page number
-        $this->Cell(0, 10, 'Page' . $_SESSION['aliasPage'] . ' of ' . $_SESSION['nbPages'], 0, false, 'C', 0);
-        //$this->Cell(0, 10, 'Page 1 of 1', 0, false, 'C', 0);
+        if ($this->enableFooter) {
+            // Position at 15 mm from bottom
+            // Page number
+            $this->Cell(0, 10, _translate('Page') . ' ' . $this->getAliasNumPage() . ' ' . _translate('of') . ' ' . $this->getAliasNbPages(), 0, false, 'C', 0);
+        }
+        if (!empty($this->trainingTxt)) {
+            $this->writeHTML('<span style="color:red">' . strtoupper((string) $this->trainingTxt) . '</span>', true, false, true, false, 'M');
+        }
     }
 }
