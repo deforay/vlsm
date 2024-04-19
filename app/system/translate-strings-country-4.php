bf409f1e41cf96ae55f7a04beeeb838e58d33949
Amit Dugar <amit@deforay.com>
2024-04-19 14:39:21 +0530

    Minor translation updates

---
+++ app/system/translate-strings-country-4.php
@@ -0,0 +1,73 @@
+<?php
+
+// SYSTEM GENERATED FILE. DO NOT EDIT.
+
+// THIS FILE IS USED TO GENERATE THE STRING TRANSLATIONS FOR COUNTRY ID: 4.
+
+_translate("Control Vl Testing");
+_translate("Coinfection");
+_translate("Recency");
+_translate("Other");
+_translate("6 Months");
+_translate("12 Months");
+_translate("24 Months");
+_translate("36 Months(3 Years)");
+_translate("&gt;= 4 years");
+_translate("3 months after a VL &gt; 1000cp/ml");
+_translate("Suspected Treatment Failure");
+_translate("VL Pregnant Woman");
+_translate("VL Breastfeeding woman");
+_translate("Tuberculosis");
+_translate("Viral Hepatitis");
+_translate("&lt; 20");
+_translate("&lt; 40");
+_translate("&lt; 400");
+_translate("Target Not Detected / Non Détecté");
+_translate("Failed");
+_translate("reason one");
+_translate("reason two");
+_translate("Bad rslt");
+_translate("Poorly labelled specimen");
+_translate("Mismatched sample and form labeling");
+_translate("Missing labels on container or tracking form");
+_translate("Sample without request forms/Tracking forms");
+_translate("Name/Information of requester is missing");
+_translate("Missing information on request form - Age");
+_translate("Missing information on request form - Sex");
+_translate("Missing information on request form - Sample Collection Date");
+_translate("Missing information on request form - ART No");
+_translate("Inappropriate specimen packing");
+_translate("Inappropriate specimen for test request");
+_translate("EDTA tube specimens that arrived hemolyzed");
+_translate("ETDA tube that arrives more than 24 hours after specimen collection");
+_translate("Plasma that arrives at a temperature above 8 C");
+_translate("Plasma tube contain less than 1.5 mL");
+_translate("DBS cards with insufficient blood spots");
+_translate("DBS card with clotting present in spots");
+_translate("DBS cards that have serum rings indicating contamination around spots");
+_translate("VL Mechine Flag");
+_translate("doublons");
+_translate("ECHANTILLON NON RECU");
+_translate("A Reprelever");
+_translate("ECHANTILLON HEMOLYSE");
+_translate("ECHANTILLON INSUFFISANT 200ML");
+_translate("DEUX ECHANTILLONS AVEC MEME CODE SUR LE CRYOTUBE SUR LE FICHE LE MEME CODE AVEC LES AGES DIFFERENTS");
+_translate("LES FICHES DIFFERENTES AUX INFORMATIONS MAIS LES DEUX  ECHANTIONS ONT LA  MEME CODE D&#039;une fiche LES ");
+_translate("A Reprelever après plusieurs manipulations échoué");
+_translate("quantite insuffisante + hemolyse");
+_translate("QUANTITE INSUFFISANTE 200MICROLITRE+HEMOLUSE");
+_translate("QUANTITE INSUFFISANTE 800MICROLITRE+HEMOLYSE");
+_translate("ECHANTILLON POURRI ET HEMOLYSE");
+_translate("QUANTITE INSUFFISANTE 8OOML");
+_translate("QUANTITE INSUFFISANTE 400MICROLITRE");
+_translate("ECHANTILLON VIDE");
+_translate("ECHANTILLON INSUFFISANTE 500MICROLITRE");
+_translate("Discondance de codification sur la fiche et cryotube");
+_translate("ECH POURRI");
+_translate("DOUBONS.DEUX ECH MEME CODE");
+_translate("INVALIDE ");
+_translate("DBS");
+_translate("Plasma");
+_translate("Whole Blood");
+_translate("Frozen Plasma");
+_translate("CAPILLARY DBS");
