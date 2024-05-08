1083067638484e058052d1b438776650d0efd66e
Amit Dugar <amit@deforay.com>
2024-05-08 13:33:49 +0530

    Fixed DHIS2 class

--- app/classes/Utilities/CaptchaUtility.php
+++ app/classes/Utilities/CaptchaUtility.php
@@ -3,6 +3,7 @@
 namespace App\Utilities;
 
 use App\Utilities\MiscUtility;
+use Gregwar\Captcha\PhraseBuilder;
 use Gregwar\Captcha\CaptchaBuilder;
 
 final class CaptchaUtility
@@ -14,7 +15,13 @@ final class CaptchaUtility
             session_start();
         }
 
-        $phrase = MiscUtility::generateRandomString(4);
+        $phrase = null;
+        //if it is development environment, then let us keep it simple
+        if (APPLICATION_ENV === "development") {
+            $phrase = "zaq";
+        }
+        $phraseBuilder = new PhraseBuilder(4, '0123456789');
+        $builder = new CaptchaBuilder($phrase, $phraseBuilder);
 
         $builder = new CaptchaBuilder($phrase);
         $builder->setDistortion(false);
