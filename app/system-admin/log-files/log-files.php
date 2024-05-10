7b22b5be07e8d35fa560884a92f1b83bea882d46
Thanaseelan <thanaseelan@deforay.com>
2024-05-10 12:43:59 +0530

    log file showing recelty logged error for last 31 days

--- app/system-admin/log-files/log-files.php
+++ app/system-admin/log-files/log-files.php
@@ -140,6 +140,7 @@ require_once(APPLICATION_PATH . '/system-admin/admin-header.php');
 						$('#logViewer').append(data);
 						let linesReturned = (data.match(/class='logLine'/g) || []).length;
 						start += linesReturned; // Update the start index based on actual lines returned
+						$('#userDate').val('<?php echo $_SESSION['selectedDate'];?>');
 					}
 					loading = false;
 				},
