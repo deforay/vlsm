796dd0446bb522b368126a7538d4a5a17ea8e362
Amit Dugar <amit@deforay.com>
2024-06-19 15:21:34 +0530

    Minor changes in EID Result PDF

--- app/eid/results/pdf/result-pdf-burkina-faso.php
+++ app/eid/results/pdf/result-pdf-burkina-faso.php
@@ -334,7 +334,7 @@ if (!empty($result)) {
     $html .= '<td style="line-height:11px;font-size:11px;font-weight:bold;text-align:left;">TEST PLATFORM</td>';
     $html .= '</tr>';
     $html .= '<tr>';
-    $html .= '<td style="line-height:11px;font-size:11px;text-align:left;">' . ($result['testingPlatform'] ?? $result['eid_test_platform']) . '</td>';
+    $html .= '<td style="line-height:11px;font-size:11px;text-align:left;">' . ($result['instrument_machine_name'] ?? $result['eid_test_platform']) . '</td>';
     $html .= '</tr>';
     $html .= '<tr>';
     $html .= '<td colspan="3" style="line-height:2px;border-bottom:1px solid #d3d3d3;"></td>';
