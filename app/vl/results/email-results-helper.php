37a06b719fa3006a05bad272cdd8613cb639cfe5
Jeyabanu <jeyabanu@deforay.com>
2024-08-01 17:49:00 +0530

    Updated some fields in email test results in all test types

--- app/vl/results/email-results-helper.php
+++ app/vl/results/email-results-helper.php
@@ -4,6 +4,7 @@ use App\Registries\AppRegistry;
 use App\Registries\ContainerRegistry;
 use App\Services\CommonService;
 use App\Services\DatabaseService;
+use App\Services\TestResultsService;
 use App\Utilities\DateUtility;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
@@ -15,6 +16,10 @@ $db = ContainerRegistry::get(DatabaseService::class);
 
 /** @var CommonService $general */
 $general = ContainerRegistry::get(CommonService::class);
+
+/** @var TestResultsService $general */
+$testResult = ContainerRegistry::get(TestResultsService::class);
+
 
 // Sanitized values from $request object
 /** @var Laminas\Diactoros\ServerRequest $request */
@@ -57,10 +62,13 @@ if (isset($_POST['toEmail']) && trim((string) $_POST['toEmail']) != '') {
          "status" => "pending",
       );
 
+
       $storeMail = $db->insert('temp_mail',$tempMailData);
 
       if($storeMail)
       {
+         $updateInfo = $testResult->updateEmailTestResultsInfo('vl',$tempMailData);
+
          $_SESSION['alertMsg'] = 'Email will be sent shortly';
          header('location:email-results.php');
       }
