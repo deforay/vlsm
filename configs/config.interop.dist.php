4488257e7776977d27cba3739638b75c50bd3434
Amit Dugar <amit@deforay.com>
2024-08-27 14:13:46 +0530

    Minor change to interop config

--- configs/config.interop.dist.php
+++ configs/config.interop.dist.php
@@ -6,3 +6,5 @@ const DHIS2_PASSWORD = 'adminpassword';
 
 const FHIR_URL = 'https://fhir.example.org/';
 const FHIR_AUTH = 'fhir-auth';
+
+const EXTERNAL_RESULTS_RECEIVER_URL = 'https://external-results-receiver.example.org/';
