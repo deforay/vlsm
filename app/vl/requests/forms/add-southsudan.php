47705ec9f183187ef8cdbf1ada2089baf23a4d51
Amit Dugar <amit@deforay.com>
2025-09-04 15:38:39 +0530

    Minor changes in South Sudan form for HIV VL

--- app/vl/requests/forms/add-southsudan.php
+++ app/vl/requests/forms/add-southsudan.php
@@ -371,9 +371,7 @@ $sFormat = '';
                                                        </div>
                                                   </div>
 
-                                             </div>
-
-                                             <div class="row">
+                                             
                                                   <div class="col-xs-3 col-md-3">
                                                        <div class="form-group">
                                                             <label for="sampleReceivedDate">Date Sample Received at Testing Lab <span class="mandatory">*</span></label>
